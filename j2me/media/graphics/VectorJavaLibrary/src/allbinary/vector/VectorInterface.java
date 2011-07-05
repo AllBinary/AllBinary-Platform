cdef5a53e338b16321679aa82f201b8b0c085186
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-05 04:19:16 -0500

    added

---
+++ j2me/media/graphics/VectorJavaLibrary/src/allbinary/vector/VectorInterface.java
@@ -0,0 +1,17 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2011 AllBinary
+ *
+ * Created By: Travis Berthelot
+ * Date: Oct 5, 2007, 7:07:26 PM
+ *
+ *
+ * Modified By         When       ?
+ *
+ */
+package allbinary.vector;
+
+public interface VectorInterface {
+   int[][] getPoints(int frame);
+   void setPoints(int[][][] currentPoints);
+}
