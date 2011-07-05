cd98253ee8a28b927f8b1feafdb984e730f7d414
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-05 05:23:57 -0500

    added

---
+++ j2me/media/graphics/geography/map/LayerPlacementLibrary/src/allbinary/game/layer/GameLayerPlacementInterfaceBasicArrayListFactoryInterfaceFactory.java
@@ -0,0 +1,19 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2011 AllBinary
+ *
+ * Created By: Travis Berthelot
+ * Date: August 2, 2006
+ *
+ *
+ * Modified By         When       ?
+ *
+ */
+package allbinary.game.layer;
+
+
+public interface GameLayerPlacementInterfaceBasicArrayListFactoryInterfaceFactory
+{
+    LayerPlacementInterfaceBasicArrayListFactoryInterface getInstance(int level);
+    int size();
+}
