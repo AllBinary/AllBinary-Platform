f5c5d162598d55744d50ed7ac0638938f7940e77
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-05 01:54:24 -0500

    added

---
+++ j2me/GroupLibrary/src/allbinary/game/identification/GroupInterface.java
@@ -0,0 +1,18 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2011 AllBinary
+ *
+ * Created By: Travis Berthelot
+ * Date: 11/19/02
+ *
+ *
+ * Modified By         When       ?
+ *
+ */
+package allbinary.game.identification;
+
+public interface GroupInterface
+{
+   String getGroupName();
+   int getGroupId();
+}
