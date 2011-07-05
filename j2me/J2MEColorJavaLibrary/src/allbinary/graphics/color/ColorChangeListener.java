1962cc627e29a689479482f7f886a9a7db03739a
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-05 06:07:27 -0500

    added

---
+++ j2me/J2MEColorJavaLibrary/src/allbinary/graphics/color/ColorChangeListener.java
@@ -0,0 +1,18 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2011 AllBinary
+ *
+ * Created By: Travis Berthelot
+ * Date: 11/19/02
+ *
+ *
+ * Modified By         When       ?
+ *
+ */
+package allbinary.graphics.color;
+
+import allbinary.logic.basic.util.event.EventListenerInterface;
+
+public interface ColorChangeListener extends EventListenerInterface
+{
+}
