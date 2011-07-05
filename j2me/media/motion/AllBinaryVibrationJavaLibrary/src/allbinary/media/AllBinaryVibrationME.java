1b78a5c6cefa2317e88765e9228bb89d048d0319
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-05 04:29:30 -0500

    added

---
+++ j2me/media/motion/AllBinaryVibrationJavaLibrary/src/allbinary/media/AllBinaryVibrationME.java
@@ -0,0 +1,20 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2011 AllBinary
+ *
+ * Created By: Travis Berthelot
+ * Date: 11/29/02
+ *
+ *
+ * Modified By         When       ?
+ *
+ */
+package allbinary.media;
+
+public class AllBinaryVibrationME 
+{
+   public void vibrate(int duration, int type, int volume) 
+   {
+   }
+
+}
