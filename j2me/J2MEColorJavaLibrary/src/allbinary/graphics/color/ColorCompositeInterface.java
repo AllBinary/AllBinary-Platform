c7605f686802ac2277409141baf2066741e45aad
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-05 06:07:53 -0500

    added

---
+++ j2me/J2MEColorJavaLibrary/src/allbinary/graphics/color/ColorCompositeInterface.java
@@ -0,0 +1,18 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2011 AllBinary
+ *
+ * Created By: Travis Berthelot
+ * Date: 11/19/02
+ *
+ *
+ * Modified By         When       ?
+ *
+ */
+package allbinary.graphics.color;
+
+public interface ColorCompositeInterface 
+extends BasicColorCompositeSetterInterface
+{
+    BasicColor getBasicColor();
+}
