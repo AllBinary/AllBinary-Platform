cc3f017e572f222d0c8620d6959ca2e76b1ad9a3
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-05 01:55:36 -0500

    added

---
+++ j2me/GroupLibrary/src/allbinary/game/identification/GroupInterfaceCompositeInterface.java
@@ -0,0 +1,17 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2011 AllBinary
+ *
+ * Created By: Travis Berthelot
+ * Date: 11/19/02
+ *
+ *
+ * Modified By         When       ?
+ *
+ */
+package allbinary.game.identification;
+
+public interface GroupInterfaceCompositeInterface
+{
+   Group getGroupInterface();
+}
