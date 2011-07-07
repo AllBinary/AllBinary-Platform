f78782b2a5f4a58b14cf610c92e1519bdf4ee2d6
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 04:42:14 -0500

    added

---
+++ j2me/AndroidToAllBinaryJavaLibrary/src/allbinary/graphics/displayable/screen/ScreenRepaintUtil.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.graphics.displayable.screen;
+
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Displayable;
+
+/**
+ *
+ * @author user
+ */
+public class ScreenRepaintUtil
+{
+    public static void repaint(Displayable displayable)
+    {
+        Canvas canvas = (Canvas) displayable; 
+        canvas.repaint();
+    }
+}
