90720458c4a4dc3904e12953b52aefb79202ea95
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 06:16:23 -0500

    added

---
+++ physics/MovementJavaLibrary/src/org/allbinary/physics/movement/Movement.java
@@ -0,0 +1,46 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.physics.movement;
+
+import allbinary.game.layer.AllBinaryGameLayer;
+import allbinary.logic.math.BasicDecimal;
+
+/**
+ *
+ * @author user
+ */
+public class Movement implements MovementInterface 
+{
+   protected static final BasicDecimal ZERO_BIGDECIMAL = new BasicDecimal();
+    
+   public Movement()
+   {
+
+   }
+   
+   public void init(short angle, BasicDecimal speed)
+   {
+   }
+   
+   public void process(AllBinaryGameLayer layer)
+   throws Exception
+   {
+      
+   }
+
+   public void stop()
+   {
+      
+   }
+}
