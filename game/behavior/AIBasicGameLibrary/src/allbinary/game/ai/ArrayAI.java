1ca416dc2d485084d8fc46e1b5124f4715b6d324
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 06:21:07 -0500

    added

---
+++ game/behavior/AIBasicGameLibrary/src/allbinary/game/ai/ArrayAI.java
@@ -0,0 +1,46 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.ai;
+
+import allbinary.ai.ArtificialIntelligenceInterface;
+import allbinary.game.input.GameInput;
+import allbinary.layer.AllBinaryLayer;
+import allbinary.layer.AllBinaryLayerManager;
+
+public class ArrayAI extends BasicAI
+{
+   private final ArtificialIntelligenceInterface[] artificialIntelligenceInterfaceArray;
+
+   public ArrayAI(ArtificialIntelligenceInterface[] artificialIntelligenceInterface,
+           AllBinaryLayer ownerLayerInterface,
+      GameInput gameInput)
+   {
+      super(ownerLayerInterface, gameInput);
+      this.artificialIntelligenceInterfaceArray = artificialIntelligenceInterface;
+   }
+
+   public void processAI(AllBinaryLayerManager allBinaryLayerManager) throws Exception
+   {
+      int size = this.artificialIntelligenceInterfaceArray.length;
+      for(int index = 0; index < size; index++)
+      {
+          this.artificialIntelligenceInterfaceArray[index].processAI(allBinaryLayerManager);
+      }
+   }
+
+   public ArtificialIntelligenceInterface[] getArtificialIntelligenceInterface()
+   {
+      return artificialIntelligenceInterfaceArray;
+   }
+}
