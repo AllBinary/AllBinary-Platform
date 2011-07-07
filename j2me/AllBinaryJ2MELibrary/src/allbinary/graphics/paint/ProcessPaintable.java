53fbb2c6106f027b469f64c9a072bf9b338cc726
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 04:28:26 -0500

    added

---
+++ j2me/AllBinaryJ2MELibrary/src/allbinary/graphics/paint/ProcessPaintable.java
@@ -0,0 +1,23 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.graphics.paint;
+
+//This is used instead of interface for performance considerations since interfaces are a little slow
+public class ProcessPaintable extends Paintable
+{
+    public void process()
+    {
+        
+    }
+}
