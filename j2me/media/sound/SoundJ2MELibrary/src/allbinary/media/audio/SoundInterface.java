725e6c722db3f9fa814fb33ce906a119de6650d4
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 06:41:55 -0500

    added

---
+++ j2me/media/sound/SoundJ2MELibrary/src/allbinary/media/audio/SoundInterface.java
@@ -0,0 +1,27 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.audio;
+
+import javax.microedition.media.Player;
+
+
+public interface SoundInterface {
+
+    String getResource();
+    
+    Player getPlayer();
+
+    void init() throws java.lang.Exception;
+
+}
