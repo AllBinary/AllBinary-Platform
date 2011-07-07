b744bf005a970d8bbb520d0da177afec6ea2500d
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 06:22:26 -0500

    added

---
+++ game/behavior/AIBasicGameLibrary/src/allbinary/game/ai/ScrollOverAtHalfAI.java
@@ -0,0 +1,59 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.ai;
+
+import allbinary.game.input.GameInput;
+import allbinary.graphics.displayable.DisplayInfoSingleton;
+import allbinary.layer.AllBinaryLayer;
+import allbinary.layer.AllBinaryLayerManager;
+
+public class ScrollOverAtHalfAI extends BasicAI
+{
+   public ScrollOverAtHalfAI(AllBinaryLayer ownerLayerInterface, GameInput gameInput)
+   {
+      super(ownerLayerInterface, gameInput);
+   }
+
+   public void processAI(AllBinaryLayerManager allBinaryLayerManager)
+           throws Exception
+   {
+       AllBinaryLayer ownerLayerInterface = this.getOwnerLayerInterface();
+       
+      int x = ownerLayerInterface.getX();
+      int y = ownerLayerInterface.getY();
+      int halfWidth = ownerLayerInterface.getHalfWidth();
+      int halfHeight = ownerLayerInterface.getHalfHeight();
+      
+      DisplayInfoSingleton displayInfo = DisplayInfoSingleton.getInstance();
+      if (x > displayInfo.getLastWidth() - halfWidth)
+      {
+         ownerLayerInterface.setPosition(halfWidth + 1, y);
+      }
+
+      if (y > displayInfo.getLastHeight() - halfHeight)
+      {
+         ownerLayerInterface.setPosition(x, halfHeight + 1);
+      }
+
+      if (x < -halfWidth)
+      {
+         ownerLayerInterface.setPosition(displayInfo.getLastWidth() - halfWidth, y);
+      }
+
+      if (y < -halfHeight)
+      {
+         ownerLayerInterface.setPosition(x, displayInfo.getLastHeight() - halfHeight);
+      }
+   }   
+}
