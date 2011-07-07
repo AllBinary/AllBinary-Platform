e6cc5865ceb06db1c8b0e38ddbdd1723bf18ff74
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 06:16:39 -0500

    added

---
+++ physics/MovementJavaLibrary/src/org/allbinary/physics/movement/MovementInterface.java
@@ -0,0 +1,27 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.physics.movement;
+
+import allbinary.game.layer.AllBinaryGameLayer;
+import allbinary.logic.math.BasicDecimal;
+
+/**
+ *
+ * @author user
+ */
+public interface MovementInterface {
+   void init(short angle, BasicDecimal speed);
+   void process(AllBinaryGameLayer layer)throws Exception;
+   void stop();
+}
