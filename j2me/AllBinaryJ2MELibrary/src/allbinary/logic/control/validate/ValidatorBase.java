3998db4f00bb2943d52e8a3656611dfd53bc770d
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 04:30:26 -0500

    added

---
+++ j2me/AllBinaryJ2MELibrary/src/allbinary/logic/control/validate/ValidatorBase.java
@@ -0,0 +1,72 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.control.validate;
+
+import java.util.Hashtable;
+
+import org.allbinary.util.BasicArrayList;
+
+import abcs.logic.java.bool.BooleanFactory;
+
+public class ValidatorBase 
+implements ValidatorBaseInterface
+{
+    private static final ValidatorBase instance = new ValidatorBase();
+
+    public static ValidatorBase getInstance()
+    {
+        return instance;
+    }    
+    
+    public ValidatorBase()
+    {
+        
+    }
+    
+    /* (non-Javadoc)
+     * @see allbinary.logic.control.validate.ValidatorBaseInterface#isValid()
+     */
+    //@Override
+    public Boolean isValid()
+    {
+       return BooleanFactory.getInstance().TRUE;
+    }
+    
+    /* (non-Javadoc)
+     * @see allbinary.logic.control.validate.ValidatorBaseInterface#validationInfo()
+     */
+    //@Override
+    public String validationInfo()
+    {
+       return null;
+    }
+
+    /* (non-Javadoc)
+     * @see allbinary.logic.control.validate.ValidatorBaseInterface#toHashMap()
+     */
+    //@Override
+    public Hashtable toHashtable()
+    {
+       return null;
+    }
+
+    /* (non-Javadoc)
+     * @see allbinary.logic.control.validate.ValidatorBaseInterface#toVector()
+     */
+    //@Override
+    public BasicArrayList toList()
+    {
+       return null;
+    }
+}
