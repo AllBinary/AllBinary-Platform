9fb50c0288ead9a80d6328396ddcda7eacb36684
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-06 22:52:34 -0500

    fixed headers

--- j2me/games/TickableJavaLibrary/src/allbinary/game/tick/TickableInterface.java
+++ j2me/games/TickableJavaLibrary/src/allbinary/game/tick/TickableInterface.java
@@ -1,14 +1,16 @@
 /*
- * AllBinary Open License Version 1
- * Copyright (c) 2011 AllBinary
- *
- * Created By: Travis Berthelot
- * Date: Sep 12, 2007, 11:09:16 PM
- *
- *
- * Modified By         When       ?
- *
- */
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
 package allbinary.game.tick;
 
 import allbinary.layer.AllBinaryLayerManager;
