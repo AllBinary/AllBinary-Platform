cce8f3dddd1cf867be7a27113ee9f0835054f3c2
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 06:26:47 -0500

    added

---
+++ game/behavior/AIBounceGameLibrary/src/allbinary/game/ai/BoundBounceAI.java
@@ -0,0 +1,59 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.ai;
+
+import javax.microedition.lcdui.Graphics;
+
+import org.allbinary.bounds.BoundsVisitorInterface;
+import org.allbinary.bounds.LayerBounds;
+
+import allbinary.game.input.GameInput;
+import allbinary.layer.AllBinaryLayer;
+import allbinary.layer.AllBinaryLayerManager;
+
+public class BoundBounceAI extends BasicAI
+{
+   protected int currentRelativeAngle = 0;
+   //private int currentSpeed;
+   
+   private LayerBounds layerBounds;
+
+   private BoundsVisitorInterface boundsVisitorInterface;
+
+    public BoundBounceAI(AllBinaryLayer ownerLayerInterface, GameInput gameInput,
+       LayerBounds layerBounds, BoundsVisitorInterface boundsVisitorInterface)
+            throws Exception
+    {
+      super(ownerLayerInterface, gameInput);
+
+      this.layerBounds = layerBounds;
+
+      this.boundsVisitorInterface = boundsVisitorInterface;
+    }
+
+   public void paint(Graphics graphics)
+   {
+      graphics.drawRect(this.layerBounds.getRectangle().getPoint().getX(),
+              this.layerBounds.getRectangle().getPoint().getY(),
+              this.layerBounds.getRectangle().getWidth(),
+              this.layerBounds.getRectangle().getHeight());
+   }
+   
+   public void processAI(AllBinaryLayerManager allBinaryLayerManager) 
+           throws Exception
+   {      
+      this.layerBounds.visit(this.boundsVisitorInterface);
+   }
+   
+}
