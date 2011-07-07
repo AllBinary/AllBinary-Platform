160ac119e46161c23356d33b11383be6270af87b
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 04:52:47 -0500

    added

---
+++ j2me/EventJ2MELibraryM/src/main/java/allbinary/logic/basic/util/event/AllBinaryEventCircularPool.java
@@ -0,0 +1,39 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.basic.util.event;
+
+import org.allbinary.util.CircularIndexUtil;
+
+import com.abcs.logic.util.cache.BaseCircularPool;
+
+public class AllBinaryEventCircularPool
+extends BaseCircularPool
+{
+    public AllBinaryEventCircularPool(int max)
+    {
+        this.OBJECT_ARRAY = new Object[max];
+        this.circularIndexUtil = CircularIndexUtil.getInstance(max);
+    }
+    
+    public void init(
+            AllBinaryEventObjectFactoryInterface allBinaryEventObjectFactoryInterface)
+    {
+        this.circularIndexUtil.setIndex(0);
+        int size = this.circularIndexUtil.getSize();
+        for (int index = 0; index < size ; index++)
+        {
+            OBJECT_ARRAY[index] = allBinaryEventObjectFactoryInterface.getInstance();
+        }
+    }
+}
