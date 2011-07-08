623337791d624fc2ce93a48a98cb2046cb719936
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:45:44 -0500

    added

---
+++ allbinary_src/VisualTransformJavaLibrary/src/allbinary/logic/visual/transform/info/CustomTransformInfo.java
@@ -0,0 +1,34 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.visual.transform.info;
+
+public class CustomTransformInfo extends TransformInfo 
+   implements TransformInfoInterface//, OutputTransformInfoInterface
+{
+
+   //private String outputTypeName;
+   //private String inputOutputTypeName;
+   //private String inputOutputTypeFile;
+
+   public CustomTransformInfo(
+      String outputTypeName,
+      String inputOutputTypeName,
+      String inputOutputTypeFile,     
+      String name, 
+      String objectFileName, String objectConfigFileName, 
+      String templateFileName, String dataFileName) throws Exception
+   {
+      
+   }
+}
