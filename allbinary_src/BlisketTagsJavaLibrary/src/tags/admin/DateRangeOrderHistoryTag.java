9ff20fe2afb9fcdabc9a55f56dfea2ac03102b68
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:16:54 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/tags/admin/DateRangeOrderHistoryTag.java
@@ -0,0 +1,126 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package tags.admin;
+
+import abcs.logic.communication.log.LogUtil;
+
+import tags.StoreValidationTransformTag;
+
+import abcs.logic.communication.http.request.AbResponseHandler;
+import abcs.logic.communication.log.LogFactory;
+
+import javax.servlet.jsp.JspTagException;
+
+public class DateRangeOrderHistoryTag extends StoreValidationTransformTag
+{
+   //private String id;
+   private String status;
+   //private String groupId;
+   /*
+   private String shipped;
+   private String partiallyShipped;
+   private String processing;
+   private String preprocessing;
+   private String cancelled;
+    */
+   
+   public DateRangeOrderHistoryTag()
+   {
+      super();
+   }
+   
+/*
+   public void setId(String id)
+   {
+      this.id=id;
+   }
+ */
+   
+   public void setStatus(String value)
+   {
+      this.status=value;
+   }
+   
+  /*
+   public void setGroupId(String value)
+   {
+      this.groupId=value;
+   }
+   */
+   /*
+   public void setShipped(String value)
+   {
+      this.shipped=value;
+   }
+    
+   public void setPartiallyShipped(String value)
+   {
+      this.partiallyShipped=value;
+   }
+    
+   public void setPreprocessing(String value)
+   {
+      this.preprocessing=value;
+   }
+    
+   public void setProcessing(String value)
+   {
+      this.processing=value;
+   }
+    
+   public void setCancelled(String value)
+   {
+      this.cancelled=value;
+   }
+    */
+   
+   
+   public int doStartTag() throws JspTagException
+   {
+      try
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.JSPTAG))
+         {
+            LogUtil.put(LogFactory.getInstance("Tag Start",this,"doStartTag"));
+         }
+         
+         this.setName("Range Order History View");
+         this.setObjectFile("views.admin.orderhistory.DateRangeOrderHistoryView");
+         
+         if(this.getCommand()!=null)
+         {
+            if (this.getCommand().compareTo(allbinary.globals.GLOBALS.VIEW)==0)
+            {
+               
+            }
+            else
+            {
+               throw new Exception("No Such View Command: " + this.getCommand());
+            }
+            return super.doStartTag();
+            
+         }
+         throw new Exception("Command Null");
+         
+      }
+      catch(Exception e)
+      {
+         AbResponseHandler.sendJspTagRedirect(
+         this.pageContext,
+         e);
+         return SKIP_BODY;
+      }
+   }
+}
+
