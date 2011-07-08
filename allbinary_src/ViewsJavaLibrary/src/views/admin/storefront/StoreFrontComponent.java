449fc2d5c25d7ed3013b8495aa0c0fe63282c2a2
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:13:39 -0500

    added

---
+++ allbinary_src/ViewsJavaLibrary/src/views/admin/storefront/StoreFrontComponent.java
@@ -0,0 +1,65 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package views.admin.storefront;
+
+import abcs.logic.communication.log.LogFactory;
+import javax.servlet.http.HttpServletRequest;
+
+import abcs.logic.communication.log.LogUtil;
+
+import allbinary.business.context.modules.storefront.StoreFront;
+import allbinary.business.context.modules.storefront.StoreFrontInterface;
+
+import allbinary.logic.visual.transform.info.TransformInfoInterface;
+
+import views.business.context.modules.storefront.HttpStoreComponentView;
+
+public class StoreFrontComponent extends HttpStoreComponentView
+{
+   protected StoreFrontInterface newStoreFrontInterface;
+   
+   public StoreFrontComponent(TransformInfoInterface transformInfoInterface) throws Exception
+   {
+      super(transformInfoInterface);
+
+      this.newStoreFrontInterface = (StoreFrontInterface) 
+         new StoreFront((HttpServletRequest) this.getPageContext().getRequest());
+
+      //the storeName should always be in the session since only a store manager 
+      //or higher can manage a store
+      if(this.newStoreFrontInterface.getName()==null)
+      {
+         this.newStoreFrontInterface.setName(
+           this.getWeblisketSession().getStoreName());
+      }
+   }
+
+   public String view() throws Exception
+   {
+      try
+      {
+         //Store is already included in store view
+         return super.view();
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to view StoreFrontComponent";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.TAGHELPERERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error, this, "view", e));
+         }
+         throw e;
+      }
+   }   
+}
