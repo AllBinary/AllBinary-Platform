4c71f0431c5ea3597d09dce9b6d6101fb069663c
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:23:27 -0500

    added

---
+++ allbinary_src/ViewsJavaLibrary/src/views/generic/address/billing/ValidationView.java
@@ -0,0 +1,69 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package views.generic.address.billing;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+
+
+import allbinary.logic.visual.transform.info.TransformInfoInterface;
+
+import allbinary.logic.control.validate.ValidationComponentInterface;
+
+import allbinary.data.tables.user.address.billing.BillingAddressesEntity;
+
+public class ValidationView extends BillingAddressesView implements ValidationComponentInterface
+{   
+   public ValidationView(TransformInfoInterface transformInfoInterface)  throws Exception
+   {
+      super(transformInfoInterface);
+   }
+   
+   public Boolean isValid()
+   {
+      BillingAddressesEntity billingAddressesEntity = 
+         new BillingAddressesEntity(         
+         this.getWeblisketSession().getUserName());
+
+      this.streetAddresses = billingAddressesEntity.get();
+      
+      if(this.streetAddresses == null)
+      {
+         return Boolean.FALSE;
+      }
+
+      if(this.streetAddresses.size()<1)
+      {
+         return Boolean.FALSE;
+      }
+      
+      return Boolean.TRUE;
+   }
+
+   public String validationInfo()
+   {
+      return "No Billing Addresses to display.<br />";
+   }
+   
+   public Document toValidationInfoDoc()
+   {
+      return null;      
+   }
+   
+   public Node toValidationInfoNode(Document document)
+   {
+      return null;      
+   }
+}
