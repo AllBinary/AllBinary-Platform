65b9e5942f294de719465bdee68b0943f197f3f5
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 00:59:38 -0500

    added

---
+++ allbinary_src/UserJavaLibrary/src/allbinary/business/advertisement/affiliate/sales/AffiliateSales.java
@@ -0,0 +1,43 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.advertisement.affiliate.sales;
+
+public class AffiliateSales
+{
+   
+   public AffiliateSales()
+   {
+   }
+
+   /*
+   public Boolean isActive()
+   {
+   }
+
+   public Boolean isPaidUp()
+   {
+   }
+    
+   public Boolean isCurrent()
+   {
+   }
+
+*/
+   
+   public Boolean isValid()
+   {
+      return Boolean.TRUE;
+   }   
+   
+}
