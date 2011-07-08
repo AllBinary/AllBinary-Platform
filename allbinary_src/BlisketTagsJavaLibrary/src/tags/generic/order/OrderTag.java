46fdde758c43249e8a923e8436402a2cdb363ac9
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:19:40 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/tags/generic/order/OrderTag.java
@@ -0,0 +1,67 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package tags.generic.order;
+
+import tags.StoreValidationTransformTag;
+
+import abcs.logic.communication.http.request.AbResponseHandler;
+
+import javax.servlet.jsp.JspTagException;
+
+public class OrderTag extends StoreValidationTransformTag
+{
+   //private String orderId;
+   
+   public OrderTag()
+   {
+   }
+   
+   /*
+   public void setOrderId(String value)
+   {
+      this.orderId=value;
+   }*/
+   
+   //         pageContext.getOut().print("Please Review the order below, and click on the Place Order button.<p>");
+   
+   public int doStartTag() throws JspTagException
+   {
+      try
+      {
+         this.setName("Basic Order View");
+         this.setObjectFile("views.generic.order.ValidationView");
+         //this.propertiesHashMap.put(OrderData.ORDERID, this.orderId);
+         
+         if(this.getCommand()!=null)
+         {
+            if (this.getCommand().compareTo(allbinary.globals.GLOBALS.VIEW)==0)
+            {
+               
+            }
+            else
+            {
+               throw new Exception("No Such View Command: " + this.getCommand());
+            }
+            return super.doStartTag();
+         }
+         throw new Exception("Command Null");
+      }
+      catch(Exception e)
+      {
+         AbResponseHandler.sendJspTagRedirect(this.pageContext, e);
+         return SKIP_BODY;
+      }
+   }
+   
+}
