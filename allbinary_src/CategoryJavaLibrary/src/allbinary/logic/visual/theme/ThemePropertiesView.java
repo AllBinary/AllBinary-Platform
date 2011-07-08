dda3ea8cd6cc124982a502c259774c0c49b3974a
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:51:24 -0500

    added

---
+++ allbinary_src/CategoryJavaLibrary/src/allbinary/logic/visual/theme/ThemePropertiesView.java
@@ -0,0 +1,37 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.visual.theme;
+
+import allbinary.data.tree.dom.DomNodeInterface;
+import allbinary.data.tree.dom.ModDomHelper;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+public class ThemePropertiesView implements DomNodeInterface
+{
+   private ThemeInterface themeInterface;
+
+   public ThemePropertiesView(ThemeInterface themeInterface) throws Exception
+   {
+      this.themeInterface = themeInterface;
+   }
+
+   public Node toXmlNode(Document document) throws Exception
+   {
+      Node node = 
+         ModDomHelper.createNameValueNodes(
+            document, ThemeData.getInstance().NAME, this.themeInterface.toHashMap());
+      return node;
+   }
+}
