70d0e4a3a3d232499d5d4df1964ad2a26df664e4
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:54:05 -0500

    added

---
+++ allbinary_src/WorkflowJavaLibrary/src/workflows/template/data/BasicStoreWorkFlow.java
@@ -0,0 +1,35 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package workflows.template.data;
+
+import java.util.HashMap;
+
+import javax.servlet.jsp.PageContext;
+
+import allbinary.business.context.modules.storefront.StoreFrontData;
+
+import allbinary.logic.control.workflow.StoreWorkFlowInterface;
+
+public class BasicStoreWorkFlow extends BasicWorkFlow implements StoreWorkFlowInterface
+{
+   public BasicStoreWorkFlow(HashMap propertiesHashMap, PageContext pageContext) throws Exception
+   {
+      super(propertiesHashMap, pageContext);
+   }
+
+   public String getStoreName() throws Exception
+   {
+      return (String) this.getPropertiesHashMap().get(StoreFrontData.getInstance().NAME);
+   }
+}
