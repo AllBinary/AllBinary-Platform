740fe007d174f7179f3c64cbe76e7ddcc3b6a966
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:05:45 -0500

    added

---
+++ allbinary_src/BlisketTagHelpersJavaLibrary/src/admin/taghelpers/ShippingHelper.java
@@ -0,0 +1,100 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.taghelpers;
+
+import abcs.logic.communication.log.LogFactory;
+import java.util.*;
+
+import javax.servlet.jsp.PageContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+import abcs.logic.communication.log.LogUtil;
+
+import allbinary.business.user.commerce.inventory.order.OrderInterface;
+
+import allbinary.business.user.commerce.shipping.ShippingMethodData;
+
+import allbinary.logic.communication.http.request.session.WeblisketSession;
+
+import allbinary.business.context.modules.storefront.StoreFrontData;
+import allbinary.business.context.modules.storefront.StoreFrontFactory;
+import allbinary.business.context.modules.storefront.StoreFrontInterface;
+
+public class ShippingHelper
+    implements TagHelperInterface
+{
+   private WeblisketSession weblisketSession;
+   
+   private StoreFrontInterface storeFrontInterface;   
+   private HashMap propertiesHashMap;
+   private PageContext pageContext;
+   
+   private HttpServletRequest request;
+      
+   private String shippingType;
+   
+   public ShippingHelper(HashMap hashMap, PageContext pageContext)
+   {
+      this.propertiesHashMap = propertiesHashMap;
+      this.pageContext = pageContext;
+
+      this.request = (HttpServletRequest) pageContext.getRequest();
+      
+      String storeName = (String) hashMap.get(StoreFrontData.getInstance().NAME);
+      
+      if(storeName!=null)
+      {
+         this.storeFrontInterface = StoreFrontFactory.getInstance(storeName);
+      }
+
+      this.weblisketSession = 
+         new WeblisketSession(hashMap, pageContext);
+      
+      this.getFormData();
+   }
+   
+   private void getFormData()
+   {      
+      this.shippingType
+         = request.getParameter(ShippingMethodData.NAME);
+   }      
+         
+   public String setShippingType()
+   {
+      try
+      {
+         String success = "Successfully Set Shipping Type";
+         OrderInterface order = this.weblisketSession.getOrder();
+         order.setShippingMethod(shippingType);
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {            
+            LogUtil.put(LogFactory.getInstance(success,this,"setShippingType()"));
+         }
+         return success;
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to view Shipping Type";
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"setShippingType()",e));
+         }
+         return error;
+      }
+   }
+   
+}
