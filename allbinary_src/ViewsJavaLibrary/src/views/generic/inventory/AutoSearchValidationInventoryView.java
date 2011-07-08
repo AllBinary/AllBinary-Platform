0c02d5eb81c01e652ad6984af4036293d76fc15b
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:26:15 -0500

    added

---
+++ allbinary_src/ViewsJavaLibrary/src/views/generic/inventory/AutoSearchValidationInventoryView.java
@@ -0,0 +1,56 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package views.generic.inventory;
+
+import java.util.*;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+
+import allbinary.logic.control.search.SearchRequest;
+
+
+
+
+public class AutoSearchValidationInventoryView 
+  extends InventorySearch
+{
+   
+   public AutoSearchValidationInventoryView(SearchRequest searchRequest)
+   {
+      super(searchRequest);
+   }
+
+   public Boolean isValid()
+   {
+      return Boolean.TRUE;
+   }
+   
+   public Document toValidationInfoDoc()
+   {
+      return null;
+   }
+   
+   public Node toValidationInfoNode(Document document)
+   {
+      return null;
+   }
+   
+   public String validationInfo()
+   {
+      return null;
+   }
+}
