317483c3981480d23ccfa2dbaa463f912b32b646
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:12:36 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/admin/tags/CustomItemsTag.java
@@ -0,0 +1,31 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.tags;
+
+import javax.servlet.jsp.*;
+
+
+
+import admin.taghelpers.CustomItemsHelperFactory;
+import admin.taghelpers.CustomItemsRequestHelperFactory;
+
+
+public class CustomItemsTag extends TableTag
+{
+   public CustomItemsTag()
+   {
+      this.setTagHelperFactory(new CustomItemsHelperFactory());
+      this.setTagRequestHelperFactory(new CustomItemsRequestHelperFactory());
+   }
+}
