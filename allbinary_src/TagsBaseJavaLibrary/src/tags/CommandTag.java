1ca22a1a5b520079e3ced287f4f28f2932d34834
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 00:49:48 -0500

    added

---
+++ allbinary_src/TagsBaseJavaLibrary/src/tags/CommandTag.java
@@ -0,0 +1,35 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package tags;
+
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class CommandTag extends TagSupport
+{
+   private String command;
+   
+   public CommandTag()
+   {
+   }
+   
+   public void setCommand(String command)
+   {
+      this.command=command;
+   }
+   
+   public String getCommand()
+   {
+      return this.command;
+   }
+}
