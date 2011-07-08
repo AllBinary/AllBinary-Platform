3374c972e014f7b2d314ce6417924ae6096559fc
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:22:42 -0500

    added

---
+++ allbinary_src/ViewsJavaLibrary/src/views/generic/address/billing/DeleteValidationView.java
@@ -0,0 +1,78 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package views.generic.address.billing;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+
+import allbinary.business.user.address.StreetAddressData;
+
+import allbinary.logic.visual.transform.info.TransformInfoHttpInterface;
+import allbinary.logic.visual.transform.info.TransformInfoInterface;
+
+import allbinary.logic.control.validate.ValidationComponentInterface;
+
+import allbinary.data.tables.user.address.billing.BillingAddressesEntity;
+
+
+public class DeleteValidationView extends BillingAddressView implements ValidationComponentInterface
+{
+   private String value;
+
+   public DeleteValidationView(TransformInfoInterface transformInfoInterface) throws Exception
+   {
+      super(transformInfoInterface);
+      
+      TransformInfoHttpInterface httpTransformInfoInterface = 
+         (TransformInfoHttpInterface) this.getTransformInfoInterface();
+      
+      this.value = httpTransformInfoInterface.getPageContext().getRequest().getParameter(StreetAddressData.ID);
+   }
+   
+   public Boolean isValid()
+   {
+      if(this.getWeblisketSession().getUserName()!=null)
+      {
+         return Boolean.FALSE;
+      }
+      
+      BillingAddressesEntity billingAddressesEntity =
+      new BillingAddressesEntity(this.getWeblisketSession().getUserName());
+      
+      this.streetAddress = billingAddressesEntity.get(new Integer(value));
+      
+      if(this.streetAddress==null)
+      {
+         return Boolean.FALSE;
+      }
+
+      return this.streetAddress.isValid();
+   }
+   
+   public String validationInfo()
+   {
+      return this.streetAddress.validationInfo();
+   }
+   
+   public Document toValidationInfoDoc()
+   {
+      return null;
+   }
+   
+   public Node toValidationInfoNode(Document document)
+   {
+      return null;
+   }
+}
