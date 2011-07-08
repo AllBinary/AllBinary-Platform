136a72e0fc57cc72a590576deb3bcbeceb6ceeb6
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 00:10:52 -0500

    added

---
+++ allbinary_src/content/ContentNoneJavaLibrary/src/allbinary/content/NotificationUtil.java
@@ -0,0 +1,32 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.content;
+
+import javax.microedition.lcdui.Command;
+
+import allbinary.data.resource.ResourceUtil;
+
+public class NotificationUtil
+{
+    private static final NotificationUtil SINGLETON = new NotificationUtil();
+        
+    public static NotificationUtil getInstance()
+    {
+        return SINGLETON;
+    }
+    
+    public void notify(Command command, String resource, String message)
+    {
+    }
+}
