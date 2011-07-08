67c368f633a49cf3be781ea32a595c7f41a6b323
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:20:41 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/tags/transform/info/TransformInfoTableTag.java
@@ -0,0 +1,27 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package tags.transform.info;
+
+import taghelpers.transform.info.TransformInfoHelperFactory;
+
+import admin.tags.TableTag;
+
+
+public class TransformInfoTableTag extends TableTag
+{
+   public TransformInfoTableTag()
+   {
+      this.setTagHelperFactory(new TransformInfoHelperFactory());
+   }
+}
