db5cb3a0c21d34786bfb0fba83d2cfba729dcc15
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 03:56:50 -0500

    added

---
+++ allbinary_src/hardware/PersonalComputerHardwareJavaLibrary/src/abcs/logic/system/hardware/components/linux/Bridge.java
@@ -0,0 +1,38 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package abcs.logic.system.hardware.components.linux;
+
+import abcs.logic.system.hardware.components.interfaces.HardwareComponentInterface;
+import abcs.logic.system.hardware.components.interfaces.BridgeInterface;
+        
+public class Bridge implements HardwareComponentInterface, BridgeInterface
+{
+    private String data;
+    
+    public Bridge(String data)
+    {
+        this.data = data;
+    }
+        
+    public boolean compareTo(HardwareComponentInterface componentInterface)
+    {
+        return true;
+    }
+    
+    public String toString()
+    {
+        return data;
+    }
+    
+}
