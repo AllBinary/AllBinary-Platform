b9f3d5394830ad766ad0cc2de24c7a30bdec3f66
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 22:59:01 -0500

    added

---
+++ allbinary_src/BlisketTagHelperFactoriesJavaLibrary/src/taghelpers/StoreTagWorkFlowHelperFactory.java
@@ -0,0 +1,54 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package taghelpers;
+
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import java.util.HashMap;
+
+import javax.servlet.jsp.PageContext;
+
+
+
+
+import abcs.logic.system.security.licensing.LicensingException;
+
+import admin.taghelpers.TagHelperFactoryInterface;
+
+public class StoreTagWorkFlowHelperFactory implements TagHelperFactoryInterface
+{
+   public StoreTagWorkFlowHelperFactory()
+   {
+   }
+
+   public Object getInstance(
+      HashMap hashMap, PageContext pageContext)
+      throws Exception, LicensingException
+   {
+        try
+        {
+            return new StoreTagWorkFlowHelper(hashMap, pageContext);
+        } catch (Exception e)
+        {
+            if (abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(
+                abcs.logic.communication.log.config.type.LogConfigType.TAGHELPERFACTORYERROR))
+            {
+                String error = "Failed To Get Instance";
+
+                LogUtil.put(LogFactory.getInstance(error, this, "getInstance(HashMap, PageContext)", e));
+            }
+            return null;
+        }
+   }   
+}
