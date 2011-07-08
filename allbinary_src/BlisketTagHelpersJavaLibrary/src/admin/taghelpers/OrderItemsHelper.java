7e0f4a179f23fc13e2944574ad33b6ecaf9316c9
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:04:09 -0500

    added

---
+++ allbinary_src/BlisketTagHelpersJavaLibrary/src/admin/taghelpers/OrderItemsHelper.java
@@ -0,0 +1,129 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.taghelpers;
+
+import java.util.HashMap;
+
+import javax.servlet.jsp.PageContext;
+
+import abcs.business.installer.Portion;
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.data.tables.user.commerce.inventory.order.OrderItemsEntityFactory;
+
+public class OrderItemsHelper implements BasicTableInterface
+{
+	private final Portion portion;
+	
+   public OrderItemsHelper(HashMap hashMap, PageContext pageContext)
+   {
+	   this.portion = new Portion(hashMap);
+   }
+      
+   public String drop()
+   {      
+      try
+      {
+         String success = OrderItemsEntityFactory.getInstance().dropTable();         
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {
+            LogUtil.put(LogFactory.getInstance(success,this,"drop()"));
+         }
+         return success;
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to drop order history table";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"drop()",e));
+         }
+         return error;
+      }
+   }
+   
+   public String create()
+   {
+      try
+      {
+         String success = OrderItemsEntityFactory.getInstance().createTable();        
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {
+            LogUtil.put(LogFactory.getInstance(success,this,"create()"));
+         }
+         return success;
+      }
+      catch(Exception e)
+      {
+         String error="Failed to create new order history table";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"create()",e));
+         }
+         return error;         
+      }
+   }
+
+   public String restore()
+   {
+      try
+      {
+         String success = "Restore Successful";         
+         String result = OrderItemsEntityFactory.getInstance().restoreTable(this.portion);
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {
+            LogUtil.put(LogFactory.getInstance(success,this,"restore()"));
+         }
+         return result;
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to restore backup";
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"restore()",e));
+         }
+         return error;
+      }
+   }
+   
+   public String backup()
+   {
+      try
+      {
+         String success = "Restore Successful";
+         String result = OrderItemsEntityFactory.getInstance().backupTable();         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {
+            LogUtil.put(LogFactory.getInstance(success,this,"backup()"));
+         }
+         return result;
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to make backup";
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"backup()",e));
+         }
+         return error;
+      }
+   }
+  
+}
