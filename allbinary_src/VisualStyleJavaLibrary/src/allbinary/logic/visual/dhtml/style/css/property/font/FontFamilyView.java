7d1ae7bb7a1f610be079c91d8937433bf422e20a
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:37:10 -0500

    added

---
+++ allbinary_src/VisualStyleJavaLibrary/src/allbinary/logic/visual/dhtml/style/css/property/font/FontFamilyView.java
@@ -0,0 +1,38 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.visual.dhtml.style.css.property.font;
+
+import allbinary.logic.visual.dhtml.style.css.property.CssPropertyView;
+import org.w3c.dom.Node;
+
+public class FontFamilyView extends CssPropertyView
+{
+   public FontFamilyView()
+   {
+      super(FontFamilyData.getInstance().VALUE);
+   }
+
+   public FontFamilyView(Node node) throws Exception
+   {
+      super(node);
+   }
+
+   /*
+   public FontFamilyView(HashMap hashMap)
+   {
+      super(hashMap);
+   }
+    **/
+}
+    
