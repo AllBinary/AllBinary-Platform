80802c96e2d12c47a2ee078aa57dca06c557ba24
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:08:15 -0500

    added

---
+++ allbinary_src/BlisketTagHelpersJavaLibrary/src/taghelpers/transform/info/TransformInfoHelper.java
@@ -0,0 +1,117 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package taghelpers.transform.info;
+
+import java.util.HashMap;
+
+import javax.servlet.jsp.PageContext;
+
+import abcs.business.installer.Portion;
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import admin.taghelpers.BasicTableInterface;
+import allbinary.data.tables.transform.info.TransformInfoEntityBuilder;
+import allbinary.data.tables.transform.info.TransformInfoEntityFactory;
+
+public class TransformInfoHelper implements BasicTableInterface
+{
+    private final Portion portion;
+
+   public TransformInfoHelper(HashMap hashMap, PageContext pageContext)
+   {
+	   this.portion = new Portion(hashMap);
+   }
+   
+   public String drop()
+   {
+      try
+      {
+         return TransformInfoEntityBuilder.getInstance().dropTable();
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to drop view info table";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"drop()",e));
+         }
+         return error;  
+      }
+   }
+   
+   public String create()
+   {
+      try
+      {
+         return TransformInfoEntityBuilder.getInstance().createTable();
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to create view info table";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"create()",e));
+         }
+         return error;  
+      }
+   }
+      
+   public String restore()
+   {
+      try
+      {
+         String success = "Restore Successful";
+         String result = TransformInfoEntityBuilder.getInstance().restoreTable(this.portion);
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {
+            LogUtil.put(LogFactory.getInstance(success,this,"restore()"));
+         }
+         return result;
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to restore backup";
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"restore()",e));
+         }
+         return error;
+      }
+   }
+   
+   public String backup()
+   {
+      try
+      {
+         String success = "Restore Successful";
+         String result = TransformInfoEntityBuilder.getInstance().backupTable();         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {
+            LogUtil.put(LogFactory.getInstance(success,this,"backup()"));
+         }
+         return result;
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to make backup";
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"backup()",e));
+         }
+         return error;
+      }
+   }
+}
