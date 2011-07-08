3b34ad1fc8ff4ecdf620952c1e81d4d62d789af3
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:18:18 -0500

    added

---
+++ allbinary_src/ViewsJavaLibrary/src/views/business/context/modules/storefront/customizer/includes/globals/GlobalsView.java
@@ -0,0 +1,81 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package views.business.context.modules.storefront.customizer.includes.globals;
+
+import abcs.logic.communication.log.LogFactory;
+import org.w3c.dom.Node;
+import org.w3c.dom.Document;
+
+import allbinary.data.tree.dom.DomNodeInterface;
+
+import allbinary.logic.visual.transform.info.TransformInfoInterface;
+
+import allbinary.logic.visual.transform.template.customizer.includes.globals.GlobalsValidation;
+
+import abcs.logic.communication.log.LogUtil;
+
+import views.business.context.modules.storefront.HttpStoreComponentView;
+
+public class GlobalsView extends HttpStoreComponentView implements DomNodeInterface
+{
+   protected GlobalsValidation globalsValidation;
+   
+   public GlobalsView(TransformInfoInterface transformInfoInterface) throws Exception
+   {
+      super(transformInfoInterface);
+      this.globalsValidation = 
+         new GlobalsValidation(this.getWeblisketSession().getStoreName());
+   }
+   
+   public void addDomNodeInterfaces()
+   {
+      this.addDomNodeInterface((DomNodeInterface) this.globalsValidation);
+   }
+   
+   public Node toXmlNode(Document document) throws Exception
+   {
+      try
+      {
+         return this.globalsValidation.toXmlNode(document);
+      }
+      catch(Exception e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(
+         abcs.logic.communication.log.config.type.LogConfigType.XSLLOGGINGERROR))
+         {
+            LogUtil.put(LogFactory.getInstance("Command Failed",this,"toXmlNode",e));
+         }
+         throw e;
+      }
+   }
+   
+   public String view() throws Exception
+   {
+      try
+      {
+         this.addDomNodeInterfaces();
+         return super.view();
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to view Globals";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(
+            abcs.logic.communication.log.config.type.LogConfigType.VIEWERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"view()",e));
+         }
+         throw e;
+      }
+   }
+}
