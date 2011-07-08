2ed53583c100c35bded7fed4d66a845da87f3009
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:11:58 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/admin/tags/BasicOptionItemsTag.java
@@ -0,0 +1,35 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.tags;
+
+import java.util.HashMap;
+
+import javax.servlet.jsp.*;
+
+
+
+import admin.taghelpers.BasicOptionItemsHelperFactory;
+import admin.taghelpers.BasicOptionItemsRequestHelperFactory;
+
+
+public class BasicOptionItemsTag extends TableTag
+{
+   private HashMap propertiesHashMap;
+   
+   public BasicOptionItemsTag()
+   {
+      this.setTagHelperFactory(new BasicOptionItemsHelperFactory());
+      this.setTagRequestHelperFactory(new BasicOptionItemsRequestHelperFactory());
+   }
+}
