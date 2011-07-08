f3279cde660903c357b01db6a31c6cf5a8570551
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:17:17 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/tags/admin/payment/gateway/ViewTag.java
@@ -0,0 +1,54 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package tags.admin.payment.gateway;
+
+import tags.StoreValidationTransformTag;
+
+import abcs.logic.communication.http.request.AbResponseHandler;
+
+import javax.servlet.jsp.JspTagException;
+
+public class ViewTag extends StoreValidationTransformTag
+{
+   public ViewTag()
+   {
+   }
+   
+   public int doStartTag() throws JspTagException
+   {
+      try
+      {
+         if(this.getCommand()!=null)
+         {
+            if (this.getCommand().compareTo(allbinary.globals.GLOBALS.VIEW)==0)
+            {
+               this.setName("Validation Gateway View");
+               this.setObjectFile("views.admin.payment.gateway.GatewayValidationView");
+            }
+            else
+            {
+               throw new Exception("No Such View Command: " + this.getCommand());
+            }
+            
+            return super.doStartTag();
+         }
+         throw new Exception("Command Null");
+      }
+      catch(Exception e)
+      {
+         AbResponseHandler.sendJspTagRedirect(this.pageContext, e);
+         return SKIP_BODY;
+      }
+   }
+}
