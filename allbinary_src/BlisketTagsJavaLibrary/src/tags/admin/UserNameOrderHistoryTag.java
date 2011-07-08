d944296975a46a37e186c2a31090b16b54ded04c
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:17:57 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/tags/admin/UserNameOrderHistoryTag.java
@@ -0,0 +1,114 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package tags.admin;
+
+import tags.StoreValidationTransformTag;
+
+import abcs.logic.communication.http.request.AbResponseHandler;
+
+import javax.servlet.jsp.JspTagException;
+
+public class UserNameOrderHistoryTag extends StoreValidationTransformTag
+{
+   //private String id;
+   private String status;
+   //private String groupId;
+   /*
+   private String shipped;
+   private String partiallyShipped;
+   private String processing;
+   private String preprocessing;
+   private String cancelled;
+    */
+   
+   public UserNameOrderHistoryTag()
+   {
+      super();
+   }
+   
+/*
+   public void setId(String id)
+   {
+      this.id=id;
+   }
+ */
+   
+   public void setStatus(String value)
+   {
+      this.status=value;
+   }
+   
+  /*
+   public void setGroupId(String value)
+   {
+      this.groupId=value;
+   }
+   */
+   /*
+   public void setShipped(String value)
+   {
+      this.shipped=value;
+   }
+    
+   public void setPartiallyShipped(String value)
+   {
+      this.partiallyShipped=value;
+   }
+    
+   public void setPreprocessing(String value)
+   {
+      this.preprocessing=value;
+   }
+    
+   public void setProcessing(String value)
+   {
+      this.processing=value;
+   }
+    
+   public void setCancelled(String value)
+   {
+      this.cancelled=value;
+   }
+    */
+   
+   public int doStartTag() throws JspTagException
+   {
+      try
+      {
+         this.setName("Range Order History View");
+         this.setObjectFile("views.admin.orderhistory.UserNameOrderHistoryView");
+         
+         if(this.getCommand()!=null)
+         {
+            if (this.getCommand().compareTo(allbinary.globals.GLOBALS.VIEW)==0)
+            {
+               
+            }
+            else
+            {
+               throw new Exception("No Such View Command: " + this.getCommand());
+            }
+            return super.doStartTag();
+         }
+         throw new Exception("Command Null");
+      }
+      catch(Exception e)
+      {
+         AbResponseHandler.sendJspTagRedirect(this.pageContext, e);
+         return SKIP_BODY;
+      }
+   }
+   
+}
+
