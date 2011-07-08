5342c9579cf8bf88e57d545a6c717078cfe2eeac
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 00:49:03 -0500

    added

---
+++ allbinary_src/TagsBaseJavaLibrary/src/admin/taghelpers/ModifyTableInterface.java
@@ -0,0 +1,24 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.taghelpers;
+
+
+public interface ModifyTableInterface
+{  
+   String insert() throws Exception;
+   
+   String delete() throws Exception;
+
+   String update() throws Exception;   
+}
