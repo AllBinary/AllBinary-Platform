8cbc10e12541254a16efcbfa8bf6586e3de25e7d
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:14:17 -0500

    added

---
+++ allbinary_src/ViewsJavaLibrary/src/views/admin/user/NewUserView.java
@@ -0,0 +1,86 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package views.admin.user;
+
+import abcs.logic.communication.log.LogFactory;
+import org.w3c.dom.Node;
+import org.w3c.dom.Document;
+
+import abcs.logic.communication.log.LogUtil;
+
+import allbinary.business.user.NewUserFactory;
+
+import allbinary.logic.visual.transform.info.TransformInfoInterface;
+
+import allbinary.logic.control.validate.ValidationComponentInterface;
+
+
+public class NewUserView extends UserView 
+   implements ValidationComponentInterface
+{
+   public NewUserView(TransformInfoInterface transformInfoInterface) 
+      throws Exception
+   {
+      super(transformInfoInterface);
+      
+      this.user = NewUserFactory.getInstance();
+   }
+   
+   public Boolean isValid()
+   {
+      try
+      {
+         return Boolean.TRUE;
+      }
+      catch(Exception e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.VIEWERROR))
+         {
+            LogUtil.put(LogFactory.getInstance("Failed to validate",this,"isValid()",e));
+         }
+         return Boolean.FALSE;
+      }
+   }
+   
+   public String validationInfo()
+   {
+      try
+      {
+         StringBuffer stringBuffer = new StringBuffer();
+
+         stringBuffer.append("Please Contact Administrator");
+         
+         return stringBuffer.toString();
+      }
+      catch(Exception e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.VIEWERROR))
+         {
+            LogUtil.put(LogFactory.getInstance("Failed to generate validation error info",this,"validationInfo()",e));
+         }
+         return "Error Getting Validation Info";
+      }
+   }
+   
+   public Document toValidationInfoDoc()
+   {
+      return null;
+   }
+   
+   public Node toValidationInfoNode(Document document)
+   {
+      return null;
+   }
+   
+}
