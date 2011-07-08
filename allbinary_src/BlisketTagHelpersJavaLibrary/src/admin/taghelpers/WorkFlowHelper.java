e1418d672faf0de2336e15deea04c83ebb754d74
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:07:08 -0500

    added

---
+++ allbinary_src/BlisketTagHelpersJavaLibrary/src/admin/taghelpers/WorkFlowHelper.java
@@ -0,0 +1,121 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.taghelpers;
+
+import java.util.HashMap;
+
+import javax.servlet.jsp.PageContext;
+
+import abcs.business.installer.Portion;
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.data.tables.workflow.WorkFlowEntityFactory;
+
+public class WorkFlowHelper implements BasicTableInterface
+{
+   private final HashMap hashMap;
+   private final PageContext pageContext;
+   
+   private final Portion portion;
+   
+   public WorkFlowHelper(HashMap hashMap, PageContext pageContext) throws Exception
+   {
+      this.hashMap = hashMap;
+      this.pageContext = pageContext;
+      
+      this.portion = new Portion(hashMap);
+   }
+   
+   public String drop()
+   {
+      try
+      {
+         return WorkFlowEntityFactory.getInstance().dropTable();
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to drop view info table";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"drop()",e));
+         }
+         return error;
+      }
+   }
+   
+   public String create()
+   {
+      try
+      {
+         return WorkFlowEntityFactory.getInstance().createTable();
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to create workflow table";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"create()",e));
+         }
+         return error;
+      }
+   }
+   
+   public String restore()
+   {
+      try
+      {
+         String success = "Restore Successful";
+         String result = WorkFlowEntityFactory.getInstance().restoreTable(this.portion);
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {
+            LogUtil.put(LogFactory.getInstance(success,this,"restore()"));
+         }
+         return result;
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to restore backup";
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"restore()",e));
+         }
+         return error;
+      }
+   }
+   
+   public String backup()
+   {
+      try
+      {
+         String success = "Restore Successful";
+         String result = WorkFlowEntityFactory.getInstance().backupTable();
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {
+            LogUtil.put(LogFactory.getInstance(success,this,"backup()"));
+         }
+         return result;
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to make backup";
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"backup()",e));
+         }
+         return error;
+      }
+   }
+}
