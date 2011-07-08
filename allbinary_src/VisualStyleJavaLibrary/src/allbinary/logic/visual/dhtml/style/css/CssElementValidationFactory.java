9c99e739966796bedd0bda08827e2b6247e963ed
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:36:09 -0500

    added

---
+++ allbinary_src/VisualStyleJavaLibrary/src/allbinary/logic/visual/dhtml/style/css/CssElementValidationFactory.java
@@ -0,0 +1,37 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.visual.dhtml.style.css;
+
+import allbinary.logic.control.validate.ValidationInterface;
+import org.w3c.dom.Node;
+
+public class CssElementValidationFactory
+{
+   private CssElementValidationFactory()
+   {
+   }
+   
+   //Node is CssElementData.NAME
+   public static ValidationInterface getInstance(Node node) throws Exception
+   {
+      return (ValidationInterface) new CssElementValidation(node);
+   }
+
+   /*
+   public static ValidationInterface getInstance(HashMap hashMap)
+   {
+      return (ValidationInterface) new CssElementValidation(hashMap);
+   }
+    **/
+}
