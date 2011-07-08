39bef69bc041ba12f6bae9b5addf87e4b20db247
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:20:03 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/tags/generic/user/UsersTag.java
@@ -0,0 +1,73 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package tags.generic.user;
+
+import javax.servlet.jsp.JspTagException;
+
+
+import abcs.logic.communication.http.request.AbResponseHandler;
+
+
+import allbinary.business.user.role.UserRoleData;
+
+
+import tags.StoreValidationTransformTag;
+
+
+public class UsersTag extends StoreValidationTransformTag
+{
+   private String role;
+   
+   public UsersTag()
+   {
+   }
+   
+   public void setRole(String value)
+   {
+      this.role=value;
+   }
+
+   public int doStartTag() throws JspTagException
+   {
+      try
+      {
+         this.setName("Generic Users View");
+         this.setObjectFile("views.generic.user.UsersValidationView");
+         
+         if(this.getCommand()!=null)
+         {
+            this.getPropertiesHashMap().put(UserRoleData.NAME,this.role);
+            
+            if (this.getCommand().compareTo(allbinary.globals.GLOBALS.VIEW)==0)
+            {
+               
+            }
+            else
+            {
+               throw new Exception("No Such View Command: " + this.getCommand());
+            }
+            return super.doStartTag();
+            
+         }
+         throw new Exception("Command Null");
+         
+      }
+      catch(Exception e)
+      {
+         AbResponseHandler.sendJspTagRedirect(this.pageContext, e);
+         return SKIP_BODY;
+      }
+   }
+   
+}
