c75665f16d833226a6274744305866be6a356bc6
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:39:13 -0500

    added

---
+++ allbinary_src/VisualTransformBaseJavaLibrary/src/allbinary/logic/visual/transform/data/TransformDocumentFactory.java
@@ -0,0 +1,27 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.visual.transform.data;
+
+public class TransformDocumentFactory
+{
+   private TransformDocumentFactory()
+   {
+   }
+   
+   public static TransformDocumentInterface getInstance() 
+      throws Exception
+   {
+      return (TransformDocumentInterface) new TransformDocument();
+   }
+}
