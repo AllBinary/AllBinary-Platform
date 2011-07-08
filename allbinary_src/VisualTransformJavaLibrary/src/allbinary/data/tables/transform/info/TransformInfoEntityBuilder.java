ba44904269277d49ddf7b4db7084b09e7cdea190
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:44:26 -0500

    added

---
+++ allbinary_src/VisualTransformJavaLibrary/src/allbinary/data/tables/transform/info/TransformInfoEntityBuilder.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.data.tables.transform.info;
+
+import allbinary.logic.visual.transform.info.TransformInfoHttpStoreFactory;
+import allbinary.logic.visual.transform.info.objectConfig.TransformInfoObjectConfigAndManipulatorFactory;
+import allbinary.logic.visual.transform.info.objectConfig.generator.TransformInfoObjectConfigGeneratorFactory;
+
+public class TransformInfoEntityBuilder {
+
+	public static TransformInfoEntity getInstance()
+	{
+		return TransformInfoEntityFactory.getInstance(
+                TransformInfoObjectConfigGeneratorFactory.getInstance(),
+                TransformInfoObjectConfigAndManipulatorFactory.getInstance(),
+                TransformInfoHttpStoreFactory.getInstance()
+                );
+	}
+}
