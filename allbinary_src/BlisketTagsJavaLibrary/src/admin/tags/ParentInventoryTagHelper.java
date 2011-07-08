764b742b20f1dee4f6bb7e5f6457a85c4f5d1c8b
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:13:44 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/admin/tags/ParentInventoryTagHelper.java
@@ -0,0 +1,57 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.tags;
+
+import javax.servlet.jsp.tagext.Tag;
+
+/**
+ *
+ * @author user
+ */
+public class ParentInventoryTagHelper
+{
+    private static final ParentInventoryTagHelper instance =
+        new ParentInventoryTagHelper();
+
+    /**
+     * @return the instance
+     */
+    public static ParentInventoryTagHelper getInstance()
+    {
+        return instance;
+    }
+
+    public void isValid(Object tagClass, Tag parentTag)
+        throws Exception
+    {
+        if (parentTag == null)
+        {
+            throw new Exception("Must have parent tag.");
+            //tags.admin.inventory.InventoryTag
+            //tags.admin.inventory.DownloadItemTag
+        } else if (!(parentTag instanceof tags.HelperTag))
+        {
+            StringBuffer stringBuffer = new StringBuffer();
+
+            //tagClass
+            stringBuffer.append("Must have at least a ");
+            stringBuffer.append("tags.HelperTag");
+            stringBuffer.append(" as parent");
+            stringBuffer.append("and not: ");
+            stringBuffer.append(parentTag.getClass().getName());
+
+            throw new Exception(stringBuffer.toString());
+        }
+    }
+}
