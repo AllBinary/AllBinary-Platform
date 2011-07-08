79760179117df34f97dbb8031492bc81cb57bbd2
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 22:54:11 -0500

    added

---
+++ allbinary_src/BlisketTagHelperFactoriesJavaLibrary/src/admin/taghelpers/CustomItemsHelperFactory.java
@@ -0,0 +1,51 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.taghelpers;
+
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import java.util.HashMap;
+
+import javax.servlet.jsp.PageContext;
+
+
+import abcs.logic.system.security.licensing.LicensingException;
+
+
+public class CustomItemsHelperFactory implements TagHelperFactoryInterface
+{
+   public CustomItemsHelperFactory()
+   {
+   }
+   
+   public Object getInstance(
+      HashMap hashMap, PageContext pageContext) 
+      throws LicensingException
+   {
+        try
+        {
+            return new CustomItemsHelper(hashMap, pageContext);
+        } catch (Exception e)
+        {
+            if (abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(
+                abcs.logic.communication.log.config.type.LogConfigType.TAGHELPERFACTORYERROR))
+            {
+                String error = "Failed To Get Instance";
+
+                LogUtil.put(LogFactory.getInstance(error, this, "getInstance(HashMap, PageContext)", e));
+            }
+            return null;
+        }
+   }  
+}
