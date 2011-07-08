d4b4c84d5464489edcdee4da516a3d7640107efe
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 00:54:16 -0500

    added

---
+++ allbinary_src/UserCommerceJavaLibrary/src/allbinary/business/user/commerce/money/payment/types/TenderType.java
@@ -0,0 +1,41 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.commerce.money.payment.types;
+
+public class TenderType
+{      
+   private String name;
+   
+   public TenderType(String name)
+   {
+      this.name = name;
+   }
+
+   public boolean isEqual(TenderType tenderType)
+   {
+      if(this.toString().compareTo(tenderType.toString())==0)
+      {
+         return true;
+      }
+      else
+      {
+         return false;
+      }
+   }
+   
+   public String toString()
+   {
+      return name;
+   }   
+}
