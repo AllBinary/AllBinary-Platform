3ea8950dcdbe4bff476147fc76aca9a24cb42668
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:35:53 -0500

    added

---
+++ allbinary_src/VisualStyleJavaLibrary/src/allbinary/logic/visual/dhtml/style/css/CssElementsValidationFactory.java
@@ -0,0 +1,51 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.visual.dhtml.style.css;
+
+import allbinary.logic.control.validate.ValidationInterface;
+import org.w3c.dom.Node;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+public class CssElementsValidationFactory
+{
+   private CssElementsValidationFactory()
+   {
+   }
+   
+   //Nodes with CssElementData.NAME
+   public static Vector getInstance(Vector cssElementStyleNodeList) throws Exception
+   //NodeList cssElementStyleNodeList
+   {
+      Vector styles = new Vector();
+      
+      for(int index = 0; index < cssElementStyleNodeList.size(); index++)
+      {
+         Node cssElementStyleNode = (Node) cssElementStyleNodeList.get(index);
+
+         ValidationInterface cssValidationInterface = (ValidationInterface)
+           CssElementValidationFactory.getInstance(cssElementStyleNode);
+
+         styles.add(cssValidationInterface);
+      }
+      return styles;
+   }
+   
+   public static Vector getInstance(HashMap hashMap)
+   {
+      Vector styles = new Vector();
+      return styles;
+   }
+}
