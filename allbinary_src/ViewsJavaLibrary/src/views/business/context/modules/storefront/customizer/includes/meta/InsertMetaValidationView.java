27e57bd19d6a5315b2edcc53b295c0dbd339091a
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:18:48 -0500

    added

---
+++ allbinary_src/ViewsJavaLibrary/src/views/business/context/modules/storefront/customizer/includes/meta/InsertMetaValidationView.java
@@ -0,0 +1,92 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package views.business.context.modules.storefront.customizer.includes.meta;
+        
+import abcs.logic.communication.log.LogFactory;
+import org.w3c.dom.Node;
+import org.w3c.dom.Document;
+
+
+import abcs.logic.communication.log.LogUtil;
+
+import allbinary.logic.visual.transform.info.TransformInfoInterface;
+
+import allbinary.data.tree.dom.DomNodeInterface;
+
+import allbinary.logic.control.validate.ValidationComponentInterface;
+
+
+import views.business.context.modules.storefront.customizer.CustomizerUtil;
+
+public class InsertMetaValidationView extends MetaView implements ValidationComponentInterface
+{
+   public InsertMetaValidationView(TransformInfoInterface transformInfoInterface) throws Exception
+   {
+      super(transformInfoInterface);
+   }
+
+    public Document toXmlDoc() throws Exception
+    {
+        return null;
+    }
+
+   public Boolean isValid()
+   {
+      try
+      {
+         Boolean isValid = Boolean.TRUE;
+         
+         //Insert XML into the view specified by the Object Config for this view
+         CustomizerUtil.insert(this.getTransformInfoInterface(),(DomNodeInterface) this);
+         
+         return isValid;
+      }
+      catch(Exception e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.VIEWERROR))
+         {
+            LogUtil.put(LogFactory.getInstance("Failed to validate",this,"isValid()",e));
+         }
+         return Boolean.FALSE;
+      }
+   }
+   
+   public String validationInfo()
+   {
+      try
+      {
+         StringBuffer stringBuffer = new StringBuffer();
+         
+         return stringBuffer.toString();
+      }
+      catch(Exception e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.VIEWERROR))
+         {
+            LogUtil.put(LogFactory.getInstance("Failed to generate validation error info",this,"validationInfo()",e));
+         }
+         return "Error Getting Validation Info";
+      }
+   }
+   
+   public Document toValidationInfoDoc()
+   {
+      return null;
+   }
+   
+   public Node toValidationInfoNode(Document document)
+   {
+      return null;
+   }
+}
