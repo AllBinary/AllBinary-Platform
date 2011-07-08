5a35f557fffe758ebd97ccb755e8612bbe3d3cce
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:15:53 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/admin/tags/XmlOptionItemsTag.java
@@ -0,0 +1,34 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.tags;
+
+import java.util.*;
+
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.*;
+
+
+
+import admin.taghelpers.XmlOptionItemsHelperFactory;
+import admin.taghelpers.XmlOptionItemsRequestHelperFactory;
+
+
+public class XmlOptionItemsTag extends TableTag
+{
+   public XmlOptionItemsTag()
+   {
+      this.setTagHelperFactory(new XmlOptionItemsHelperFactory());
+      this.setTagRequestHelperFactory(new XmlOptionItemsRequestHelperFactory());
+   }
+}
