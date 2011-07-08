6a429f5e73f1beb72808c0e6ab44ecff1e0c5f6e
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:03:31 -0500

    added

---
+++ allbinary_src/BlisketTagHelpersJavaLibrary/src/admin/taghelpers/LogHelper.java
@@ -0,0 +1,122 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.taghelpers;
+
+import java.util.HashMap;
+
+import javax.servlet.jsp.PageContext;
+
+import abcs.business.installer.Portion;
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.data.tables.log.LogTableEntity;
+import allbinary.data.tables.log.LogTableEntityFactory;
+ 
+public class LogHelper implements BasicTableInterface
+{
+   //private HttpServletRequest request;
+
+	private final Portion portion;
+
+   public LogHelper(HashMap hashMap, PageContext pageContext)
+   {
+	   this.portion = new Portion(hashMap);
+   }
+   
+   public String drop()
+   {
+      try
+      {
+         return LogTableEntityFactory.getInstance().getLogTableEntityInstance().dropTable();
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to drop log table";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"drop()",e));
+         }
+         return error;  
+      }
+   }
+   
+   public String create()
+   {
+      try
+      {
+         return LogTableEntityFactory.getInstance().getLogTableEntityInstance().createTable();
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to create new log table";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"create()",e));
+         }
+         return error;  
+      }
+   }
+  
+   public String restore()
+   {
+      try
+      {
+         String success = "Restore Successful";
+         
+         LogTableEntity logTableEntity = 
+        	 LogTableEntityFactory.getInstance().getLogTableEntityInstance();
+
+         String result = logTableEntity.restoreTable(this.portion);
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {
+            LogUtil.put(LogFactory.getInstance(success,this,"restore()"));
+         }
+         return result;
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to restore backup";
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"restore()",e));
+         }
+         return error;
+      }
+   }
+   
+   public String backup()
+   {
+      try
+      {
+         String success = "Restore Successful";
+         String result = LogTableEntityFactory.getInstance().getLogTableEntityInstance().backupTable();
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGS))
+         {
+            LogUtil.put(LogFactory.getInstance(success,this,"backup()"));
+         }
+         return result;
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to make backup";
+         
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLTAGSERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"backup()",e));
+         }
+         return error;
+      }
+   }      
+}
