de655b5910b3574ba28a90755d8249093236d518
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:09:47 -0500

    added

---
+++ allbinary_src/ViewsJavaLibrary/src/views/admin/inventory/download/NewValidationView.java
@@ -0,0 +1,68 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package views.admin.inventory.download;
+
+import abcs.logic.basic.string.StringUtil;
+import org.w3c.dom.Node;
+import org.w3c.dom.Document;
+
+import allbinary.business.user.commerce.inventory.item.ItemInterface;
+import allbinary.business.user.commerce.inventory.item.BasicItem;
+import allbinary.business.user.commerce.inventory.item.download.DownloadableItem;
+
+
+import allbinary.logic.visual.transform.info.TransformInfoInterface;
+
+import allbinary.logic.control.validate.ValidationComponentInterface;
+
+
+public class NewValidationView 
+    extends DownloadableInventoryItemView
+    implements ValidationComponentInterface
+{
+   public NewValidationView(TransformInfoInterface transformInfoInterface) throws Exception
+   {
+      super(transformInfoInterface, StringUtil.getInstance().EMPTY_STRING);
+      
+      this.itemInterface = (ItemInterface) new BasicItem();
+      this.downloadableItem = new DownloadableItem(this.itemInterface.getId());
+   }
+
+   public Boolean isValid()
+   {
+      try
+      {
+         return Boolean.TRUE;
+      }
+      catch(Exception e)
+      {
+         return Boolean.FALSE;
+      }
+   }
+   
+   public Document toValidationInfoDoc()
+   {
+      return null;
+   }
+   
+   public Node toValidationInfoNode(Document document)
+   {
+      return null;
+   }
+   
+   public String validationInfo()
+   {
+      return "Error Creating New Downloadable Item";
+   }
+}
