15d1ad78c0e15f51c0fe689adfe2ff26d8a74539
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 00:25:47 -0500

    added

---
+++ allbinary_src/InitInfoJavaLibrary/src/abcs/globals/WebAppUrlGlobals.java
@@ -0,0 +1,62 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package abcs.globals;
+
+import abcs.logic.basic.string.StringValidationUtil;
+import abcs.business.init.InitInfo;
+
+public class WebAppUrlGlobals extends UrlGlobalsInterface
+{
+   private String path;
+   private boolean isWebappPathSet = false;
+   
+   public boolean isTestingMode()
+   {
+      return InitInfo.getInstance().isTesting();
+   }
+   
+   public String getWebappPath()
+   {
+      return path;
+   }
+
+   public synchronized void setWebappPath(String path)
+   {
+      path = path;
+      isWebappPathSet = true;
+   }  
+   
+   public String getTestHtmlPath()
+   {
+      return InitInfo.getInstance().getTestHtmlPath();
+   }
+      
+   public String getMainPath() throws Exception
+   {
+      String mainPath = InitInfo.getInstance().getMainPath();
+      if(!StringValidationUtil.getInstance().isEmpty(mainPath))
+      {
+         return mainPath;
+      }
+      else
+      if(isWebappPathSet)
+      {
+         return getWebappPath(); 
+      }
+      else
+      {
+         throw new Exception("Webapp Path is not set");
+      }
+   }   
+}
