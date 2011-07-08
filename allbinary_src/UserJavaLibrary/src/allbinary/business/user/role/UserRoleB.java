40ba55cd94c579864756c294763f7db7ab84c359
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:04:07 -0500

    added

---
+++ allbinary_src/UserJavaLibrary/src/allbinary/business/user/role/UserRoleB.java
@@ -0,0 +1,68 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.role;
+
+import abcs.business.user.role.BasicUserRole;
+import abcs.logic.basic.string.StringValidationUtil;
+import allbinary.business.user.UserFactoryInterface;
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+ *
+ * @author user
+ */
+public class UserRoleB extends UserRole
+{
+    public static Vector roleVector = new Vector();
+
+    static
+    {
+        Class clazz = UserRoleFactory.class;
+    }
+
+    protected UserRoleB(
+        BasicUserRole aBasicUserRole,
+        UserFactoryInterface aUserFactoryInterface)
+   {
+        super(aBasicUserRole, aUserFactoryInterface);
+
+        roleVector.add(this);
+   }
+
+   public static synchronized UserRole getRole(String role) throws Exception
+   {
+      if(!StringValidationUtil.getInstance().isEmpty(role))
+      {
+         Vector roleVector = UserRoleB.getAll();
+         Iterator iter = roleVector.iterator();
+         while(iter.hasNext())
+         {
+            UserRole userRole = (UserRole) iter.next();
+
+            if(role.compareTo(userRole.toString()) == 0)
+            {
+               return userRole;
+            }
+         }
+      }
+      throw new Exception("Unable to get role - Unknown Role = " + role);
+   }
+
+   public static Vector getAll()
+   {
+      return UserRoleB.roleVector;
+   }
+
+}
