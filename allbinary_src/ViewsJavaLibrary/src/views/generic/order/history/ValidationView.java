69a510d5d8667f5e180ead4dd4338edfd68f53fe
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:27:24 -0500

    added

---
+++ allbinary_src/ViewsJavaLibrary/src/views/generic/order/history/ValidationView.java
@@ -0,0 +1,48 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package views.generic.order.history;
+
+import allbinary.logic.visual.transform.info.TransformInfoInterface;
+
+import allbinary.logic.control.validate.ValidationComponentInterface;
+
+import allbinary.globals.MESSAGES;
+
+public class ValidationView extends OrderHistoryView implements ValidationComponentInterface
+{
+   public ValidationView(TransformInfoInterface transformInfoInterface) throws Exception
+   {
+      super(transformInfoInterface);
+   }
+   
+   public Boolean isValid()
+   {
+      return Boolean.TRUE;
+   }
+   
+   public org.w3c.dom.Document toValidationInfoDoc()
+   {
+      return null;
+   }
+   
+   public org.w3c.dom.Node toValidationInfoNode(org.w3c.dom.Document document)
+   {
+      return null;      
+   }
+      
+   public String validationInfo()
+   {
+      return MESSAGES.CONTACTADMIN;
+   }
+}
