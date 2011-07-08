52cc646db19242ae5e4ef6eabbe3d20619fd802d
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 22:49:13 -0500

    added

---
+++ allbinary_src/BlisketJavaLibrary/src/allbinary/logic/communication/smtp/event/handler/factory/UserEmailEventHandlerSingletons.java
@@ -0,0 +1,97 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.communication.smtp.event.handler.factory;
+
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.business.user.UserInterface;
+import allbinary.logic.communication.smtp.event.UserEmailEventNameData;
+import allbinary.logic.communication.smtp.event.handler.EmailEventHandlerUtil;
+import allbinary.logic.communication.smtp.event.handler.UserEmailEventHandler;
+import allbinary.logic.communication.smtp.event.modules.log.LogUserEmailEventListenerModule;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+public class UserEmailEventHandlerSingletons
+{
+	private static final UserEmailEventHandlerSingletons instance = 
+		new UserEmailEventHandlerSingletons();
+	
+   private static HashMap userEmailEventHandlerHashMap = null;
+   
+   private UserEmailEventHandlerSingletons()
+   {
+   }
+
+   public static UserEmailEventHandler getInstance(
+      UserEmailEventNameData userEmailEventNameData, 
+      UserInterface userInterface)
+      throws Exception
+   {
+      if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.EMAILLOGGING))
+      {
+         LogUtil.put(LogFactory.getInstance("Start", instance, "getInstance"));
+      }
+      
+      if(UserEmailEventHandlerSingletons.userEmailEventHandlerHashMap == null)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.EMAILLOGGING))
+         {
+            LogUtil.put(LogFactory.getInstance("Initializing HashMap", instance, "getInstance"));
+         }
+         
+         UserEmailEventHandlerSingletons.userEmailEventHandlerHashMap = new HashMap();
+      }
+      
+      UserEmailEventHandler userEmailEventHandler = (UserEmailEventHandler)
+         UserEmailEventHandlerSingletons.userEmailEventHandlerHashMap.get(
+         userEmailEventNameData);
+      
+      if(userEmailEventHandler == null)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.EMAILLOGGING))
+         {
+            LogUtil.put(LogFactory.getInstance("Creating New Named UserEmailEventHandler",
+               instance, "getInstance"));
+         }
+         
+         //Create New Handler and add listeners
+         UserEmailEventHandler newUserEmailEventHandler = new UserEmailEventHandler();
+
+         Vector vector = 
+            EmailEventHandlerUtil.getUserEmailEventListenerVector(
+               userEmailEventNameData, userInterface);
+         newUserEmailEventHandler.addListener(vector);
+         newUserEmailEventHandler.addListener(new LogUserEmailEventListenerModule());
+         
+         UserEmailEventHandlerSingletons.userEmailEventHandlerHashMap.put(
+            userEmailEventNameData, newUserEmailEventHandler);
+
+         return newUserEmailEventHandler;
+      }
+      else
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.EMAILLOGGING))
+         {
+            LogUtil.put(LogFactory.getInstance("Returning existing UserEmailEventHandler",
+               instance, "getInstance"));
+         }
+
+         //Return existing event Handler
+         return userEmailEventHandler;
+      }
+   }
+
+}
