ca82c7756379eef2c434c0c523a5880727e092d4
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:53:05 -0500

    added

---
+++ allbinary_src/WorkflowBaseJavaLibrary/src/allbinary/logic/control/workflow/StoreWorkFlowInterface.java
@@ -0,0 +1,23 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.control.workflow;
+
+//An actual workflow could implement all of the following in the process method
+//and is determined by the request command in the url
+//extends ValidationComponentInterface, ValidationInterface, ComponentInterface, DomNodeInterface, DataMappingInterface
+
+public interface StoreWorkFlowInterface extends WorkFlowInterface
+{
+   public String getStoreName() throws Exception;
+}
