f59b3d3b773fa21a0dc689b86c96f6bcabe7c8d8
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:44:12 -0500

    added

---
+++ allbinary_src/CategoryBaseJavaLibrary/src/allbinary/business/category/properties/CategoryPropertiesFactoryInterface.java
@@ -0,0 +1,28 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.category.properties;
+
+public interface CategoryPropertiesFactoryInterface
+{
+   public CategoryPropertiesInterface getInstance();
+
+   /*
+   public CategoryPropertiesInterface getInstance(String name);
+
+   public CategoryPropertiesInterface getInstance(Node node);
+
+   public CategoryPropertiesInterface getInstance(
+      HashMap categoryPropertiesHashMap);
+    */
+}
