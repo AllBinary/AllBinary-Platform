e4dbaa8dc0f1fb2efa7e90e3979c68cfb78f3411
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 00:54:33 -0500

    added

---
+++ allbinary_src/UserCommerceJavaLibrary/src/allbinary/business/user/commerce/money/payment/types/TransactionType.java
@@ -0,0 +1,36 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.commerce.money.payment.types;
+
+public class TransactionType
+{      
+   private String name;
+   
+   public TransactionType(String name)
+   {
+      this.name = name;
+   }
+
+   public boolean isEqual(TransactionType transactionType)
+   {
+      if(this.toString().compareTo(transactionType.toString())==0)
+         return true;
+      else return false;
+   }
+   
+   public String toString()
+   {
+      return name;
+   }
+}
