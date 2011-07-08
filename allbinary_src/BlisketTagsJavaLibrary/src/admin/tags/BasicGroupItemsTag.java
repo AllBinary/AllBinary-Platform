387045874bd4013f4725407e537080b04f982857
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-07 23:11:50 -0500

    added

---
+++ allbinary_src/BlisketTagsJavaLibrary/src/admin/tags/BasicGroupItemsTag.java
@@ -0,0 +1,36 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.tags;
+
+import java.util.*;
+
+import javax.servlet.jsp.*;
+
+
+
+
+import admin.taghelpers.BasicGroupItemsHelperFactory;
+import admin.taghelpers.BasicGroupItemsRequestHelperFactory;
+
+
+public class BasicGroupItemsTag extends TableTag
+{
+   private HashMap propertiesHashMap;
+   
+   public BasicGroupItemsTag()
+   {
+      this.setTagHelperFactory(new BasicGroupItemsHelperFactory());
+      this.setTagRequestHelperFactory(new BasicGroupItemsRequestHelperFactory());
+   }
+}
