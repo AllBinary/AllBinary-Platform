e1b915342aab847b270c9e356049d4653c592a13
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 01:17:32 -0500

    added

---
+++ allbinary_src/ViewsJavaLibrary/src/views/business/context/modules/storefront/customizer/generic/page/PageView.java
@@ -0,0 +1,82 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package views.business.context.modules.storefront.customizer.generic.page;
+
+
+
+
+import abcs.logic.communication.log.LogFactory;
+import allbinary.logic.visual.transform.info.TransformInfoInterface;
+
+import abcs.logic.communication.log.LogUtil;
+
+import views.business.context.modules.storefront.HttpStoreComponentView;
+
+//implements DomNodeInterface
+public class PageView extends HttpStoreComponentView 
+{     
+   public PageView(TransformInfoInterface transformInfoInterface) throws Exception
+   {
+      super(transformInfoInterface);
+      
+      if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.VIEW))
+      {
+         LogUtil.put(LogFactory.getInstance("View Name: " + transformInfoInterface.getName(), this, "Constructor"));
+      }
+   }
+   
+   /*
+   public void addDomNodeInterfaces()
+   {
+      this.addDomNodeInterface((DomNodeInterface) this);
+   }
+   */
+   
+   /*
+   public Node toXmlNode(Document document) throws Exception
+   {
+      try
+      {
+         return this.pageValidation.toXmlNode(document);
+      }
+      catch(Exception e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(
+         abcs.logic.communication.log.config.type.LogConfigType.XSLLOGGINGERROR))
+         {
+            LogUtil.put("Command Failed",this,"toXmlNode",e);
+         }
+         throw e;
+      }
+   }
+   */
+   
+   public String view() throws Exception
+   {
+      try
+      {
+         //this.addDomNodeInterfaces();
+         return super.view();
+      }
+      catch(Exception e)
+      {
+         String error = "Failed to view TopBar";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.TAGHELPERERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"view()",e));
+         }
+         throw e;
+      }
+   }
+}
