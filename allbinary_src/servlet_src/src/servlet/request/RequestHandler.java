8b8003d5031ef9ddefd0580a868883b97dacbfc5
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 00:48:32 -0500

    added

---
+++ allbinary_src/servlet_src/src/servlet/request/RequestHandler.java
@@ -0,0 +1,95 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package servlet.request;
+
+import java.io.*;
+import java.net.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import abcs.logic.system.security.licensing.LicensingException;
+
+import abcs.logic.communication.log.LogUtil;
+
+import allbinary.logic.control.workflow.WorkFlowInterface;
+import allbinary.logic.control.workflow.RequestWorkFlowFactory;
+
+public class RequestHandler extends HttpServlet
+{
+   public void init(ServletConfig config) throws ServletException
+   {
+      super.init(config);
+   }
+   
+   public void destroy()
+   {
+      
+   }
+   
+   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+   throws ServletException, IOException
+   {
+      try
+      {
+         response.setContentType("text/html");
+         PrintWriter out = response.getWriter();
+         
+         WorkFlowInterface workflowInterface =
+         RequestWorkFlowFactory.getInstance((ServletRequest) request, (ServletResponse) response,
+         this.getServletConfig(),
+         this.getServletContext());
+         
+         if(workflowInterface != null)
+         {
+            out.print(workflowInterface.process());
+         }
+         out.flush();
+         out.close();
+      }
+      catch(LicensingException e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SERVLETERROR))
+         {
+            LogUtil.put("Servlet LicensingException",this,"processRequest()",e);
+         }
+         response.sendRedirect(abcs.globals.URLGLOBALS.LICENSEERRORPAGE);
+      }
+      catch(Exception e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SERVLETERROR))
+         {
+            LogUtil.put("Servlet Exception",this,"processRequest()",e);
+         }
+         response.sendRedirect(abcs.globals.URLGLOBALS.ERRORPAGE);
+      }
+   }
+   
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+   throws ServletException, IOException
+   {
+      processRequest(request, response);
+   }
+   
+   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+   throws ServletException, IOException
+   {
+      processRequest(request, response);
+   }
+
+   public String getServletInfo()
+   {
+      return "WorkFlow Request Handler";
+   }
+}
