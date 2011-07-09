f69c3a84f0ab8687f481090512f3e3eb96ccc08e
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 23:11:48 -0500

    added

---
+++ j2me/games/CollisionLibrary/src/allbinary/game/collision/CollisionHelper.java
@@ -0,0 +1,53 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.collision;
+
+import org.allbinary.game.layer.CollidableCompositeLayer;
+
+import allbinary.layer.AllBinaryLayer;
+
+/**
+ *
+ * @author user
+ */
+public class CollisionHelper {
+
+   protected AllBinaryLayer sourceLayerInterface;
+   
+   public CollisionHelper(AllBinaryLayer sourceLayerInterface)
+   {
+      this.sourceLayerInterface = sourceLayerInterface;
+   }
+   
+   public boolean isCollidable(CollidableCompositeLayer layerInterface)
+   {
+      return true;
+   }
+
+   /**
+    * @return the sourceLayerInterface
+    */
+   public AllBinaryLayer getOwnerLayerInterface()
+   {
+      return sourceLayerInterface;
+   }
+
+   /**
+    * @param sourceLayerInterface the sourceLayerInterface to set
+    */
+   public void setOwnerLayerInterface(AllBinaryLayer sourceLayerInterface)
+   {
+      this.sourceLayerInterface = sourceLayerInterface;
+   }
+}
