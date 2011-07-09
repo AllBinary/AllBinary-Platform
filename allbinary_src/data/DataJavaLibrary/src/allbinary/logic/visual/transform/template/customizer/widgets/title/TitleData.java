4c25f78336b1a8b33b6bcdce6eb3a83b2fb45987
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:46:08 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/logic/visual/transform/template/customizer/widgets/title/TitleData.java
@@ -0,0 +1,35 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.visual.transform.template.customizer.widgets.title;
+
+public class TitleData
+{
+	private static final TitleData instance = new TitleData();
+	
+	   public static TitleData getInstance() {
+			return instance;
+		}
+	
+   private TitleData()
+   {
+   }
+   
+public final String NAME = "TITLE_NAME";
+   
+   public final String TEXT = "TITLE_TEXT";
+   
+   public final String VIEWNAMEKEY = "Title";
+   
+   public final int MAXLEN = 255;
+}
