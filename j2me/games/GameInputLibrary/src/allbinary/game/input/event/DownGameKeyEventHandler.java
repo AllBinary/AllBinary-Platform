364b7f83e39c99853426c47438e7949d685fd12b
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 23:44:37 -0500

    added

---
+++ j2me/games/GameInputLibrary/src/allbinary/game/input/event/DownGameKeyEventHandler.java
@@ -0,0 +1,40 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.input.event;
+
+import allbinary.logic.basic.util.event.AllBinaryEventObject;
+import allbinary.logic.basic.util.event.EventListenerInterface;
+import allbinary.logic.basic.util.event.handler.BasicEventHandler;
+
+public class DownGameKeyEventHandler extends BasicEventHandler
+{
+    private static final DownGameKeyEventHandler gameKeyEventHandler = new DownGameKeyEventHandler();
+
+    private DownGameKeyEventHandler()
+    {
+    }
+
+    public static DownGameKeyEventHandler getInstance()
+    {
+        return DownGameKeyEventHandler.gameKeyEventHandler;
+    }
+
+    protected void process(AllBinaryEventObject eventObject,
+            EventListenerInterface eventListenerInterface) throws Exception
+    {
+
+        ((DownGameKeyEventListenerInterface) eventListenerInterface)
+                .onDownGameKeyEvent((GameKeyEvent) eventObject);
+    }
+}
