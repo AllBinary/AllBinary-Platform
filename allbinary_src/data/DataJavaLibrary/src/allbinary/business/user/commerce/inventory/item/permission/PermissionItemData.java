15125b349b95fee3fc7989af400eb5da1f3c4260
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:34:52 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/business/user/commerce/inventory/item/permission/PermissionItemData.java
@@ -0,0 +1,25 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.commerce.inventory.item.permission;
+
+public class PermissionItemData
+{   
+   //role username or other //who has permission
+   public static final String WHO = "PERMISSIONSITEM_WHO";
+   public static final String WHAT = "PERMISSIONSITEM_WHAT";
+   public static final String TYPE = "PERMISSIONSITEM_TYPE";
+   public static final String START_TIME = "PERMISSIONSITEM_START_TIME";
+   public static final String END_TIME = "PERMISSIONSITEM_END_TIME";
+   public static final String REMOTE_ADDR = "PERMISSIONSITEM_REMOTE_ADDR";
+}
