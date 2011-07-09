ddd4b7d5f13c872af2e3e13c55c742c09238436c
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 23:02:21 -0500

    added

---
+++ j2me/games/BasicWeaponPartsJavaLibrary/src/allbinary/game/part/weapon/BasicProjectileWeaponPart.java
@@ -0,0 +1,71 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.part.weapon;
+
+import allbinary.animation.Animation;
+import allbinary.game.combat.weapon.WeaponProperties;
+import allbinary.game.layer.weapon.WeaponLayer;
+import allbinary.game.layer.weapon.WeaponLayerCircularPool;
+import allbinary.game.score.ScoreableInterface;
+import allbinary.graphics.RelativeRelationship;
+import allbinary.layer.AllBinaryLayer;
+import allbinary.layer.AllBinaryLayerManager;
+
+public class BasicProjectileWeaponPart extends BasicWeaponPart  
+{
+   private WeaponLayerCircularPool weaponLayerCircularStaticPool;
+
+   public BasicProjectileWeaponPart(
+      Animation animationInterface,
+      WeaponLayerCircularPool weaponLayerCircularStaticPool) {
+      
+      super(animationInterface);
+      
+      this.weaponLayerCircularStaticPool = weaponLayerCircularStaticPool;
+   }
+   
+   public BasicProjectileWeaponPart(
+           Animation animationInterface,
+           AllBinaryLayer sourceLayerInterface, 
+           WeaponLayerCircularPool weaponLayerCircularStaticPool,
+           WeaponProperties weaponProperties,  
+           ScoreableInterface scoreableInterface, 
+           RelativeRelationship relativeRelationship) 
+   {
+      super(animationInterface, 
+              sourceLayerInterface, 
+              weaponProperties, 
+              scoreableInterface, 
+              relativeRelationship
+              );
+
+      this.weaponLayerCircularStaticPool = weaponLayerCircularStaticPool;      
+   }
+
+   public void process(AllBinaryLayerManager allbinaryLayerManager,
+           short angle, WeaponProperties weaponProperties, 
+           ScoreableInterface scoreableInterface)
+           throws Exception 
+   {
+      WeaponLayer weaponLayer =
+              weaponLayerCircularStaticPool.getInstance(
+              this.getOwnerLayerInterface(), 
+              this.relativeRelationship.getX(), 
+              this.relativeRelationship.getY(), 
+              angle, weaponProperties, scoreableInterface);
+
+      //weaponLayerArray[index].moveOutsideRadius((this.getWidth() >> 1));
+      allbinaryLayerManager.append(weaponLayer);
+   }
+}
