3175e69fa355ccbf2c8ee6f6a13ccbf8e6af3647
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:35:07 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/business/user/commerce/inventory/order/OrderData.java
@@ -0,0 +1,27 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.commerce.inventory.order;
+
+public class OrderData
+{
+   public static final String ORDERS = "ORDERS";
+   
+   public static final String ORDER = "ORDER";
+   
+   public static final String ID = "ORDER_ID";
+   public static final String CUSTOMERCOMMENT = "ORDER_CUSTOMER_COMMENT";
+   public static final String CUSTOMERCANCELCOMMENT = "ORDER_CUSTOMER_CANCEL_COMMENT";   
+   public static final String STORECOMMENT = "ORDER_STORE_COMMENT";
+   public static final String STORECANCELCOMMENT = "ORDER_STORE_CANCEL_COMMENT";
+}
