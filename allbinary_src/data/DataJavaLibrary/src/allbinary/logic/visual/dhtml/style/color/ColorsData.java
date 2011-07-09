4f9d783d9291ec0448bf51360b9d958f55e9cbd9
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:42:34 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/logic/visual/dhtml/style/color/ColorsData.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.visual.dhtml.style.color;
+
+public class ColorsData
+{
+	private static final ColorsData instance = new ColorsData();
+	
+	   public static ColorsData getInstance() {
+			return instance;
+		}
+	
+   private ColorsData()
+   {
+   }
+   
+   public final String NAME = "COLORS_NAME";
+   
+}
