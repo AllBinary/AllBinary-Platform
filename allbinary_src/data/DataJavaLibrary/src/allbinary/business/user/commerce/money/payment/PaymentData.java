77c6657466a5ff2880fdb1114040920855787077
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:36:39 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/business/user/commerce/money/payment/PaymentData.java
@@ -0,0 +1,60 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.commerce.money.payment;
+
+public class PaymentData
+{   
+   public static final int MIN = 4;
+   public static final int MAX = 19;
+      
+   public static final String ADDNEW = "Add New Payment";
+   //public static final String ADD = "Add Payment";
+   //public static final String REMOVE = "Remove Payment";
+   public static final String VIEW = "View Payment";
+   public static final String SELECT = "Select Payment";
+   public static final String CHANGE = "Change Payment";   
+   
+   /*
+   public static final String ADDNEW = "PAYMENT_ADD_NEW_PAYMENT";
+   public static final String ADD = "PAYMENT_ADD_PAYMENT";
+   public static final String REMOVE = "PAYMENT_REMOVE_PAYMENT";
+   public static final String VIEW = "PAYMENT_VIEW_PAYMENT";
+   public static final String SELECT = "PAYMENT_SELECT_PAYMENT";
+   public static final String CHANGE = "PAYMENT_CHANGE_PAYMENT";
+   */
+   
+   public static final String PAYMENT = "PAYMENT";
+   
+   public static final String METHOD = "PAYMENT_METHOD";
+   
+   public static final String ID = "PAYMENT_ID";
+   public static final String NUMBER = "PAYMENT_NUMBER";
+   public static final String NAME = "PAYMENT_NAME";
+   
+   public static final String TYPE="PAYMENT_TYPE";
+   public static final String EXPIRATION="PAYMENT_EXPIRATION";
+   
+   public static final String TENDERTYPE = "PAYMENT_TENDERTYPE";
+   public static final String TRANSACTIONTYPE = "PAYMENT_TRANSACTIONTYPE";
+      
+   public static final String ABA = "PAYMENT_ABA";
+   public static final String ACCOUNT = "PAYMENT_ACCOUNT";
+   public static final String ACCOUNTTYPE = "PAYMENT_ACCOUNTTYPE";
+   public static final String AUTHORIZATIONCODE = "PAYMENT_AUTHORIZATIONCODE";
+   public static final String CHECKNUMBER = "PAYMENT_CHECKNUMBER";
+   public static final String CHECKTYPE = "PAYMENT_CHECKTYPE";
+   public static final String DRIVERSLICENSE = "PAYMENT_DRIVERSLICENSE";
+   public static final String MAGNETICINKCHECKREADER = "PAYMENT_MAGNETICINKCHECKREADER";
+   
+}
