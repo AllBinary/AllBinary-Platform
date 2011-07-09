f31995e41e10f5658aae2f8a59c54fc52e29539a
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 23:57:39 -0500

    added

---
+++ store/NoThirdPartyStoreJavaLibrary/src/org/allbinary/thirdparty/store/NoThirdPartyStore.java
@@ -0,0 +1,35 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.thirdparty.store;
+
+import abcs.logic.basic.string.StringUtil;
+import abcs.logic.system.security.licensing.AbeClientInformationInterface;
+
+/**
+ *
+ * @author user
+ */
+public class NoThirdPartyStore extends ThirdPartyStore
+{
+    protected NoThirdPartyStore()
+    {
+        super(StringUtil.getInstance().EMPTY_STRING);
+    }
+
+    public void authorize(
+        AbeClientInformationInterface clientInformationInterface)
+    {
+        this.setAuthorized(true);
+    }
+}
