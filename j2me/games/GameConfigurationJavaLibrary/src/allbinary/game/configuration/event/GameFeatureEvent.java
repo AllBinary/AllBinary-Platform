c179af130cb9ec8b84c4b12577787fc2bbad3b99
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 23:38:27 -0500

    added

---
+++ j2me/games/GameConfigurationJavaLibrary/src/allbinary/game/configuration/event/GameFeatureEvent.java
@@ -0,0 +1,38 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.configuration.event;
+
+import allbinary.logic.basic.util.event.AllBinaryEventObject;
+
+public class GameFeatureEvent extends AllBinaryEventObject
+{
+    private final String whatChanged;
+    
+    public GameFeatureEvent(Object object, String whatChanged)
+    {
+        super(object);
+
+        this.whatChanged = whatChanged;
+    }
+
+    public String getWhatChanged()
+    {
+        return whatChanged;
+    }
+
+    public Object getGameOption()
+    {
+        return this.getSource();
+    }
+}
