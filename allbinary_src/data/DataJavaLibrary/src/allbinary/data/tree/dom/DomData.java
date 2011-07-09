453597e8ae06eb271457c6274b7b1745b23330f3
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:38:24 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/data/tree/dom/DomData.java
@@ -0,0 +1,27 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.data.tree.dom;
+
+public class DomData
+{
+   private DomData()
+   {
+   }
+   
+   public static final String ID = "id";
+   public static final String CHILD = "child";
+   public static final String NAME = "name";
+   public static final String VALUE = "value";
+   public static final String INDEX = "index";
+}
