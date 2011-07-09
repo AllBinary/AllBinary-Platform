055448f9f8654f84ba06aa848fca933b24499c24
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:33:23 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/business/user/address/ShippingAddressData.java
@@ -0,0 +1,60 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.address;
+
+public class ShippingAddressData
+{
+   private ShippingAddressData()
+   {}
+   
+   public static final String SETTOBILLINGADDRESS="Set To Billing Address";   
+
+   /*
+   public static final String ADD = "Add Shipping Address";   
+   public static final String EDIT = "Edit Shipping Address";
+   public static final String UPDATE = "Update Shipping Address";
+   public static final String REMOVE = "Remove Shipping Address";
+   public static final String VIEW = "View Shipping Address";
+   public static final String CHANGE = "Change Shipping Address";
+   public static final String MAKENEW = "Add New Shipping Address";
+
+   public static final String ADDNAME = "ADDSHIPPINGADDRESS";   
+   public static final String REMOVENAME = "REMOVESHIPPINGADDRESS";
+   public static final String MAKENEWNAME = "MAKENEWSHIPPINGADDRESS";
+   public static final String SETTOBILLINGADDRESSNAME = "SETTOBILLINGADDRESS";
+   */
+
+   public static final String SELECT = "Select Shipping Address";
+   public static final String SELECTNAME = "SELECTSHIPPINGADDRESS";
+   
+   public static final String MULTIPLE = "SHIPPINGADDRESSES";
+//public static String DEFAULTSHIPPINGADDRESS= "DEFAULT_SHIPPING_ADDRESS";
+
+   public static final String SHIPPINGADDRESSES = "SHIPPINGADDRESSES";
+
+   public static final String SHIPPINGADDRESS = "SHIPPINGADDRESS";   
+   
+   public static final String ID = "SHIPPINGADDRESS_ID";
+   public static final String ADDRESS = "SHIPPINGADDRESS_ADDRESS";
+   public static final String NAME = "SHIPPINGADDRESS_NAME";
+   public static final String STREET = "SHIPPINGADDRESS_STREET";
+   public static final String CITY = "SHIPPINGADDRESS_CITY";
+   public static final String STATE = "SHIPPINGADDRESS_STATE";
+   public static final String CODE = "SHIPPINGADDRESS_CODE";
+   public static final String COUNTRY = "SHIPPINGADDRESS_COUNTRY";
+   
+   public static final String TAX = "SHIPPINGADDRESS_TAX";
+   
+   
+}
