9be765d7a02bf971437db446f86058ee2fc9489d
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:29:12 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/abcs/logic/communication/log/config/type/LogConfigTypesData.java
@@ -0,0 +1,26 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package abcs.logic.communication.log.config.type;
+
+public class LogConfigTypesData
+{
+	private static final LogConfigTypesData instance = new LogConfigTypesData();
+
+	public static LogConfigTypesData getInstance() {
+		return instance;
+	}
+	
+   public final String NAME = "LOG_CONFIG_TYPES_NAME";
+   public final String DESCRIPTION = "LOG_CONFIG_TYPES_DESCRIPTION";
+}
