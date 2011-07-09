6acdf835845702f8cfe272f15cbe9353abb94ff5
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 23:38:19 -0500

    added

---
+++ j2me/games/GameConfigurationJavaLibrary/src/allbinary/game/configuration/event/ChangedGameFeatureListener.java
@@ -0,0 +1,31 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.configuration.event;
+
+import allbinary.game.configuration.GameConfiguration;
+
+public class ChangedGameFeatureListener extends BaseChangedGameFeatureListener
+{
+    private static final ChangedGameFeatureListener gameFeatureListener = new ChangedGameFeatureListener();
+    
+    public static ChangedGameFeatureListener getInstance()
+    {
+        return gameFeatureListener;
+    }
+
+    public boolean isChanged(GameConfiguration gameConfiguration)
+    {
+        return list.contains(gameConfiguration);
+    }    
+}
