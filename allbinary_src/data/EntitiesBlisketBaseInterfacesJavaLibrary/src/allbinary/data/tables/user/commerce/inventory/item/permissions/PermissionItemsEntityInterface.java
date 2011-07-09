255b57e7f1bf89439da8ae822c610d70dcdba639
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 02:06:18 -0500

    added

---
+++ allbinary_src/data/EntitiesBlisketBaseInterfacesJavaLibrary/src/allbinary/data/tables/user/commerce/inventory/item/permissions/PermissionItemsEntityInterface.java
@@ -0,0 +1,28 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.data.tables.user.commerce.inventory.item.permissions;
+
+import allbinary.data.tables.BasicTableInterface;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+public interface PermissionItemsEntityInterface extends BasicTableInterface
+{
+   public void delete(String value);
+
+   public void insert(Vector values);
+
+   public void update(HashMap updatedValues);   
+}
