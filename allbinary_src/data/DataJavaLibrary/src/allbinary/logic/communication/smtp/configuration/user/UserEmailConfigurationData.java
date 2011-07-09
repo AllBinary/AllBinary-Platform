0b3e48a1ce94e25f5f3b4a02c06122f0817cd46f
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:40:33 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/logic/communication/smtp/configuration/user/UserEmailConfigurationData.java
@@ -0,0 +1,24 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.communication.smtp.configuration.user;
+
+public class UserEmailConfigurationData
+{
+   public static final String NAME = "USER_EMAIL_CONFIGURATION_NAME";
+
+   private UserEmailConfigurationData()
+   {
+
+   }
+}
