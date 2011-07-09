cc5fdb66dbfb8aa35fd898aab0e4b5b6c0fa30ae
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:43:06 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/logic/visual/dhtml/style/css/property/CssPropertyValueData.java
@@ -0,0 +1,29 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.visual.dhtml.style.css.property;
+
+public class CssPropertyValueData
+{
+	private static final CssPropertyValueData instance = new CssPropertyValueData();
+	
+	   public static CssPropertyValueData getInstance() {
+			return instance;
+		}
+	
+   private CssPropertyValueData()
+   {
+   }  
+   
+   public final String NAME = "CSS_PROPERTY_VALUE";
+}
