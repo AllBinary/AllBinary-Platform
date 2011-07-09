e1f3be77dbc36a1c8b6fe77d23c7110f96541ba0
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:30:32 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/business/advertisement/area/AdvertisementAreaData.java
@@ -0,0 +1,31 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.advertisement.area;
+
+public class AdvertisementAreaData
+{
+	private static final AdvertisementAreaData instance = new AdvertisementAreaData();
+
+	   public static AdvertisementAreaData getInstance() {
+			return instance;
+		}
+	
+   private AdvertisementAreaData()
+   {
+   }
+
+public final String NAME = "ADVERTISEMENT_NAME";
+   public final String DESCRIPTION = "ADVERTISEMENT_DESCRIPTION";
+   public final String CONSTRAINTS = "ADVERTISEMENT_CONSTRAINTS";
+}
