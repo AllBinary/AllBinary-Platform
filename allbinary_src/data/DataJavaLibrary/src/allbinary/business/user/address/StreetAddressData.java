c79b0bb16b580171b9d2365ae4271f02ecdc5e21
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:33:31 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/business/user/address/StreetAddressData.java
@@ -0,0 +1,38 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.address;
+
+public class StreetAddressData
+{
+   private StreetAddressData()
+   {}
+   public static final int MAX = 255;
+   public static final int MIN = 0;
+
+   public static final String FORM = "FORM";
+
+   public static final String STATES = "STATES";
+   
+   public static final String NUMBEROFADDRESSES = "STREETADDRESS_NUMBEROFADDRESSES";
+
+   public static final String ID = "STREETADDRESS_ID";
+   public static final String DEFAULT = "STREETADDRESS_DEFAULT_ADDRESS";
+   public static final String ADDRESS = "STREETADDRESS_ADDRESS";
+   public static final String NAME = "STREETADDRESS_NAME";
+   public static final String STREET = "STREETADDRESS_STREET";
+   public static final String CITY = "STREETADDRESS_CITY";
+   public static final String STATE = "STREETADDRESS_STATE";
+   public static final String CODE = "STREETADDRESS_CODE";
+   public static final String COUNTRY = "STREETADDRESS_COUNTRY";
+}
