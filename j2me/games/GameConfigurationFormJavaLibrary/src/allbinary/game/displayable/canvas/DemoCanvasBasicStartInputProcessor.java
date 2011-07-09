f70265439b22f93bda25583b562534be3f0b02c0
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 23:37:07 -0500

    added

---
+++ j2me/games/GameConfigurationFormJavaLibrary/src/allbinary/game/displayable/canvas/DemoCanvasBasicStartInputProcessor.java
@@ -0,0 +1,61 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.displayable.canvas;
+
+import org.allbinary.util.BasicArrayList;
+
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.canvas.RunnableCanvas;
+import allbinary.game.commands.GameCommandsFactory;
+import allbinary.game.input.GameInputStrings;
+import allbinary.game.input.event.GameKeyEvent;
+
+public class DemoCanvasBasicStartInputProcessor
+    extends BasicMenuInputProcessor
+{
+
+    public DemoCanvasBasicStartInputProcessor(
+        BasicArrayList gameKeyEventList, RunnableCanvas gameCanvas)
+    {
+        super(gameKeyEventList, gameCanvas);
+    }
+
+    /*
+    public int processInput(int key) throws Exception
+    {
+        return -1;
+    }
+     */
+
+    public int processInput()
+        throws Exception
+    {
+        BasicArrayList list = this.getGameKeyEventList();
+        
+        int size = list.size();
+        
+        GameKeyEvent gameKeyEvent;
+        
+        for (int index = 0; index < size; index++)
+        {
+            gameKeyEvent = (GameKeyEvent) list.get(index);
+
+            LogUtil.put(LogFactory.getInstance("Start GameKey: " + gameKeyEvent, this, GameInputStrings.getInstance().PROCESS_INPUT));
+        }
+        this.getCanvas().getCustomCommandListener().commandAction(
+                GameCommandsFactory.getInstance().START_COMMAND, null);
+        return size;
+    }
+}
