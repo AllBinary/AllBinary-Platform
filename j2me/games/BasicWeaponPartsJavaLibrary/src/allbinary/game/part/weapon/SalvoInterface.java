da2cd4bb036dde567c53e2e63e1d49eb262dd499
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 23:02:52 -0500

    added

---
+++ j2me/games/BasicWeaponPartsJavaLibrary/src/allbinary/game/part/weapon/SalvoInterface.java
@@ -0,0 +1,29 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.part.weapon;
+
+import allbinary.game.combat.weapon.WeaponProperties;
+import allbinary.game.score.ScoreableInterface;
+import allbinary.layer.AllBinaryLayerManager;
+
+public interface SalvoInterface {
+
+	void process(AllBinaryLayerManager allbinaryLayerManager, short angle) 
+	throws Exception;
+	
+	void process(AllBinaryLayerManager allbinaryLayerManager,
+			short angle, WeaponProperties weaponProperties, 
+			ScoreableInterface scoreableInterface)
+	throws Exception;
+}
