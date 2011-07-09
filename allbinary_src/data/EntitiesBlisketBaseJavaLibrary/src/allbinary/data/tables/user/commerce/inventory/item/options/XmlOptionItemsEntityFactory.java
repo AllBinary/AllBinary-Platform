d40121750e8815c6e6858a3f889b47cf3b58037c
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 02:15:29 -0500

    added

---
+++ allbinary_src/data/EntitiesBlisketBaseJavaLibrary/src/allbinary/data/tables/user/commerce/inventory/item/options/XmlOptionItemsEntityFactory.java
@@ -0,0 +1,67 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.data.tables.user.commerce.inventory.item.options;
+
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+
+public class XmlOptionItemsEntityFactory
+{
+    private static final XmlOptionItemsEntityFactory instance =
+            new XmlOptionItemsEntityFactory();
+
+    /**
+     * @return the instance
+     */
+    public static XmlOptionItemsEntityFactory getInstance() {
+        return instance;
+    }
+
+   //private static final String CLASSNAME = "allbinary.data.tables.InventoryEntity";
+   
+   private XmlOptionItemsEntityFactory()
+   {
+   }
+   
+   public XmlOptionItemsEntity getXmlOptionItemsEntityInstance() //throws LicensingException
+   {
+      try
+      {
+         //Object object = AbeFactory.getInstance(CLASSNAME);
+         //return (InventoryEntityInterface) InterfaceCastProxy.newInstance(object);
+         return new allbinary.data.tables.user.commerce.inventory.item.options.XmlOptionItemsEntity();
+      }
+      /*
+      catch(LicensingException e)
+      {
+         String error = "Failed to get instance";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.ENTITYFACTORYERROR))
+         {
+            LogUtil.put(error,"InventoryEntityFactory","getInstance()",e);
+         }
+         throw e;
+      }
+      */
+      catch(Exception e)
+      {
+         String error = "Failed to get instance";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.ENTITYFACTORYERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,"InventoryEntityFactory","getInstance()",e));
+         }
+         return null;
+      }
+   }
+   
+}
