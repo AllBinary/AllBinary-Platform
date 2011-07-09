7445b1cf35695779332df4ff1fa1aa606e0aea64
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 02:02:34 -0500

    added

---
+++ allbinary_src/data/DataTableInterfaces/src/allbinary/data/tables/TableMappingInterface.java
@@ -0,0 +1,27 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.data.tables;
+
+import java.util.Vector;
+import java.util.HashMap;
+
+public interface TableMappingInterface
+{
+   public Object getKey() throws Exception;
+   
+   //Used for inserting into database   
+   public Vector toVector() throws Exception;
+   //Used for updating viewinfo in database   
+   public HashMap toHashMap() throws Exception;
+}
