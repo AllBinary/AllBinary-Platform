952a1174a54fc2da7340d56a2d6fe2f8ae3498f5
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:37:03 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/business/user/commerce/shipping/ShippingMethodData.java
@@ -0,0 +1,31 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.commerce.shipping;
+
+public class ShippingMethodData
+{
+   public ShippingMethodData()
+   {
+   }
+      
+   public static final String DESCRIPTION = "SHIPPINGMETHOD_DESCRIPTION";
+   public static final String DEFAULT = "SHIPPINGMETHOD_DEFAULT";
+   public static final String NAME = "SHIPPINGMETHOD_NAME";
+
+   //This specifies a group of items with the same shipping data - address and method
+   public static final String GROUP = "SHIPPINGMETHOD_GROUP";
+   
+   public static final String COST = "SHIPPINGMETHOD_COST";
+   
+}
