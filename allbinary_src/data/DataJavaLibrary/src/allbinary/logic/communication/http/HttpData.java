bc12fceea230a51fe137f33c2c6ff3088a856691
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:39:19 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/logic/communication/http/HttpData.java
@@ -0,0 +1,27 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.communication.http;
+
+import abcs.logic.basic.math.MathData;
+
+public class HttpData
+{
+   private HttpData()
+   {
+   }
+   
+   public static String EQUALS = MathData.getInstance().EQUALS;
+   public static String PARAM_SEP = "&";
+   public static String URL_PARAM_SEP = "?";
+}
