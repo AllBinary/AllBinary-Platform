3e501f16ce80458137ba93aad9e079e0c528b763
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 23:45:24 -0500

    added

---
+++ j2me/games/GameInputLibrary/src/allbinary/game/input/event/GameKeyEventUtil.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.input.event;
+
+public class GameKeyEventUtil
+{
+
+   private GameKeyEventUtil()
+   {
+   }
+
+   public static int getKey(Object object)
+   {
+      //Integer integer = (Integer) object;
+      //return integer.intValue();
+      GameKeyEvent gameKeyEvent = (GameKeyEvent) object;
+      return gameKeyEvent.getKey();
+   }
+}
