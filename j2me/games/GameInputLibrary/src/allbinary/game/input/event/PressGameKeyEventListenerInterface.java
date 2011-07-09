9a250533b2e1754e1f46f28761361449bfe08ccf
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-08 23:45:40 -0500

    added

---
+++ j2me/games/GameInputLibrary/src/allbinary/game/input/event/PressGameKeyEventListenerInterface.java
@@ -0,0 +1,22 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.input.event;
+
+import allbinary.logic.basic.util.event.EventListenerInterface;
+
+public interface PressGameKeyEventListenerInterface 
+   extends EventListenerInterface
+{
+    void onPressGameKeyEvent(GameKeyEvent gameKeyEvent) throws Exception;
+}
