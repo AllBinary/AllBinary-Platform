509de89eb10ccd6c2661599aa411673849093ea0
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 01:37:52 -0500

    added

---
+++ allbinary_src/data/DataJavaLibrary/src/allbinary/business/user/role/UserRoleData.java
@@ -0,0 +1,24 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.role;
+
+public class UserRoleData
+{
+   private UserRoleData()
+   {
+   }
+
+   public static final String NAME = "ROLE";
+   //_NAME
+}
