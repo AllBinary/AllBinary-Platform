88e421900b9179cbd48ad588653e755a9b354472
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-09 02:16:52 -0500

    added

---
+++ allbinary_src/data/EntitiesBlisketBaseJavaLibrary/src/allbinary/data/tables/user/commerce/money/payment/transaction/TransactionResultEntityFactory.java
@@ -0,0 +1,66 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.data.tables.user.commerce.money.payment.transaction;
+
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+
+public class TransactionResultEntityFactory
+{
+    private static final TransactionResultEntityFactory instance =
+            new TransactionResultEntityFactory();
+
+    /**
+     * @return the instance
+     */
+    public static TransactionResultEntityFactory getInstance() {
+        return instance;
+    }
+
+   //private final String CLASSNAME = "allbinary.data.tables.transaction.TransactionResultEntity";
+   
+   private TransactionResultEntityFactory()
+   {
+   }
+   
+   public TransactionResultEntity getTransactionResultEntityInstance() //throws LicensingException
+   {
+      try
+      {
+         //Object object = AbeFactory.getInstance(CLASSNAME);
+         //return (TransactionEntityInterface) InterfaceCastProxy.newInstance(object);
+         return new allbinary.data.tables.user.commerce.money.payment.transaction.TransactionResultEntity();
+      }
+      /*
+      catch(LicensingException e)
+      {
+         String error = "Failed to get instance";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.ENTITYFACTORYERROR))
+         {
+            LogUtil.put(error,"TransactionEntityFactory","getInstance()",e);
+         }
+         throw e;
+      }
+       */
+      catch(Exception e)
+      {
+         String error = "Failed to get instance";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.ENTITYFACTORYERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,"TransactionResultEntityFactory","getInstance()",e));
+         }
+         return null;
+      }
+   }
+}
