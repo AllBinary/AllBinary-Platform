f6621cf100edc208931cc5e97e5c064bc3cbee3b
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-10 05:33:00 -0500

    added

---
+++ j2me/games/GameSoundsJavaLibrary/src/allbinary/media/audio/CompositeSound.java
@@ -0,0 +1,40 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.audio;
+
+public class CompositeSound extends Sound
+{    
+    protected CompositeSound(String resource)
+    {
+        super(resource);
+    }
+
+    public void init() throws Exception
+    {
+        //player = AllBinaryMediaManager.createPlayer(this.getResource());
+        
+        player = new PlayerComposite(
+                AllBinaryMediaManager.createPlayer(this.getResource()));
+    }
+    /*
+    if(this.playerComposite.getMediaTime() == 0)
+    {
+       if(this.playerComposite.setMediaTime(500) == -1)
+       {
+           LogUtil.put(LogFactory.getInstance("Unable To Set Media Time", this, CommonStrings.getInstance().INIT));
+       }
+       LogUtil.put(LogFactory.getInstance("Set MediaTime: " + this.playerComposite.getMediaTime(), this, CommonStrings.getInstance().INIT));
+    }
+     */
+}
