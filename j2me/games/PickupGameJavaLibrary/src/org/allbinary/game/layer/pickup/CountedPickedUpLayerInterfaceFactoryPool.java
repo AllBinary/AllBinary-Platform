a38e73e85beb8ffddfe95b67f737334a54be2423
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-10 05:44:56 -0500

    added

---
+++ j2me/games/PickupGameJavaLibrary/src/org/allbinary/game/layer/pickup/CountedPickedUpLayerInterfaceFactoryPool.java
@@ -0,0 +1,43 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.game.layer.pickup;
+
+import org.allbinary.util.BasicArrayList;
+
+public class CountedPickedUpLayerInterfaceFactoryPool
+{
+    private static CountedPickedUpLayerInterfaceFactoryPool SINGLETON;
+    
+    private BasicArrayList list;
+    
+    private CountedPickedUpLayerInterfaceFactoryPool()
+    {
+        list = new BasicArrayList();
+    }
+    
+    public BasicArrayList getList()
+    {
+            return list;
+    }
+    
+    public static CountedPickedUpLayerInterfaceFactoryPool getInstance()
+    {
+        return SINGLETON;
+    }
+    
+    public static void init()
+    {
+        SINGLETON = new CountedPickedUpLayerInterfaceFactoryPool();
+    }
+}
