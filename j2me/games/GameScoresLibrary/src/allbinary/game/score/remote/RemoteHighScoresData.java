e4aebd6af46ab0c491a5d6afc63605263ce1ab95
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-10 05:42:44 -0500

    added

---
+++ j2me/games/GameScoresLibrary/src/allbinary/game/score/remote/RemoteHighScoresData.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.score.remote;
+
+public class RemoteHighScoresData
+{
+    private static final RemoteHighScoresData SINGLETON = new RemoteHighScoresData();
+    
+    public static final RemoteHighScoresData getInstance()
+    {
+        return SINGLETON;
+    }
+    
+    public String CUSTOMER_USER_NAME = "CUSTOMER_USER_NAME";
+    public String DISPLAY_NAME = "DISPLAY_NAME";
+    public String SCORE = "SCORE";
+    public String GAME_INFO = "GAME_INFO";
+    public String GAME_CONFIGURATION = "GAME_CONFIGURATION";
+}
