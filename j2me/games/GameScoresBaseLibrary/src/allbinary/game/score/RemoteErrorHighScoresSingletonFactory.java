bf7e8a061fb9e2033d944597b42bcfce6a71974f
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-10 05:36:48 -0500

    added

---
+++ j2me/games/GameScoresBaseLibrary/src/allbinary/game/score/RemoteErrorHighScoresSingletonFactory.java
@@ -0,0 +1,27 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.score;
+
+import abcs.logic.basic.string.CommonStrings;
+
+public class RemoteErrorHighScoresSingletonFactory
+{
+    private static final HighScores SINGLETON = new HighScores("Loading", "Remote High Scores Did Not Load", 
+            CommonStrings.getInstance().PERIODS);
+    
+    public static final HighScores getInstance()
+    {
+        return SINGLETON;
+    }
+}
