cc194ee0297938922700153189c0eed9c28b1ca2
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-10 06:20:24 -0500

    added

---
+++ j2me/games/WeaponLayerJavaLibrary/src/allbinary/game/layer/weapon/DamageWeaponLayer.java
@@ -0,0 +1,55 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.layer.weapon;
+
+import org.allbinary.physics.movement.Movement;
+
+import allbinary.animation.Animation;
+import allbinary.graphics.Rectangle;
+import allbinary.view.ViewPosition;
+
+public class DamageWeaponLayer extends WeaponLayer
+{
+    public DamageWeaponLayer(Movement movement,
+            Animation animationInterface,
+            Rectangle rectangle, ViewPosition viewPosition)
+            throws Exception
+    {
+        super(movement, animationInterface, rectangle, viewPosition);
+        
+        this.setCollidableInferface(new CollidableDamageWeaponBehavior(this, true));
+    }
+
+    public DamageWeaponLayer(Movement movement,
+            Animation animationInterface,
+            Animation destroyedAnimationInterface,
+            Rectangle rectangle, ViewPosition viewPosition)
+            throws Exception
+    {
+        super(movement, animationInterface, destroyedAnimationInterface, rectangle, viewPosition);
+        
+        this.setCollidableInferface(new CollidableDamageWeaponBehavior(this, true));
+    }
+
+    public DamageWeaponLayer(Movement movement,
+            Animation animationInterface,
+            Animation destroyedAnimationInterface,
+            Rectangle rectangle, ViewPosition viewPosition, String username, int actorSessionId, int multiPlayerType)
+            throws Exception
+    {
+        super(movement, animationInterface, destroyedAnimationInterface, rectangle, viewPosition, username, actorSessionId, multiPlayerType);
+        
+        this.setCollidableInferface(new CollidableDamageWeaponBehavior(this, true));
+    }
+}
