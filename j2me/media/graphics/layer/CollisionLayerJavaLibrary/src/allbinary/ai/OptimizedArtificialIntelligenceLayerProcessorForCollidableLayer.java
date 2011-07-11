20cd508ebcaf035d44f434dc29184d37c2470225
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-11 04:42:28 -0500

    added

--- j2me/media/graphics/layer/CollisionLayerJavaLibrary/src/allbinary/ai/OptimizedArtificialIntelligenceLayerProcessorForCollidableLayer.java
+++ j2me/media/graphics/layer/CollisionLayerJavaLibrary/src/allbinary/ai/OptimizedArtificialIntelligenceLayerProcessorForCollidableLayer.java
@@ -1,14 +1,16 @@
 /*
- * AllBinary Open License Version 1
- * Copyright (c) 2011 AllBinary
- *
- * Created By: Travis Berthelot
- * Date: August 27, 2006
- *
- *
- * Modified By         When       ?
- *
- */
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
 package allbinary.ai;
 
 import org.allbinary.game.layer.CollidableCompositeLayer;
