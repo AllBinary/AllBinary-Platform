39bd23bf3aff81b15597affe464c5cb05a274309
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-11 02:54:19 -0500

    added

---
+++ j2me/games/weapon/WeaponMineJavaLibrary/src/allbinary/game/vector/MineVectorData.java
@@ -0,0 +1,25 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.vector;
+
+public class MineVectorData
+{
+   private MineVectorData()
+   {
+   }
+   
+  public static int points[][][] = {  
+  {{10,5},{2,6},{1000,1000},{2,2},{10,9},{1000,1000},{10,2},{2,9},{1000,1000},{2,5},{10,6},{1000,1000},{7,9},{5,2},{1000,1000},{7,2},{5,9},{1000,1000}}};
+   
+}
