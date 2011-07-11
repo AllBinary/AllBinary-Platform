9f7d7a1a4074db8310406e1a216d8ebd537aac8c
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-11 02:53:53 -0500

    added

---
+++ j2me/games/weapon/WeaponMineJavaLibrary/src/allbinary/game/layer/weapon/mine/MineLayerCircularStaticPool.java
@@ -0,0 +1,34 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.layer.weapon.mine;
+
+import allbinary.game.layer.weapon.WeaponLayerCircularPool;
+import allbinary.view.ViewPositionFactoryInterface;
+
+public class MineLayerCircularStaticPool {
+
+	private static WeaponLayerCircularPool WEAPON_LAYER_CIRCULAR_STATIC_POOL = 
+		new WeaponLayerCircularPool();
+
+    public static void init(ViewPositionFactoryInterface viewPositionFactoryInterface) throws Exception
+    {
+    	MineCountedPickedUpLayerInterfaceFactory.init();
+    	WEAPON_LAYER_CIRCULAR_STATIC_POOL.init(new MineLayerFactory(viewPositionFactoryInterface), 20);
+    }
+
+    public static WeaponLayerCircularPool getInstance()
+    {
+    	return WEAPON_LAYER_CIRCULAR_STATIC_POOL;
+    }
+}
