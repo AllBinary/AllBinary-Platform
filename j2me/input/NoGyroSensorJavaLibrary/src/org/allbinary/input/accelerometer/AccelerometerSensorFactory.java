1d2b7e6054ecf47c6f41af62c2049ed87461db96
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 23:22:43 -0500

    added

---
+++ j2me/input/NoGyroSensorJavaLibrary/src/org/allbinary/input/accelerometer/AccelerometerSensorFactory.java
@@ -0,0 +1,63 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.input.accelerometer;
+
+import org.allbinary.input.gyro.AllBinaryOrientationSensor;
+
+import allbinary.game.configuration.feature.Features;
+import allbinary.game.configuration.feature.SensorFeatureFactory;
+import org.allbinary.input.gyro.AllBinaryOrientationSensor;
+
+public class AccelerometerSensorFactory
+{
+    //private static final AccelerometerSensorFactory instance = new AccelerometerSensorFactory();
+    
+    private static AllBinaryOrientationSensor allBinaryAccelerometerSensor;
+    
+    public static void init() throws Exception
+    {
+        //PreLogUtil.put("Start", instance, "init");
+        
+        Features features = 
+            Features.getInstance();
+        SensorFeatureFactory sensorFeatureFactory = 
+            SensorFeatureFactory.getInstance();
+        
+        if (features.isFeature(
+                sensorFeatureFactory.ORIENTATION_SENSORS))
+        {
+            throw new Exception();
+        }
+        else if (features.isFeature(
+                sensorFeatureFactory.SIMULATED_ORIENTATION_SENSORS))
+        {
+            throw new Exception();
+        }
+        else if (features.isFeature(
+                sensorFeatureFactory.NO_ORIENTATION))
+        {
+            allBinaryAccelerometerSensor = new NoAccelerometerSensor();
+        }
+        else
+        {
+            throw new Exception("Not Such SensorFeature");
+        }
+    }
+    
+    public static AllBinaryOrientationSensor getInstance()
+    {
+        return allBinaryAccelerometerSensor;
+    }
+
+}
