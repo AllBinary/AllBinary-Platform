22651bd7fd3fde5458fceaa015d22c130b748aec
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:35:36 -0500

    added

---
+++ graphics/ImageJavaLibrary/src/allbinary/media/image/io/IIOImageCacheable.java
@@ -0,0 +1,62 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.image.io;
+
+import java.awt.image.BufferedImage;
+
+import javax.imageio.IIOImage;
+
+import com.abcs.logic.util.cache.CacheableInterface;
+
+public class IIOImageCacheable
+    implements CacheableInterface
+{    
+    private String key;
+ 
+    private IIOImage iioImage;
+
+    public IIOImageCacheable(String key)
+    {
+        this.key = key;
+    }
+    
+    public Object getKey()
+    {
+        return this.key;
+    }
+    
+    public void setBufferedImage(BufferedImage bufferedImage)
+    {
+        this.setIioImage(new IIOImage(bufferedImage, null, null));
+    }
+
+    public IIOImage getIioImage()
+    {
+        return iioImage;
+    }
+
+    private void setIioImage(IIOImage iioImage)
+    {
+        this.iioImage = iioImage;
+    }
+    
+    public String toString()
+    {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(this.getClass().getName());
+        stringBuffer.append(" Reusable IIOImage");
+        return stringBuffer.toString();
+    }
+    
+}
