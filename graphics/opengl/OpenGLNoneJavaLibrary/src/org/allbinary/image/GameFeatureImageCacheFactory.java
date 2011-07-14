b04f398afd9f1c66c77bed5e24b6c33cadb906c9
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:56:16 -0500

    added

---
+++ graphics/opengl/OpenGLNoneJavaLibrary/src/org/allbinary/image/GameFeatureImageCacheFactory.java
@@ -0,0 +1,36 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.image;
+
+import allbinary.image.ImageCache;
+import allbinary.image.ImageCacheFactory;
+
+public class GameFeatureImageCacheFactory
+{
+    public static final ImageCache getInstance()
+    {
+            return ImageCacheFactory.getInstance();
+    }
+    
+    public static void init()
+    {
+        ImageCacheFactory.init();
+    }
+
+    public static void releaseAll()
+    {
+        ImageCacheFactory.getInstance().releaseAll();
+    }
+    
+}
