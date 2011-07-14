1ce14ef9b2a71fa1d146541c6c1f1e3ee6b6b80a
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:02:50 -0500

    added

---
+++ graphics/AllBinaryGraphicsJavaApplication/src/allbinary/graphics/j2me/workarea/tools/GraphicItemBuilder.java
@@ -0,0 +1,37 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.graphics.j2me.workarea.tools;
+
+import org.w3c.dom.Node;
+
+public class GraphicItemBuilder
+{
+   private GraphicItemBuilder()
+   {
+   }
+   
+   public static GraphicItemInterface getInstance(Node node) throws Exception
+   {
+      String graphicItemName = node.getNodeName();      
+
+      if(graphicItemName.compareTo(PointsDomUtil.getInstance().LINES) == 0)
+      {
+          return new LinesGraphicItem(node);
+      }
+      else
+      {
+          throw new Exception("Graphic Item Does Not Exist: " + graphicItemName);
+      }
+   }
+}
