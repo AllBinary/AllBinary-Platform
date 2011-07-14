559e746b17570b48e24457ad13bafd382ea91406
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 23:04:20 -0500

    added

---
+++ j2me/input/ME/GameInputMEJavaLibrary/src/allbinary/game/input/J2MEGameKey.java
@@ -0,0 +1,48 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.input;
+
+import abcs.logic.basic.string.StringUtil;
+
+public class J2MEGameKey extends Input
+{
+    protected J2MEGameKey(int key, String keyName)
+    {
+        super(key, keyName);
+
+        int id = this.getId();
+        if(id >= 0)
+        {
+            inputIntegerArray[id] = this;
+        }
+    }
+
+    public String toString()
+    {
+        return "J2MEGameKey: " + super.toString();
+    }
+
+    public static String getString(int keyCode)
+    {
+        Input input = Input.getInstance(keyCode);
+        if(input != null)
+        {
+            return input.getName();
+        }
+        else
+        {
+            return StringUtil.getInstance();
+        }
+    }
+}
