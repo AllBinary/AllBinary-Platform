deccba0fe4eb07758d85f9896fc8b1a6b0cb7c23
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 02:27:10 -0500

    added

---
+++ j2me/lib/TrigBaseLibrary/src/allbinary/math/FrameUtil.java
@@ -0,0 +1,56 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.math;
+
+public class FrameUtil
+{
+    private static final FrameUtil instance = new FrameUtil();
+
+    public static FrameUtil getInstance()
+    {
+        return instance;
+    }
+
+    private FrameUtil()
+    {
+    }
+
+    // TWB - Note the angle is actually 90 degrees off
+    public int getFrameForAngle(short angle, int angleIncrement)
+    {
+        return this.adjustAngleToFrameAngle(angle) / angleIncrement;
+    }
+
+    public int getFrameAngle(int frame, int angleIncrement)
+    {
+        int frameAngle = angleIncrement * frame - 90;
+        return this.adjustAngleToFrameAngle(frameAngle);
+    }
+
+    public short adjustAngleToFrameAngle(int currentAngle)
+    {
+        if (currentAngle > 359)
+        {
+            return (short) (currentAngle - AngleFactory.getInstance().TOTAL_ANGLE);
+        }
+
+        if (currentAngle < 0)
+        {
+            return (short) (currentAngle + AngleFactory.getInstance().TOTAL_ANGLE);
+        }
+
+        return (short) currentAngle;
+    }
+
+}
