c9035407b5f745e5f72145e635e098ca1cc4d93e
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 23:16:23 -0500

    added

---
+++ j2me/input/MotionGestureBaseJavaLibrary/src/allbinary/input/motion/gesture/observer/MotionGestureEventListener.java
@@ -0,0 +1,29 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.input.motion.gesture.observer;
+
+
+public interface MotionGestureEventListener extends BaseMotionGestureEventListener {
+    
+    void onPressedMotionGestureEvent(MotionGestureEvent ev);
+    void onUpMotionGestureEvent(MotionGestureEvent ev);
+    void onDownMotionGestureEvent(MotionGestureEvent ev);
+    void onLeftMotionGestureEvent(MotionGestureEvent ev);
+    void onRightMotionGestureEvent(MotionGestureEvent ev);
+    void onDiagonalDownRightMotionGestureEvent(MotionGestureEvent ev);
+    void onDiagonalDownLeftMotionGestureEvent(MotionGestureEvent ev);
+    void onDiagonalUpRightMotionGestureEvent(MotionGestureEvent ev);
+    void onDiagonalUpLeftMotionGestureEvent(MotionGestureEvent ev);
+    void released(MotionGestureEvent ev) throws Exception;
+}
