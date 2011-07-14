25a3a27eb8cbdcdfce5da7ff371b0aa907fcf84b
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:08:13 -0500

    added

---
+++ graphics/AllBinaryGraphicsUserInterfaceJavaLibrary/src/allbinary/gui/swing/workers/JListSwingWorker.java
@@ -0,0 +1,60 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.gui.swing.workers;
+
+import javax.swing.JList;
+import javax.swing.ListModel;
+import javax.swing.SwingWorker;
+
+public class JListSwingWorker extends SwingWorker
+{
+    private JList jList;
+    private ListModel listModel;
+    
+    public JListSwingWorker(JList jList, ListModel listModel)
+    {
+        this.setJList(jList);
+        this.setListModel(listModel);
+    }
+    
+    public Object doInBackground()
+    {
+        return null;
+    }
+    
+    protected void done()
+    {
+        this.getJList().setModel(this.getListModel());
+    }
+
+    private JList getJList()
+    {
+        return jList;
+    }
+
+    private void setJList(JList jList)
+    {
+        this.jList = jList;
+    }
+
+    private ListModel getListModel()
+    {
+        return listModel;
+    }
+
+    private void setListModel(ListModel listModel)
+    {
+        this.listModel = listModel;
+    }
+}
