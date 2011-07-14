f3f17dea9e90476314290a589f9a23c3082cd0de
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 23:25:10 -0500

    added

---
+++ j2me/input/NoTouchButtonJavaLibrary/src/allbinary/input/motion/AllMotionRecognizer.java
@@ -0,0 +1,50 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.input.motion;
+
+/**
+ * 
+ * @author user
+ */
+public class AllMotionRecognizer
+{    
+    private final int id;
+
+    private static int index = 0;
+
+    public AllMotionRecognizer()
+    {
+        this.id = index++;
+    }
+    
+    public void processStartMotionEvent(int x, int y, int modifiers)
+            throws Exception
+    {
+    }
+
+    public void processEndMotionEvent(int x, int y, int modifiers)
+            throws Exception
+    {
+    }
+
+    public void processDraggedMotionEvent(int x, int y, int modifiers)
+            throws Exception
+    {
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+}
