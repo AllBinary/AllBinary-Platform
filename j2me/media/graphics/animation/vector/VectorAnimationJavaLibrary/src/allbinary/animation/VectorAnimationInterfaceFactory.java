c3ae5102049ce4b588bbdc38308696b44eb1b27a
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 01:03:21 -0500

    added

---
+++ j2me/media/graphics/animation/vector/VectorAnimationJavaLibrary/src/allbinary/animation/VectorAnimationInterfaceFactory.java
@@ -0,0 +1,41 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.animation;
+
+import allbinary.graphics.color.BasicColor;
+
+public class VectorAnimationInterfaceFactory implements
+		AnimationInterfaceFactoryInterface {
+
+	private int currentPoints[][][];
+	private BasicColor basicColor;
+
+	public VectorAnimationInterfaceFactory(int[][][] currentPoints, BasicColor basicColor) {
+		this.currentPoints = currentPoints;
+		this.setBasicColor(basicColor);
+	}
+
+	public Animation getInstance() throws Exception {
+		return new VectorAnimation(this.currentPoints, this.getBasicColor());
+	}
+
+	protected void setBasicColor(BasicColor basicColor) {
+		this.basicColor = basicColor;
+	}
+
+	protected BasicColor getBasicColor() {
+		return basicColor;
+	}
+
+}
