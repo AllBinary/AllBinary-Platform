1f5772813f766f32c51935904dc99c1f95d788d6
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 02:26:47 -0500

    added

---
+++ j2me/lib/TrigBaseLibrary/src/allbinary/math/AngleIncrementInfoFactory.java
@@ -0,0 +1,48 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.math;
+
+public class AngleIncrementInfoFactory
+{
+    private static final AngleIncrementInfoFactory instance = new AngleIncrementInfoFactory();
+
+    public static AngleIncrementInfoFactory getInstance()
+    {
+        return instance;
+    }
+    
+    private final AngleIncrementInfo[] angleIncrementInfoArray = new AngleIncrementInfo[AngleFactory.getInstance().TOTAL_ANGLE];
+    
+    public AngleIncrementInfo getInstance(int angleIncrement)
+    {
+       //Integer integer = SmallIntegerSingletonFactory.getInstance(angleIncrement);
+        
+        int halfAngleIncrement = (angleIncrement >> 1);
+        
+       AngleIncrementInfo angleIncrementInfo = angleIncrementInfoArray[halfAngleIncrement];
+       //hashtable.get(integer);
+       
+       if(angleIncrementInfo == null)
+       {
+           //Integer integer = SmallIntegerSingletonFactory.getInstance(angleIncrement);
+           angleIncrementInfo =
+                   //new AngleIncrementInfo(integer);
+                   new AngleIncrementInfo(angleIncrement);
+          //hashtable.put(integer, angleIncrementInfo);
+          angleIncrementInfoArray[halfAngleIncrement] = angleIncrementInfo;
+       }
+       
+       return angleIncrementInfo;
+    }
+}
