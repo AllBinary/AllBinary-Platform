7fff15ceab0e16c4d4fd767f22847db64bc1285f
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 22:47:13 -0500

    added

---
+++ j2me/input/JSEGyroSensorJavaLibrary/src/org/allbinary/input/AllBinarySensorManager.java
@@ -0,0 +1,50 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.input;
+
+import org.allbinary.util.BasicArrayList;
+
+public class AllBinarySensorManager
+{
+    public final String ORIENTATION_SENSOR_INPUT = "Orientation Sensor Input";
+    public final String ORIENTATION_TYPE = "Orientation Type";
+
+    private static final AllBinarySensorManager SINGLETON = new AllBinarySensorManager();
+
+    private AllBinarySensorManager()
+    {
+    }
+
+    public static AllBinarySensorManager getInstance()
+    {
+        return SINGLETON;
+    }
+
+    //private final AllBinarySensor[] allBinarySensorArray = new AllBinarySensor[0];
+    //public AllBinarySensor[] init()
+    public void init()
+    throws Exception
+    {
+    }
+    
+    private final BasicArrayList list = new BasicArrayList();
+    public BasicArrayList getSensorNamesList()
+    {
+        return list;
+    }
+
+    public void shutdown() throws Exception
+    {
+    }
+}
