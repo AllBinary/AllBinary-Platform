01790ff2cfda3aa399d6f709aac13d44e6d4a875
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 00:24:23 -0500

    added

---
+++ j2me/media/graphics/animation/AnimationLibrary/src/allbinary/animation/vector/RectangleFilledAnimation.java
@@ -0,0 +1,60 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.animation.vector;
+
+import javax.microedition.lcdui.Graphics;
+
+import allbinary.animation.Animation;
+import allbinary.graphics.color.BasicColor;
+import allbinary.graphics.color.ColorCompositeInterface;
+
+public class RectangleFilledAnimation 
+extends Animation 
+implements ColorCompositeInterface
+{
+   private int width;
+   private int height;
+   
+   public RectangleFilledAnimation(int width, int height, BasicColor basicColor) {
+      this.width = width;
+      this.height = height;
+      this.setBasicColor(basicColor);
+   }
+
+   public void nextFrame() {
+   }
+
+   public void paint(Graphics graphics, int x, int y) {
+       this.basicColorUtil.setBasicColor(
+               graphics, this.getBasicColor(), this.getColor());
+
+      graphics.fillRect(x, y, width, height);
+   }
+
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+
+    /**
+     * @param height the height to set
+     */
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+}
