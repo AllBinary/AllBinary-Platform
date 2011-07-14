9cce8d543b8fa49ef5a91164c1227babc18eeb83
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:22:41 -0500

    added

---
+++ graphics/ImageComparisonJavaLibrary/src/allbinary/media/image/comparison/motion/MotionRectanglesResultsFrameCacheable.java
@@ -0,0 +1,57 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.image.comparison.motion;
+
+import java.awt.image.BufferedImage;
+
+import com.abcs.logic.util.cache.CacheableInterface;
+
+public class MotionRectanglesResultsFrameCacheable
+    implements CacheableInterface
+{
+    private MotionRectangles motionRectangles;
+    
+    private Long frame;
+    
+    public MotionRectanglesResultsFrameCacheable(
+        MotionRectangles motionRectangles, Long frame)
+    {
+        this.frame = frame;
+        this.setMotionRectangles(motionRectangles);
+    }
+    
+    public Object getKey()
+    {
+        return this.frame;
+    }    
+
+    public MotionRectangles getMotionRectangles()
+    {
+        return motionRectangles;
+    }
+
+    public void setMotionRectangles(MotionRectangles motionRectangles)
+    {
+        this.motionRectangles = motionRectangles;
+    }
+    
+    public String toString()
+    {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(this.getClass().getName());
+        stringBuffer.append(" Frame: ");
+        stringBuffer.append(this.getKey());
+        return stringBuffer.toString();
+    }
+}
