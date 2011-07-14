c1fc4642e66b023d5a3134d879f0ec63db3a2172
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 00:26:38 -0500

    added

---
+++ j2me/media/graphics/animation/AnimationResourceJavaLibrary/src/org/allbinary/game/layer/resources/OnDemandResources.java
@@ -0,0 +1,34 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.game.layer.resources;
+
+import allbinary.animation.resource.BaseResourceAnimationInterfaceFactoryInterfaceFactory;
+import allbinary.image.ImageCache;
+
+public class OnDemandResources
+{
+    protected OnDemandResources()
+    {
+    }
+    
+    public int init(
+            ImageCache imageCache,
+            BaseResourceAnimationInterfaceFactoryInterfaceFactory 
+            resourceAnimationInterfaceFactoryInterfaceFactory, 
+            int portion, String loadingString, int index)
+        throws Exception
+    {
+        return 0;
+    }
+}
