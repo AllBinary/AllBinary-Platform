cfc331de16f7cb2083a3f6ed9cb4865ec827741d
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:21:13 -0500

    added

---
+++ graphics/ImageComparisonJavaLibrary/src/allbinary/media/image/comparison/motion/AllMotionRectanglesResultsCacheSingleton.java
@@ -0,0 +1,52 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.image.comparison.motion;
+
+import abcs.logic.communication.log.Log;
+import abcs.logic.communication.log.LogUtil;
+import com.abcs.logic.util.cache.CacheInterface;
+import com.abcs.logic.util.cache.CacheInterfaceFactory;
+import com.abcs.logic.util.cache.CachePolicy;
+import com.abcs.logic.util.cache.CacheType;
+
+public class AllMotionRectanglesResultsCacheSingleton
+{
+    private static CacheInterface cacheInterface = null;
+    
+    static
+    {
+        try
+        {
+            LogUtil.put(new Log("Start", "MotionRectanglesResultsCacheSingleton", "Static Block"));
+            
+            cacheInterface = CacheInterfaceFactory.getInstance(
+                CacheType.SHIFT_ONE_CACHE, CachePolicy.ONE_MINUTE_FIVE_MAX);
+            
+            LogUtil.put(new Log("End", "MotionRectanglesResultsCacheSingleton", "Static Block"));
+        }
+        catch(Exception e)
+        {
+            LogUtil.put(new Log("Exception", "MotionRectanglesResultsCacheSingleton", "Static Block", e));
+        }
+    }
+    
+    private AllMotionRectanglesResultsCacheSingleton()
+    {
+    }
+    
+    public static CacheInterface getInstance()
+    {
+        return cacheInterface;
+    }    
+}
