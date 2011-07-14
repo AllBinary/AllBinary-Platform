a4446639c44529333d6853529f0d08bc7c3d0f53
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 22:47:44 -0500

    added

---
+++ j2me/input/JSEGyroSensorJavaLibrary/src/org/allbinary/input/gyro/OrientationToGameKeyEvent.java
@@ -0,0 +1,61 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.input.gyro;
+
+import allbinary.game.configuration.feature.Features;
+import allbinary.game.configuration.feature.SensorFeatureFactory;
+import allbinary.logic.math.Orientation;
+
+public class OrientationToGameKeyEvent
+{
+    private AllBinaryOrientationSensor allBinaryGyroSensor;
+    
+    private final int MIN = 15; 
+    
+    public OrientationToGameKeyEvent() throws Exception
+    {
+        
+            if(Features.getInstance().isFeature(
+                SensorFeatureFactory.getInstance().NO_ORIENTATION))
+            {
+                allBinaryGyroSensor = new NoGyroSensor();
+            }
+                else
+                {
+                    throw new Exception("Not Such Feature");
+                }
+        
+        //allBinaryGyroSensor.init();
+    }
+
+    public void setAllBinaryGyroSensor(AllBinaryOrientationSensor allBinaryGyroSensor)
+    {
+        this.allBinaryGyroSensor = allBinaryGyroSensor;
+    }
+
+    public AllBinaryOrientationSensor getAllBinaryGyroSensor()
+    {
+        return allBinaryGyroSensor;
+    }
+        
+    public void process() throws Exception
+    {
+    }
+    
+    private void updateGameKeyEvents(Orientation orientation, 
+            int yaw, int pitch, int roll)
+    throws Exception
+    {
+    }
+}
