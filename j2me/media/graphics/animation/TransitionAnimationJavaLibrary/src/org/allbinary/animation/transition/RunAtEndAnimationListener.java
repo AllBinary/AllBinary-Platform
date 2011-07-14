593877eb5ad2191708c2e58c4a1419aaed4c17db
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 00:55:23 -0500

    added

---
+++ j2me/media/graphics/animation/TransitionAnimationJavaLibrary/src/org/allbinary/animation/transition/RunAtEndAnimationListener.java
@@ -0,0 +1,33 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.animation.transition;
+
+public class RunAtEndAnimationListener implements AnimationListenerInterface
+{
+    protected Runnable runnable;
+    
+    protected RunAtEndAnimationListener(Runnable runnable) {
+        this.runnable = runnable;
+    }
+    
+    public void onAnimationStart() {
+    }
+
+    public void onAnimationEnd() {
+    }
+
+    public void onAnimationRepeat() {
+    }
+
+}
