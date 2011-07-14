33fdb5cc5c44943b51a5fe612f58c211474eef46
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 01:03:38 -0500

    added

---
+++ j2me/media/graphics/animation/vector/VectorAnimationJavaLibrary/src/allbinary/animation/VectorExplosionAnimationFactory.java
@@ -0,0 +1,49 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.animation;
+
+import allbinary.graphics.color.BasicColor;
+
+public class VectorExplosionAnimationFactory 
+    implements ProceduralAnimationInterfaceFactoryInterface{
+
+	private BasicColor basicColor;
+	
+	public VectorExplosionAnimationFactory(BasicColor basicColor) {
+		this.setBasicColor(basicColor);
+	}
+
+	private final VectorExplosionGenerator vectorExplosionGenerator = VectorExplosionGenerator.getInstance();
+	
+	public Animation getInstance(Animation animationInterface)
+	throws Exception 
+	{
+		VectorAnimation vectorRotationAnimationInterface = 
+			(VectorAnimation) animationInterface;
+		int frame = vectorRotationAnimationInterface.getFrame();
+		int[][] framePoints = vectorRotationAnimationInterface.getPoints(frame);
+		int[][][] points = vectorExplosionGenerator.getInstance(
+		        framePoints, 6, vectorExplosionGenerator.ROTATION);
+		
+		return new VectorRotationAnimation(points, this.getBasicColor());
+	}
+
+	private void setBasicColor(BasicColor basicColor) {
+		this.basicColor = basicColor;
+	}
+
+	private BasicColor getBasicColor() {
+		return basicColor;
+	}
+}
