811041913eaf7fbd369fa94bfdce2d1510f55042
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:32:59 -0500

    added

---
+++ graphics/ImageJavaLibrary/src/allbinary/media/image/analysis/ColorRangeInterface.java
@@ -0,0 +1,34 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.image.analysis;
+
+import java.awt.Color;
+
+public interface ColorRangeInterface
+{
+    int getMinRed();
+    void setMinRed(int minRed);
+    int getMaxRed();
+    void setMaxRed(int maxRed);
+    int getMinGreen();
+    void setMinGreen(int minGreen);
+    int getMaxGreen();
+    void setMaxGreen(int maxGreen);
+    int getMinBlue();
+    void setMinBlue(int minBlue);
+    int getMaxBlue();
+    void setMaxBlue(int maxBlue);
+    
+    boolean isInRange(Color color);
+}
