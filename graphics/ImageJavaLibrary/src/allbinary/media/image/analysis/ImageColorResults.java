4a69aac2b4661ff4310c49a4f799a0cf56df907d
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:33:31 -0500

    added

---
+++ graphics/ImageJavaLibrary/src/allbinary/media/image/analysis/ImageColorResults.java
@@ -0,0 +1,56 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.image.analysis;
+
+public class ImageColorResults
+{
+    private ColorRange colorRange;
+    private ColorAverage colorAverage;
+        
+    public ImageColorResults()
+    {
+        this.setColorRange(new ColorRange());
+
+        this.getColorRange().setMaxRed(-1);
+        this.getColorRange().setMaxGreen(-1);
+        this.getColorRange().setMaxBlue(-1);
+
+        this.getColorRange().setMinRed(256);
+        this.getColorRange().setMinGreen(256);
+        this.getColorRange().setMinBlue(256);
+        
+        this.setColorAverage(new ColorAverage());
+    }
+
+    public ColorRange getColorRange()
+    {
+        return colorRange;
+    }
+
+    public void setColorRange(ColorRange colorRange)
+    {
+        this.colorRange = colorRange;
+    }
+
+    public ColorAverage getColorAverage()
+    {
+        return colorAverage;
+    }
+
+    public void setColorAverage(ColorAverage colorAverage)
+    {
+        this.colorAverage = colorAverage;
+    }
+    
+}
