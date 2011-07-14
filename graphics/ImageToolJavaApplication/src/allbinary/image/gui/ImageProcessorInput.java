d37ee4e3b0073c32c539839dd3b2d96e9b828d86
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:25:43 -0500

    added

---
+++ graphics/ImageToolJavaApplication/src/allbinary/image/gui/ImageProcessorInput.java
@@ -0,0 +1,40 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.image.gui;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ImageProcessorInput {
+
+    private BufferedImage bufferedImageArray[];
+    private File[] files;
+
+    public ImageProcessorInput(File[] files, BufferedImage bufferedImageArray[]) throws Exception {
+        super();
+
+        this.bufferedImageArray = bufferedImageArray;
+        this.files = files;
+    }
+
+   public BufferedImage[] getBufferedImageArray()
+   {
+      return bufferedImageArray;
+   }
+
+   public File[] getFiles()
+   {
+      return files;
+   }
+}
