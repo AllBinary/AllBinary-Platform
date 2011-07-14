984ac2eda9f62f861470a82009762462fa2e55cb
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 22:47:21 -0500

    added

---
+++ j2me/input/JSEGyroSensorJavaLibrary/src/org/allbinary/input/gyro/AccelerometerSensorUpdateProcessor.java
@@ -0,0 +1,55 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.input.gyro;
+
+import org.allbinary.input.accelerometer.AccelerometerSensorFactory;
+
+import allbinary.canvas.Processor;
+import allbinary.game.configuration.feature.Features;
+import allbinary.game.configuration.feature.SensorFeatureFactory;
+import allbinary.game.input.motion.action.NoCompleteMotionGestureInputEventListener;
+import allbinary.game.input.motion.action.SingleKeyPressGameKeyFromMotionGestureEventListener;
+
+public class AccelerometerSensorUpdateProcessor extends Processor
+{
+    public void process() throws Exception
+    {
+        SensorFeatureFactory sensorFeatureFactory = 
+            SensorFeatureFactory.getInstance();
+
+        // Stop/Start sensor input
+        if (Features.getInstance().isFeature(
+                sensorFeatureFactory.ORIENTATION_SENSORS))
+        {
+            AccelerometerSensorFactory.getInstance().setListener(
+                    new SingleKeyPressGameKeyFromMotionGestureEventListener());
+        }
+        else if (Features.getInstance().isFeature(
+                sensorFeatureFactory.SIMULATED_ORIENTATION_SENSORS))
+        {
+            AccelerometerSensorFactory.getInstance().setListener(
+                    new SingleKeyPressGameKeyFromMotionGestureEventListener());
+        }        
+        else if (Features.getInstance().isFeature(
+                sensorFeatureFactory.NO_ORIENTATION))
+        {
+            AccelerometerSensorFactory.getInstance().setListener(
+                    NoCompleteMotionGestureInputEventListener.getInstance());
+        }
+        else
+        {
+            throw new Exception("Sensor Feature Not Set");
+        }
+    }
+}
