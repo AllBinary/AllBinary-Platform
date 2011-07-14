629d8dde15437ace3e7adc98fe13caf5d5d27acb
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 07:12:47 -0500

    added

---
+++ j2me/android/AndroidThroughAPI2JavaLibrary/src/org/allbinary/android/input/api1/VirtualKeyboardAPI1.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.android.input.api1;
+
+import org.allbinary.android.input.VirtualKeyboard;
+
+import allbinary.input.event.VirtualKeyboardEventListenerInterface;
+import android.app.Activity;
+
+public class VirtualKeyboardAPI1 extends VirtualKeyboard
+implements VirtualKeyboardEventListenerInterface
+{
+    public static int MIN_API = 1;
+    public static int MAX_API = 2;
+    
+    public VirtualKeyboardAPI1(Activity activity)
+    {
+    }
+}
