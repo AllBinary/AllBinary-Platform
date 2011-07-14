2d45cf5b5abfd6eec909ab69889fbf91bf55c5b5
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 02:25:45 -0500

    added

---
+++ j2me/lib/MathVectorLibrary/src/allbinary/logic/math/vector/MathVectorInterface.java
@@ -0,0 +1,32 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.math.vector;
+
+import allbinary.logic.math.BasicDecimal;
+
+public interface MathVectorInterface
+{
+   //BasicDecimal getDirection();
+   int getDirection();
+
+   BasicDecimal getMagnitude();
+
+   BasicDecimal getResult();
+
+   //void setDirection(BasicDecimal basicDecimal);
+   //void setDirection(int direction);
+
+   //void setMagnitude(BasicDecimal magnitudeBasicDecimal);
+
+}
