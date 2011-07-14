683eef5c03b93f0b292d632db5916adb8270f6c3
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 23:24:09 -0500

    added

---
+++ j2me/input/NoSensorJavaLibrary/src/org/allbinary/input/AllBinarySensorManager.java
@@ -0,0 +1,61 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.input;
+
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import org.allbinary.input.accelerometer.AccelerometerSensorFactory;
+import org.allbinary.input.gyro.GyroSensorFactory;
+import org.allbinary.util.BasicArrayList;
+
+public class AllBinarySensorManager
+{
+    public final String ORIENTATION_SENSOR_INPUT = "Orientation Sensor Input";
+    public final String ORIENTATION_TYPE = "Orientation Type";
+
+    private static final AllBinarySensorManager SINGLETON = new AllBinarySensorManager();
+
+    private AllBinarySensorManager()
+    {
+        try
+        {
+            GyroSensorFactory.init();
+            AccelerometerSensorFactory.init();
+        } catch (Exception e)
+        {
+            LogUtil.put(LogFactory.getInstance("Exception", this, "Constructor", e));
+        }
+    }
+
+    public static AllBinarySensorManager getInstance()
+    {
+        return SINGLETON;
+    }
+
+    //private final AllBinarySensor[] allBinarySensorArray = new AllBinarySensor[0];
+    //public AllBinarySensor[] init()
+    public void init()
+    {
+    }
+    
+    private final BasicArrayList list = new BasicArrayList();
+    public BasicArrayList getSensorNamesList()
+    {
+        return list;
+    }
+
+    public void shutdown()
+    {
+    }
+}
