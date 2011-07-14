ad8fe0d4b0db513ba45f0e692809c4310e681e3c
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 23:23:12 -0500

    added

---
+++ j2me/input/NoGyroSensorJavaLibrary/src/org/allbinary/input/gyro/NoGyroSensor.java
@@ -0,0 +1,38 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.input.gyro;
+
+import org.allbinary.input.AllBinarySensorListener;
+
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.logic.math.Orientation;
+
+public class NoGyroSensor extends AllBinaryOrientationSensor
+implements AllBinarySensorListener
+{
+    public NoGyroSensor()
+    {
+        LogUtil.put(LogFactory.getInstance("Start", this, "Constructor"));
+    }
+    
+    public Orientation getOrientation() throws Exception
+    {
+        return Orientation.NONE;
+    }
+    
+    public void onSensorChange()
+    {
+    }
+}
