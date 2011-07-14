bfa87ba36d43bcbe663cd45d3933ff4f083e74d0
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 22:43:50 -0500

    added

---
+++ j2me/input/AllBinaryAccelerometerSensorJavaLibrary/src/org/allbinary/input/accelerometer/NoAccelerometerSensor.java
@@ -0,0 +1,31 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.input.accelerometer;
+
+import org.allbinary.input.gyro.AllBinaryOrientationSensor;
+
+import allbinary.logic.math.Orientation;
+
+public class NoAccelerometerSensor extends AllBinaryOrientationSensor
+{
+    public Orientation getOrientation() throws Exception
+    {
+        return Orientation.NONE;
+    }
+    
+    public void onSensorChange()
+    {
+    }
+    
+}
