2d2bd89dab633d68e1b30ce00e384b8b8b6ad65b
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 02:26:22 -0500

    added

---
+++ j2me/lib/TrigBaseLibrary/src/allbinary/math/Angle.java
@@ -0,0 +1,40 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.math;
+
+
+public class Angle
+{    
+    //private Integer value;
+    private short value;
+
+    protected Angle(short angle)
+    {
+        //this.setValue(SmallIntegerSingletonFactory.getInstance(angle));
+        this.setValue(angle);
+    }
+
+    //public Integer getValue()
+    public short getValue()
+    {
+        return value;
+    }
+
+    //private void setValue(Integer value)
+    private void setValue(short value)
+    {
+        this.value = value;
+    }
+
+}
