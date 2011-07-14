e2510bb338e629592d4fd65679409eb156d3813a
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 00:23:07 -0500

    added

---
+++ j2me/media/graphics/animation/AnimationLibrary/src/allbinary/animation/vector/AdjustedCircleAnimation.java
@@ -0,0 +1,76 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.animation.vector;
+
+import javax.microedition.lcdui.Graphics;
+
+import allbinary.AndroidUtil;
+import allbinary.graphics.color.BasicColor;
+import allbinary.math.AngleFactory;
+
+/**
+ *
+ * @author user
+ */
+public class AdjustedCircleAnimation 
+    extends CircleAnimation
+{
+    private int xAdjustment;
+    private int yAdjustment;
+
+    public AdjustedCircleAnimation(
+            int width, int height,
+            int xAdjustment, int yAdjustment,
+            BasicColor basicColor)
+    {
+        super(width, height, basicColor);
+
+        this.xAdjustment = xAdjustment;
+        this.yAdjustment = yAdjustment;
+    }
+
+    public AdjustedCircleAnimation(
+            int width, int height, int innerWidth,
+            BasicColor basicColor)
+    {
+        super(width, height, basicColor);
+
+        //this.xAdjustment = xAdjustment;
+        //this.yAdjustment = yAdjustment;
+
+        if (AndroidUtil.isAndroid())
+        {
+            int widthPortion = (((innerWidth / 3) * 10) / 7);
+            
+            this.xAdjustment = -((width >> 1) - widthPortion);
+            this.yAdjustment = this.xAdjustment;
+        }
+        else
+        {
+            this.xAdjustment = -((width >> 1) - innerWidth);
+            this.yAdjustment = this.xAdjustment;
+        }
+    }
+
+    private final int TOTAL_ANGLE = AngleFactory.getInstance().TOTAL_ANGLE;
+    
+   public void paint(Graphics graphics, int x, int y) {
+      
+       this.basicColorUtil.setBasicColor(
+               graphics, this.getBasicColor(), this.getColor());
+      
+      graphics.drawArc(x + this.xAdjustment, y + this.yAdjustment,
+          width, height, 0, TOTAL_ANGLE);
+   }
+}
