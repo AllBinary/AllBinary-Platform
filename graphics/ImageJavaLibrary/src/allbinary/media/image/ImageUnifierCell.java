0c300bfb0a3366df979e702336d12481e4babb87
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:35:02 -0500

    added

---
+++ graphics/ImageJavaLibrary/src/allbinary/media/image/ImageUnifierCell.java
@@ -0,0 +1,47 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.image;
+
+public class ImageUnifierCell
+{
+    private Integer width;
+    private Integer height;
+    
+    public ImageUnifierCell(Integer width, Integer height)
+    {
+        this.setWidth(width);
+        this.setHeight(height);
+    }
+
+    public Integer getWidth()
+    {
+        return width;
+    }
+
+    public void setWidth(Integer width)
+    {
+        this.width = width;
+    }
+
+    public Integer getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(Integer height)
+    {
+        this.height = height;
+    }
+    
+}
