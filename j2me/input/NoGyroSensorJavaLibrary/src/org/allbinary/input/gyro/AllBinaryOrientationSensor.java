697581ebbbd17967c50217d2881039efe0de6a62
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-13 23:22:58 -0500

    added

---
+++ j2me/input/NoGyroSensorJavaLibrary/src/org/allbinary/input/gyro/AllBinaryOrientationSensor.java
@@ -0,0 +1,43 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.input.gyro;
+
+import org.allbinary.input.AllBinarySensor;
+
+import abcs.logic.basic.NotImplemented;
+import allbinary.game.input.CompleteMotionGestureInputEventListenerInterface;
+import allbinary.game.input.motion.action.NoCompleteMotionGestureInputEventListener;
+import allbinary.logic.math.Orientation;
+
+public class AllBinaryOrientationSensor extends AllBinarySensor
+{
+    protected CompleteMotionGestureInputEventListenerInterface listener =
+        NoCompleteMotionGestureInputEventListener.getInstance();
+
+    public void setListener(
+            CompleteMotionGestureInputEventListenerInterface completeMotionGestureInputEventListenerInterface)
+    {
+        this.listener = completeMotionGestureInputEventListenerInterface;
+    }
+    
+    protected AllBinaryOrientationSensor()
+    {
+        
+    }
+    
+    public Orientation getOrientation() throws Exception
+    {
+        throw new Exception(NotImplemented.NAME);
+    }
+}
