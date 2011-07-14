449626cb23b0b808b02698ac7a11692613b65098
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 00:24:33 -0500

    added

---
+++ j2me/media/graphics/animation/AnimationLibrary/src/allbinary/graphics/paint/AnimationCompositePaintable.java
@@ -0,0 +1,44 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.graphics.paint;
+
+import javax.microedition.lcdui.Graphics;
+
+import allbinary.animation.Animation;
+
+/**
+ *
+ * @author user
+ */
+public class AnimationCompositePaintable extends InitUpdatePaintable
+{
+    private final Animation animationInterface;
+    
+    public AnimationCompositePaintable(
+        Animation animationInterface)
+    {
+        this.animationInterface = animationInterface;
+    }
+
+    public void paint(Graphics graphics)
+    {
+        this.animationInterface.paint(graphics, 0, 0);
+    }
+
+    public void paintThreed(Graphics graphics)
+    {
+        this.animationInterface.paintThreed(graphics, 0, 0, 0);
+    }
+
+}
