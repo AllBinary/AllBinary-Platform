92eac4a57eeba53d3818d260873b73a425fcdb93
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 07:13:47 -0500

    added

---
+++ j2me/android/VirtualKeyboardDynamicJavaLibrary/src/org/allbinary/android/input/VirtualKeyboardFactory.java
@@ -0,0 +1,45 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.android.input;
+
+import org.allbinary.android.AndroidInfoFactory;
+import org.allbinary.android.input.api1.VirtualKeyboardAPI1;
+import org.allbinary.android.input.api3.VirtualKeyboardAPI3;
+
+import android.app.Activity;
+
+public class VirtualKeyboardFactory
+{
+    private static VirtualKeyboard virtualKeyboard;
+    
+    public static final void init(Activity activity)
+    {
+        int SDK_VERSION = AndroidInfoFactory.getInstance().getVersion();
+        
+        //VERSION_CODES.BASE_1_1
+        if(SDK_VERSION <= 2)
+        {
+            virtualKeyboard = new VirtualKeyboardAPI1(activity);
+        }
+        else
+        {
+            virtualKeyboard = new VirtualKeyboardAPI3(activity);
+        }
+    }
+
+    public static final VirtualKeyboard getInstance()
+    {
+        return virtualKeyboard;
+    }
+}
