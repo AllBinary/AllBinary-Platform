a84aa72b1fda098541e3be33e0ea6e2471728847
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-14 05:33:45 -0500

    added

---
+++ graphics/ImageJavaLibrary/src/allbinary/media/image/cache/BufferedImageCacheable.java
@@ -0,0 +1,59 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.image.cache;
+
+import java.awt.image.BufferedImage;
+
+import javax.imageio.IIOImage;
+
+import com.abcs.logic.util.cache.CacheableInterface;
+
+public class BufferedImageCacheable
+    implements CacheableInterface
+{
+    private BufferedImageInfo key;
+    
+    private BufferedImage bufferedImage;
+    
+    public BufferedImageCacheable(BufferedImageInfo key)
+    {
+        this.key = key;
+        this.setBufferedImage(new BufferedImage(
+            key.getWidth(), key.getHeight(), key.getType()));
+    }
+    
+    public Object getKey()
+    {
+        return this.key;
+    }
+
+    public BufferedImage getBufferedImage()
+    {
+        return bufferedImage;
+    }
+
+    private void setBufferedImage(BufferedImage bufferedImage)
+    {
+        this.bufferedImage = bufferedImage;
+    }
+    
+    public String toString()
+    {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(this.getClass().getName());
+        stringBuffer.append(" ");
+        stringBuffer.append(this.getKey().toString());
+        return stringBuffer.toString();
+    }
+}
