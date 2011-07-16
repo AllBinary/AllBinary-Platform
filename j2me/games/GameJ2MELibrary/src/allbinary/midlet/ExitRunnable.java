320d138a28edbf050d86ed73d4c1134f1492a030
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:57:57 -0500

    added

---
+++ j2me/games/GameJ2MELibrary/src/allbinary/midlet/ExitRunnable.java
@@ -0,0 +1,88 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.midlet;
+
+import javax.microedition.lcdui.Displayable;
+
+import abcs.logic.basic.string.CommonStrings;
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.canvas.Processor;
+import allbinary.graphics.canvas.transition.progress.ProgressCanvas;
+import allbinary.graphics.canvas.transition.progress.ProgressCanvasFactory;
+import allbinary.graphics.displayable.command.MyCommandsFactory;
+
+public class ExitRunnable implements Runnable
+{
+    private final AllBinaryMidlet midlet;
+    private final Processor processor;
+    private final boolean isProgress;
+    
+    public ExitRunnable(AllBinaryMidlet midlet, Processor processor, boolean isProgress)
+    {
+        this.midlet = midlet;
+        this.isProgress = isProgress;
+        this.processor = processor;
+    }
+    
+    public void run()
+    {
+        try
+        {
+            LogUtil.put(LogFactory.getInstance(CommonStrings.getInstance().START_RUNNABLE, this, CommonStrings.getInstance().RUN));
+
+            ProgressCanvas progressCanvas = ProgressCanvasFactory.getInstance();
+
+            if (this.isProgress)
+            {
+                progressCanvas.start();
+
+                this.midlet.commandAction(MyCommandsFactory.getInstance().SET_DISPLAYABLE, progressCanvas);
+
+                // progressCanvas.waitUntilDisplayed();
+            }
+
+            this.midlet.destroyApp(false);
+
+            if (this.isProgress)
+            {
+                progressCanvas.end();
+            }
+
+            // Why set to null
+            this.midlet.setDisplay((Displayable) null);
+
+            // TWB - Only remove from context when multiple midlets share the
+            // same emulator
+            // Note this would also require that the midlet be reinitialized
+            // when resuming from onSaveInstanceState
+            this.midlet.notifyDestroyed();
+
+            LogUtil.put(LogFactory.getInstance(CommonStrings.getInstance().END_RUNNABLE, this, CommonStrings.getInstance().RUN));
+
+            processor.process();
+        } catch (Exception e)
+        {
+            LogUtil.put(LogFactory.getInstance(CommonStrings.getInstance().EXCEPTION, this, CommonStrings.getInstance().RUN, e));
+
+            try
+            {
+                processor.process();
+            } catch (Exception e2)
+            {
+                LogUtil.put(LogFactory.getInstance(CommonStrings.getInstance().EXCEPTION, this, CommonStrings.getInstance().RUN, e));
+            }
+        }
+    }
+}
