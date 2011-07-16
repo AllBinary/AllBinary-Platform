ea3d7a33adc2888bf8dfd57014dc0597e1fc9e86
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:47:29 -0500

    added

---
+++ j2me/games/GameJ2MELibrary/src/allbinary/game/displayable/canvas/GameFrameRunnable.java
@@ -0,0 +1,58 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.displayable.canvas;
+
+import abcs.logic.basic.string.CommonStrings;
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.time.GameTickTimeDelayHelperFactory;
+
+public class GameFrameRunnable implements Runnable
+{
+    private final AllBinaryGameCanvas allBinaryGameCanvas;
+    
+    public GameFrameRunnable(AllBinaryGameCanvas allBinaryGameCanvas)
+    {
+        this.allBinaryGameCanvas = allBinaryGameCanvas;
+    }
+
+    private final GameTickTimeDelayHelperFactory gameTickTimeDelayHelperFactory = 
+        GameTickTimeDelayHelperFactory.getInstance();
+    
+    public void run()
+    {
+        try
+        {
+            final AllBinaryGameCanvas allBinaryGameCanvas = this.allBinaryGameCanvas;
+            
+            allBinaryGameCanvas.getLoopTimeHelper().setStartTime(
+                    gameTickTimeDelayHelperFactory.setStartTime());
+
+            allBinaryGameCanvas.processGame();
+
+            if (!allBinaryGameCanvas.isRunning()
+                    || allBinaryGameCanvas.isPaused())
+            {
+                allBinaryGameCanvas.notifyDonePainting();
+            }
+        }
+        catch (Exception e)
+        {
+            LogUtil.put(LogFactory.getInstance(
+                    CommonStrings.getInstance().EXCEPTION,
+                    this, CommonStrings.getInstance().RUN, e));
+        }
+    }
+}
+
