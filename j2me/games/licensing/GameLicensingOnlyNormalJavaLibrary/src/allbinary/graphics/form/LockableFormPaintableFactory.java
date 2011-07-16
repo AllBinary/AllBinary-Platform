6b7c7420169d0a2a665bc7199ee252ae9855ac75
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 11:57:00 -0500

    added

---
+++ j2me/games/licensing/GameLicensingOnlyNormalJavaLibrary/src/allbinary/graphics/form/LockableFormPaintableFactory.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.graphics.form;
+
+public class LockableFormPaintableFactory extends ItemPaintableFactory
+{   
+    private final int lockedIndex;
+    
+    public LockableFormPaintableFactory(int lockedIndex)
+    {
+        this.lockedIndex = lockedIndex;   
+    }
+    
+    public ItemPaintable getInstance(PaintableForm paintableForm) 
+    throws Exception
+    {
+        return new LockablePaintable(this.lockedIndex);
+    }    
+}
