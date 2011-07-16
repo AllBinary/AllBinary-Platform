e6bd29c1d42966053098ac88719a41b070bc18bd
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 05:39:07 -0500

    added

---
+++ automation/InputAutomationJavaApplication/src/bundle/input/automation/module/InputAutomationModuleServiceInterface.java
@@ -0,0 +1,24 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package bundle.input.automation.module;
+
+import allbinary.osgi.OSGIServiceInterface;
+
+import allbinary.input.automation.module.InputAutomationModuleFactoryInterface;
+
+public interface InputAutomationModuleServiceInterface extends OSGIServiceInterface
+{
+    InputAutomationModuleFactoryInterface[] getInputAutomationModuleInterfaceArray();
+    
+}
