805a290e96185d8d4908b9303e502ad362aa0b62
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 05:38:43 -0500

    added

---
+++ automation/InputAutomationJavaApplication/src/bundle/input/automation/InputAutomationBundleActivatorListenerInterface.java
@@ -0,0 +1,26 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package bundle.input.automation;
+
+import org.osgi.framework.BundleContext;
+
+public interface InputAutomationBundleActivatorListenerInterface
+{
+    //Registers InputAutomationBundle module change service
+    //Allows module to start and stop while the gui is running
+    void registerAsService() throws Exception;
+
+    //Add all modules that are already running as a service to the gui
+    void useServices() throws Exception;
+}
