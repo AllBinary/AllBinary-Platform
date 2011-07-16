0e018e63d314c47c69f0db14e4447b9c39f9ee89
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:00:08 -0500

    added

---
+++ j2me/media/graphics/BoundsJavaLibrary/src/org/allbinary/bounds/event/BoundsChangeEvent.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.bounds.event;
+
+import allbinary.graphics.Rectangle;
+import allbinary.logic.basic.util.event.AllBinaryEventObject;
+
+public class BoundsChangeEvent extends AllBinaryEventObject
+{
+    public BoundsChangeEvent(Rectangle rectangle)
+    {
+        super(rectangle);
+    }
+    
+    public Rectangle getRectangle()
+    {
+        return (Rectangle) this.getSource();
+    }
+}
