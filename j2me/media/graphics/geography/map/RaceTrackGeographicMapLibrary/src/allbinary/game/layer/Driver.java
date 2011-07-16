2a5aac8b8d29ed2c0c5fb1a3bbb458926d91b262
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:20:26 -0500

    added

---
+++ j2me/media/graphics/geography/map/RaceTrackGeographicMapLibrary/src/allbinary/game/layer/Driver.java
@@ -0,0 +1,68 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.layer;
+
+/**
+ *
+ * @author user
+ */
+public class Driver {
+
+   //driver intelligence
+   private Integer turnSpeed;
+   private Integer trackPathSelectionSkill;
+
+   private Aggression aggression;
+   
+   public Driver(
+      Integer turnSpeed, 
+      Integer trackPathSelectionSkill, 
+      Aggression aggression)
+   {
+      this.setTurnSpeed(turnSpeed);
+      this.setTrackPathSelectionSkill(trackPathSelectionSkill);
+      this.setAggression(aggression);
+   }
+
+   public Integer getTurnSpeed()
+   {
+      return turnSpeed;
+   }
+
+   public void setTurnSpeed(Integer turnSpeed)
+   {
+      this.turnSpeed = turnSpeed;
+   }
+
+   public Integer getTrackPathSelectionSkill()
+   {
+      return trackPathSelectionSkill;
+   }
+
+   public void setTrackPathSelectionSkill(Integer trackPathSelectionSkill)
+   {
+      this.trackPathSelectionSkill = trackPathSelectionSkill;
+   }
+
+   public Aggression getAggression()
+   {
+      return aggression;
+   }
+
+   public void setAggression(Aggression aggression)
+   {
+      this.aggression = aggression;
+   }
+   
+}
