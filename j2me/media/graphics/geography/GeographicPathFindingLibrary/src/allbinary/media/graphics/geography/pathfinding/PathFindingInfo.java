d618aab4dd5cca6a7b5f2c30c57e8fd0d4674d17
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:31:10 -0500

    added

---
+++ j2me/media/graphics/geography/GeographicPathFindingLibrary/src/allbinary/media/graphics/geography/pathfinding/PathFindingInfo.java
@@ -0,0 +1,76 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.graphics.geography.pathfinding;
+
+import org.allbinary.util.BasicArrayList;
+
+public class PathFindingInfo implements PathFindingInfoInterface
+{
+    private GeographicPathFinderInterface pathFinder;
+    private BasicArrayList startPathFindingNode;
+    private BasicArrayList endPathFindingNode;
+    
+    private final BasePathFindingNodeCostInfoFactoryInterface pathFindingNodeCostInfoFactoryInterface;
+    
+    public PathFindingInfo(
+            BasePathFindingNodeCostInfoFactoryInterface pathFindingNodeCostInfoFactoryInterface)
+    {
+       this.pathFindingNodeCostInfoFactoryInterface =
+           pathFindingNodeCostInfoFactoryInterface;
+
+       this.startPathFindingNode = new BasicArrayList(1);
+       this.endPathFindingNode = new BasicArrayList(1);
+    }
+
+    public void init()
+    {
+        this.startPathFindingNode.clear();
+        this.endPathFindingNode.clear();
+    }
+
+    public BasicArrayList getStartPathFindingNodeList()
+    {
+        return this.startPathFindingNode;
+    }
+    
+    public BasicArrayList getEndPathFindingNodeList()
+    {
+        return this.endPathFindingNode;
+    }
+
+    public GeographicPathFinderInterface getPathFinder()
+    {        
+        return this.pathFinder;
+    }
+
+    public void addStartPathFindingNode(PathFindingNode startPathFindingNode)
+    {
+        this.startPathFindingNode.add(startPathFindingNode);
+    }
+
+    public void addEndPathFindingNode(PathFindingNode endPathFindingNode)
+    {
+        this.endPathFindingNode.add(endPathFindingNode);
+    }
+
+    public void setPathFinder(GeographicPathFinderInterface pathFinder)
+    {
+        this.pathFinder = pathFinder;
+    }
+
+   public BasePathFindingNodeCostInfoFactoryInterface getPathFindingNodeCostInfoFactoryInterface()
+   {
+      return pathFindingNodeCostInfoFactoryInterface;
+   }
+}
