00d593160cfb3a0a3058092c7542493f4e6d993e
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:52:41 -0500

    added

---
+++ j2me/games/GameJ2MELibrary/src/allbinary/game/layer/PaintableLayerComposite.java
@@ -0,0 +1,41 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.layer;
+
+import javax.microedition.lcdui.Graphics;
+
+import allbinary.graphics.paint.Paintable;
+import allbinary.layer.Layer;
+
+/**
+ *
+ * @author user
+ */
+public class PaintableLayerComposite extends Paintable
+{
+   private Layer[] paintableArray;
+   
+   public PaintableLayerComposite(Layer[] paintableArray)
+   {
+      this.paintableArray = paintableArray;
+   }
+   
+   public void paint(Graphics graphics)
+   {
+      for(int index = paintableArray.length; --index >= 0;)
+      {
+         this.paintableArray[index].paint(graphics);
+      }
+   }
+}
