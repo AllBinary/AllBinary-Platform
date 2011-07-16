c0c6a8339f8d7b319db37e7b26339a238236fa35
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:23:30 -0500

    added

---
+++ j2me/media/graphics/geography/map/RaceTrackGeographicMapLibrary/src/allbinary/media/graphics/geography/map/racetrack/FinalPositionRaceTrackFactory.java
@@ -0,0 +1,35 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.graphics.geography.map.racetrack;
+
+public class FinalPositionRaceTrackFactory
+{
+   public static final int FIRST_POSITION = 1;
+
+   private static int index = FIRST_POSITION;
+      
+   private FinalPositionRaceTrackFactory()
+   {
+   }
+   
+   public synchronized static int getInstance()
+   {
+      return index++;
+   }
+
+   public synchronized static void reset()
+   {
+      index = FIRST_POSITION;
+   }
+}
