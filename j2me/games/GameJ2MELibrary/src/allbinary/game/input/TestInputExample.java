55d2a3e7ee129f8475b4c79f31c2163d53b6b836
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:51:23 -0500

    added

---
+++ j2me/games/GameJ2MELibrary/src/allbinary/game/input/TestInputExample.java
@@ -0,0 +1,61 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.input;
+
+import javax.microedition.lcdui.Graphics;
+
+public class TestInputExample
+implements TestInputInterface
+{
+    private int testValue;
+    
+    public void up()
+    {
+        
+    }
+    
+    public void down()
+    {
+        
+    }
+    
+    public void left()
+    {
+        this.testValue++;
+    }
+    
+    public void right()
+    {
+        this.testValue--;
+    }
+    
+    public void strafeLeft()
+    {
+        
+    }
+    
+    public void strafeRight()
+    {
+        
+    }
+    
+    public void paint(Graphics graphics)
+    {
+        
+    }
+    
+    public void paintThreed(Graphics graphics)
+    {
+    }    
+}
