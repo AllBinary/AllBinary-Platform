1c546a82aa158c3127b46d3a5cdb5efb6a5cd760
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:51:30 -0500

    added

---
+++ j2me/games/GameJ2MELibrary/src/allbinary/game/layer/AllBinaryGameLayer.java
@@ -0,0 +1,154 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.layer;
+
+import javax.microedition.lcdui.Graphics;
+
+import org.allbinary.util.BasicArrayList;
+
+import abcs.logic.basic.NotImplemented;
+import abcs.logic.basic.string.CommonSeps;
+import abcs.logic.communication.log.ForcedLogUtil;
+import allbinary.graphics.Rectangle;
+import allbinary.graphics.SpacialStrings;
+import allbinary.graphics.color.BasicColor;
+import allbinary.graphics.color.BasicColorFactory;
+import allbinary.graphics.color.BasicColorSetUtil;
+import allbinary.graphics.color.BasicColorUtilFactory;
+import allbinary.layer.AllBinaryLayer;
+import allbinary.layer.AllBinaryLayerManager;
+import allbinary.logic.basic.util.event.AllBinaryEventObject;
+import allbinary.logic.basic.util.event.handler.BasicEventHandler;
+import allbinary.math.PositionStrings;
+import allbinary.view.ViewPosition;
+import allbinary.view.event.ViewPositionEvent;
+import allbinary.view.event.ViewPositionEventListenerInterface;
+
+public class AllBinaryGameLayer extends AllBinaryLayer 
+   implements ViewPositionEventListenerInterface
+{
+    private final BasicArrayList gameKeyEventList = new BasicArrayList();
+
+    /*
+     * public AllBinaryGameLayer(int x, int y, int width, int height) { super(x,
+     * y, width, height); }
+     */
+    public AllBinaryGameLayer(Rectangle layerInfo)
+    {
+        this(layerInfo, new ViewPosition());
+    }
+
+    public AllBinaryGameLayer(Rectangle layerInfo, ViewPosition viewPosition)
+    {
+        super(layerInfo, viewPosition);
+    }
+
+    public BasicArrayList getGameKeyEventList()
+    {
+        return gameKeyEventList;
+    }
+
+    public void processInput(AllBinaryLayerManager allBinaryLayerManager) throws Exception
+    {
+        throw new Exception(NotImplemented.NAME);
+    }
+
+    public void processTick(AllBinaryLayerManager allBinaryLayerManager) throws Exception
+    {
+        throw new Exception(NotImplemented.NAME);
+    }
+
+    public void onEvent(AllBinaryEventObject eventObject)
+    {
+        ForcedLogUtil.log(BasicEventHandler.PERFORMANCE_MESSAGE, this);
+
+        //this.onGeographicMapCellPositionEvent((GeographicMapCellPositionEvent) eventObject);
+    }
+
+    public void onViewPositionChangeEvent() throws Exception
+    {
+        this.onChangeEvent(this.viewPositionEvent);
+    }
+
+    public void onChangeEvent(ViewPositionEvent layerManagerEvent)
+       throws Exception
+    {
+        //LogUtil.put(LogFactory.getInstance(CommonStrings.getInstance().START, this, "onChangeEvent"));
+        if (GameLayerUtil.isOnScreen(this))
+        {
+            this.setVisible(true);
+        }
+        else
+        {
+            this.setVisible(false);
+        }
+    }
+    
+    protected final BasicColorSetUtil basicColorUtil = 
+        BasicColorUtilFactory.getInstance();
+    
+    private final BasicColor RED = BasicColorFactory.getInstance().RED;
+    
+    //Should be overridden
+    public void paint(Graphics graphics)
+    {
+        //LogUtil.put(LogFactory.getInstance(NotImplemented.NAME, this, "paint"));
+
+        ViewPosition viewPosition = this.getViewPosition();
+        int viewX = viewPosition.getX();
+        int viewY = viewPosition.getY();
+
+        //LogUtil.put(LogFactory.getInstance("viewX: " + viewX + " viewY: " + viewY, this, "paint"));
+
+        this.basicColorUtil.setBasicColor(graphics, RED);
+
+        graphics.drawRect(viewX, viewY, this.getWidth(), this.getHeight());
+        //this.getViewPosition().getX2() - viewX,
+        //this.getViewPosition().getY2() - viewY);
+
+        //graphics.drawRect(viewX, viewY, width, height);
+
+        //super.paint(graphics);
+    }
+    
+    private static final String TYPE = "Type: ";
+    
+    public String toString()
+    {
+        StringBuilder stringBuffer = new StringBuilder();
+        
+        CommonSeps commonSeps = CommonSeps.getInstance();
+        
+        stringBuffer.append(commonSeps.NEW_LINE);
+        stringBuffer.append(this.getName());
+        stringBuffer.append(commonSeps.COLON_SEP);
+        stringBuffer.append(TYPE);
+        stringBuffer.append(this.getType());
+        stringBuffer.append(commonSeps.SPACE);
+        stringBuffer.append(PositionStrings.getInstance().X_LABEL);
+        stringBuffer.append(this.getX());
+        stringBuffer.append(commonSeps.SPACE);
+        stringBuffer.append(PositionStrings.getInstance().Y_LABEL);
+        stringBuffer.append(this.getY());
+        
+        stringBuffer.append(commonSeps.SPACE);
+        stringBuffer.append(SpacialStrings.getInstance().WIDTH_LABEL);
+        stringBuffer.append(this.getWidth());
+        stringBuffer.append(commonSeps.SPACE);
+        stringBuffer.append(SpacialStrings.getInstance().HEIGHT_LABEL);
+        stringBuffer.append(this.getHeight());
+        
+        return stringBuffer.toString();
+    }
+}
