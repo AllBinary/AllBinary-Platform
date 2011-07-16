47690da15353577e43586e30c0c9c99816e222bb
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:21:13 -0500

    added

---
+++ j2me/media/graphics/geography/map/RaceTrackGeographicMapLibrary/src/allbinary/game/layer/hud/LapInfo.java
@@ -0,0 +1,64 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.layer.hud;
+
+public class LapInfo
+{
+    private int numberOfLaps;
+    private int totalLaps;
+
+    public LapInfo(int totalLaps)
+    {
+        this.totalLaps = totalLaps;
+        this.numberOfLaps = 1;
+    }
+
+    public void add()
+    {
+        this.numberOfLaps++;
+    }
+
+    public int getCurrentLap()
+    {
+        return this.numberOfLaps;
+    }
+
+    public int getTotalLaps()
+    {
+        return this.totalLaps;
+    }
+
+    public boolean isDone()
+    {
+        if (this.numberOfLaps >= this.totalLaps)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString()
+    {
+        StringBuilder stringBuffer = new StringBuilder();
+
+        stringBuffer.append("isDone: ");
+        stringBuffer.append(this.isDone());
+        stringBuffer.append(" CurrentLap: ");
+        stringBuffer.append(this.getCurrentLap());
+        stringBuffer.append(" Total Laps: ");
+        stringBuffer.append(this.getTotalLaps());
+
+        return stringBuffer.toString();
+    }
+}
