af9bf6e916daed08f10aa2c34d1e479fd34bd2ea
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:44:16 -0500

    added

---
+++ j2me/games/GameJ2MELibrary/src/allbinary/game/ai/ArtificialIntelligenceInterfaceFactoryInterface.java
@@ -0,0 +1,27 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.ai;
+
+import java.util.Hashtable;
+
+import allbinary.ai.ArtificialIntelligenceInterface;
+import allbinary.game.input.GameInput;
+import allbinary.layer.AllBinaryLayer;
+
+public interface ArtificialIntelligenceInterfaceFactoryInterface {
+    
+    ArtificialIntelligenceInterface getInstance(
+          Hashtable hashtable, AllBinaryLayer ownerLayerInterface, GameInput gameInput)
+          throws Exception;
+}
