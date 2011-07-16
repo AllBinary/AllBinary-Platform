050fe9981523c7801a74219f46527488bfd9d6ef
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:53:47 -0500

    added

---
+++ j2me/games/GameJ2MELibrary/src/allbinary/game/midlet/DemoGameMidletState.java
@@ -0,0 +1,26 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.midlet;
+
+public class DemoGameMidletState
+{    
+    //private static int index = 0;
+    
+    //private int id;
+    
+    public DemoGameMidletState()
+    {
+    //    id = index++;
+    }
+}
