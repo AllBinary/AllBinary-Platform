6372c81637de0786018ddbd189d35a35913d6aee
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 11:55:07 -0500

    added

---
+++ j2me/games/licensing/GameLicensingOnlyDemoJavaLibrary/src/abcs/logic/system/security/licensing/LockedFeatureNotificationUtil.java
@@ -0,0 +1,62 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package abcs.logic.system.security.licensing;
+
+
+import abcs.logic.java.bool.BooleanFactory;
+import allbinary.game.layer.hud.basic.event.GameNotificationEvent;
+import allbinary.game.layer.hud.basic.event.GameNotificationEventHandler;
+import allbinary.graphics.color.BasicColor;
+import allbinary.logic.math.SmallIntegerSingletonFactory;
+import allbinary.media.audio.ErrorSound;
+import allbinary.media.audio.PrimaryPlayerQueueFactory;
+
+public class LockedFeatureNotificationUtil
+{
+    private static final LockedFeatureNotificationUtil instance = 
+        new LockedFeatureNotificationUtil();
+
+    public static LockedFeatureNotificationUtil getInstance()
+    {
+        return instance;
+    }
+
+    private final GameNotificationEvent gameNotificationEvent = 
+        new GameNotificationEvent(
+                this, 
+                LicenseStrings.getInstance().LOCKED,
+                SmallIntegerSingletonFactory.getInstance(3),
+                BasicColor.RED,
+                BooleanFactory.getInstance().FALSE);
+    
+    public void fire() throws Exception
+    {
+        PrimaryPlayerQueueFactory.getInstance().add(
+                ErrorSound.getInstance());
+
+        GameNotificationEventHandler.getInstance().fireEvent(
+                gameNotificationEvent);
+    }
+
+    public void fire(BasicColor basicColor) throws Exception
+    {
+        PrimaryPlayerQueueFactory.getInstance().add(
+                ErrorSound.getInstance());
+
+        gameNotificationEvent.setBasicColor(basicColor);
+        
+        GameNotificationEventHandler.getInstance().fireEvent(
+                gameNotificationEvent);
+    }
+}
