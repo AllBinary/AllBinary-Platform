27984a294ab9792f0b0aefd97bfe09c9e290b89e
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:10:47 -0500

    added

---
+++ j2me/media/graphics/geography/map/GeographicMapLibrary/src/allbinary/media/graphics/geography/map/GeographicMapInterface.java
@@ -0,0 +1,57 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.graphics.geography.map;
+
+import allbinary.game.layer.AllBinaryTiledLayer;
+import allbinary.graphics.color.BasicColor;
+import allbinary.layer.Layer;
+
+public interface GeographicMapInterface
+{
+   public Integer getId();
+   public String getName();
+   
+   void reset();
+   
+   AllBinaryTiledLayer getAllBinaryTiledLayer();
+
+   boolean getCellPositionsAt(
+      Layer layer, 
+      GeographicMapCellPosition[][] currentCellPositionArray,
+      GeographicMapCellPosition[][] cellPositionArray)
+      throws Exception;
+         
+   GeographicMapCellPosition getCellPositionAt(int x, int y)
+      throws Exception;
+
+   GeographicMapCellPosition getCellPositionAtNoThrow(int x, int y)
+       throws Exception;
+
+   //GPoint getPointAt(GeographicMapCellPosition geographicMapCellPosition)
+     // throws Exception;
+
+   int getCellTypeFromMapCellTypeInt(int cellTypeId);
+
+   GeographicMapCellType getCellTypeAt(int x, int y) 
+      throws Exception;
+   GeographicMapCellType getCellTypeAt(GeographicMapCellPosition geographicMapCellPosition) 
+      throws Exception;
+   
+   BasicGeographicMapCellPositionFactory getGeographicMapCellPositionFactory();
+   GeographicMapCellPositionFactoryInterface getGeographicMapCellPositionFactoryInterface();
+   
+   public BasicColor getForegroundBasicColor();
+   public BasicColor getBackgroundBasicColor();
+
+}
