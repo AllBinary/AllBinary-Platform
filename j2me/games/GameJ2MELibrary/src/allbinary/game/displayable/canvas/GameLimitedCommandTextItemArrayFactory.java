e3173644a0f63d420d8d7b765356f81abe5b8231
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:48:07 -0500

    added

---
+++ j2me/games/GameJ2MELibrary/src/allbinary/game/displayable/canvas/GameLimitedCommandTextItemArrayFactory.java
@@ -0,0 +1,36 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.displayable.canvas;
+
+import allbinary.graphics.form.item.CommandTextItemArrayFactory;
+import allbinary.graphics.form.item.LimitCommandsVisitor;
+
+public class GameLimitedCommandTextItemArrayFactory
+{
+    private static final GameLimitedCommandTextItemArrayFactory instance = 
+        new GameLimitedCommandTextItemArrayFactory();
+    
+    private final CommandTextItemArrayFactory commandTextItemArrayFactory =
+        new CommandTextItemArrayFactory(new LimitCommandsVisitor());
+
+    public static GameLimitedCommandTextItemArrayFactory getInstance()
+    {
+        return instance;
+    }
+
+    public CommandTextItemArrayFactory getCommandTextItemArrayFactory()
+    {
+        return commandTextItemArrayFactory;
+    }
+}
