1eb1ce138476f3dfe064df4d23db09e73690f34d
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:52:50 -0500

    added

---
+++ j2me/games/GameJ2MELibrary/src/allbinary/game/layer/PlayerGameInputGameLayer.java
@@ -0,0 +1,65 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.layer;
+
+import javax.microedition.lcdui.Graphics;
+
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.game.input.PlayerGameInput;
+import allbinary.game.input.PlayerGameInputCompositeInterface;
+import allbinary.graphics.PointFactory;
+import allbinary.graphics.Rectangle;
+import allbinary.layer.AllBinaryLayerManager;
+import allbinary.view.StaticViewPosition;
+
+/**
+ *
+ * @author Berthelot, Travis
+ * @version 1.0
+ */
+public class PlayerGameInputGameLayer extends AllBinaryGameLayer
+    implements PlayerGameInputCompositeInterface
+{
+    private PlayerGameInput playerGameInput;
+   
+    public PlayerGameInputGameLayer()
+        throws Exception
+    {
+        super(
+            new Rectangle(PointFactory.getInstance().ZERO_ZERO, 0, 0),
+            new StaticViewPosition(0, 0));
+
+        this.playerGameInput = new PlayerGameInput(this.getGameKeyEventList());
+    }
+    
+    public PlayerGameInput getPlayerGameInput()
+    {
+        return this.playerGameInput;
+    }
+
+    public void initInputProcessors()
+    {
+    }
+    
+    public void processInput(AllBinaryLayerManager myManager) throws Exception
+    {
+        
+    }
+    
+   public void paint(Graphics graphics)
+   {
+       LogUtil.put(LogFactory.getInstance("Should Not Be Called Except For Testing Input", this, "paint"));
+   }
+}
