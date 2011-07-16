f9afa60ee38800a9fabba1f910abe8a4edd0b37b
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 05:24:46 -0500

    added

---
+++ automation/InputAutomationGenericModuleJavaLibrary/src/allbinary/input/automation/module/generic/GenericInputAutomationAction.java
@@ -0,0 +1,37 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.input.automation.module.generic;
+
+import allbinary.input.automation.robot.InputRobotInterface;
+import allbinary.input.automation.module.InputAutomationActionInterface;
+
+public class GenericInputAutomationAction
+    implements InputAutomationActionInterface
+{
+    
+    public GenericInputAutomationAction()
+    {
+    }
+    
+    public void test() 
+        throws Exception
+    {
+        
+    }
+    
+    public InputRobotInterface[] getInputRobotInterfaceArray()
+    {
+        return null;
+    }
+}
