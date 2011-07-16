3a427a6b511f6489fffe7bc51a5de9c9a4007d09
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:48:33 -0500

    added

---
+++ j2me/games/GameJ2MELibrary/src/allbinary/game/displayable/canvas/GameRunnable.java
@@ -0,0 +1,34 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.displayable.canvas;
+
+public class GameRunnable implements Runnable
+{
+    private static final GameRunnable instance = new GameRunnable();
+
+    public static GameRunnable getInstance()
+    {
+        return instance;
+    }
+
+    public void run()
+    {
+    }
+    
+    public void processLoopSleep()
+    throws Exception
+    {
+        Thread.sleep(120);
+    }
+}
