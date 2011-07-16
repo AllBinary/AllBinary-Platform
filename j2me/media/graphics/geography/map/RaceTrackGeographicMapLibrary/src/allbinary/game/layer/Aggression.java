0bb0d84ec743b09c37f3104178c8c9368e68a62f
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:20:07 -0500

    added

---
+++ j2me/media/graphics/geography/map/RaceTrackGeographicMapLibrary/src/allbinary/game/layer/Aggression.java
@@ -0,0 +1,63 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.layer;
+
+/**
+ *
+ * @author user
+ */
+public class Aggression {
+
+   private Integer ram;
+   private Integer projectile;
+   private Integer drops;
+   
+   public Aggression(Integer ram, Integer projectile, Integer drops)
+   {
+      this.setRam(ram);
+      this.setProjectile(projectile);
+      this.setDrops(drops);
+   }
+   
+   public Integer getRam()
+   {
+      return ram;
+   }
+
+   public void setRam(Integer ram)
+   {
+      this.ram = ram;
+   }
+
+   public Integer getProjectile()
+   {
+      return projectile;
+   }
+
+   public void setProjectile(Integer projectile)
+   {
+      this.projectile = projectile;
+   }
+
+   public Integer getDrops()
+   {
+      return drops;
+   }
+
+   public void setDrops(Integer drops)
+   {
+      this.drops = drops;
+   }
+
+}
