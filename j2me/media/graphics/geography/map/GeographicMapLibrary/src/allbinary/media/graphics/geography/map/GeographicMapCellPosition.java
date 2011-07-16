b4f25b7044e029c11876e5e19987887c6fafe0e3
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:09:14 -0500

    added

---
+++ j2me/media/graphics/geography/map/GeographicMapLibrary/src/allbinary/media/graphics/geography/map/GeographicMapCellPosition.java
@@ -0,0 +1,63 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.graphics.geography.map;
+
+import allbinary.graphics.CellPosition;
+import allbinary.graphics.GPoint;
+import allbinary.graphics.PointFactory;
+
+public class GeographicMapCellPosition extends CellPosition
+{
+   private GPoint point;
+   private GPoint midPoint;
+
+   public GeographicMapCellPosition(int column, int row, int columns, int rows, int width, int height)
+           throws Exception
+   {
+      super(column, row, columns, rows);
+
+      //Integer x = new Integer(this.getColumn() * tiledLayer.getCellWidth());
+      //Integer y = new Integer(this.getRow() * tiledLayer.getCellHeight());
+      //this.point = PointFactory.getInstance(x, y);
+
+      int x = this.getColumn() * width;
+      int y = this.getRow() * height;
+      
+      PointFactory pointFactory = PointFactory.getInstance();
+      
+      this.point = pointFactory.getInstance(x, y);
+      this.midPoint = pointFactory.getInstance(x + (width >> 1), y + (height >> 1));
+   }
+
+   public GPoint getPoint()
+   {
+      return point;
+   }
+
+    /**
+     * @return the midPoint
+     */
+    public GPoint getMidPoint()
+    {
+        return midPoint;
+    }
+
+    /**
+     * @param midPoint the midPoint to set
+     */
+    public void setMidPoint(GPoint midPoint)
+    {
+        this.midPoint = midPoint;
+    }
+}
