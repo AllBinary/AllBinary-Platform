a60f256b3b4e269bfc4a0739737053c03fd20b38
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 12:22:58 -0500

    added

---
+++ j2me/media/graphics/geography/map/RaceTrackGeographicMapLibrary/src/allbinary/media/graphics/geography/map/racetrack/CloseLoopRaceTrackGeogrpahicMapCellTypeInitializer.java
@@ -0,0 +1,41 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.media.graphics.geography.map.racetrack;
+
+import allbinary.logic.math.SmallIntegerSingletonFactory;
+import allbinary.media.graphics.geography.map.GeographicMapCellTypeFactory;
+import allbinary.media.graphics.geography.pathfinding.PathFindingNodeCostInfoData;
+
+public class CloseLoopRaceTrackGeogrpahicMapCellTypeInitializer
+{
+    public static void init()
+    {
+        final RaceTrackGeographicMapCellTypeFactory raceTrackGeographicMapCellTypeFactory = 
+            RaceTrackGeographicMapCellTypeFactory.getInstance();
+        
+        final GeographicMapCellTypeFactory geographicMapCellTypeFactory = 
+            GeographicMapCellTypeFactory.getInstance();
+        
+        geographicMapCellTypeFactory.EMPTY_CELL_TYPE =
+          new RaceTrackGeographicMapCellType(
+          SmallIntegerSingletonFactory.getInstance().getInstance(0), 
+          PathFindingNodeCostInfoData.MAX_NODE_COST);
+
+        geographicMapCellTypeFactory.EASY_CELL_TYPE =
+            geographicMapCellTypeFactory.EMPTY_CELL_TYPE;
+       
+        raceTrackGeographicMapCellTypeFactory.FINISH_LINE_ROAD_CELL_TYPE = 
+            raceTrackGeographicMapCellTypeFactory.START_LINE_ROAD_CELL_TYPE;
+    }
+}
