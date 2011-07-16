5316855ff63a00e1ba89d932f50675397fe765b7
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-16 05:17:07 -0500

    added

---
+++ automation/InputAutomationGenericModuleJavaLibrary/src/allbinary/input/automation/module/generic/configuration/profile/actions/script/condition/processors/input/KeyboardActionScriptInputData.java
@@ -0,0 +1,28 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.input.automation.module.generic.configuration.profile.actions.script.condition.processors.input;
+
+public class KeyboardActionScriptInputData
+{
+    private KeyboardActionScriptInputData()
+    {
+    }
+ 
+    public static String NAME = "KEY_ACTION_INPUT_NAME";
+    
+    public static String KEY = "KEY_ACTION_INPUT_KEY";
+    public static String DELAY_BETWEEN_KEYS = "KEY_ACTION_INPUT_DELAY_BETWEEN_KEYS";
+    public static String PRESS = "KEY_ACTION_INPUT_PRESS";
+    public static String RELEASE = "KEY_ACTION_INPUT_RELEASE";
+}
