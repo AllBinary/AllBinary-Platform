d50e8b081540a0213fc97cb16c46bd47b2838da6
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-17 07:13:50 -0500

    added

---
+++ j2me/games/test/TestGameDemoBaseJavaLibrary/src/org/allbinary/game/testgamedemo/init/TestGameDemoResourceInitialization.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.game.testgamedemo.init;
+
+import allbinary.game.resource.ResourceInitialization;
+
+public class TestGameDemoResourceInitialization 
+    extends ResourceInitialization
+{
+    public TestGameDemoResourceInitialization()
+    {
+    }
+
+    public void init()
+    throws Exception
+    {
+        super.init();
+    }
+}
