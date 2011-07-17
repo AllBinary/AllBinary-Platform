90c439758eb42e0e8cfdbcac47da956e109efb1a
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-17 07:13:58 -0500

    added

---
+++ j2me/games/test/TestGameDemoBaseJavaLibrary/src/org/allbinary/game/testgamedemo/TestGameDemoMIDlet.java
@@ -0,0 +1,116 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.game.testgamedemo;
+
+import org.allbinary.game.testgamedemo.canvas.TestGameDemoGameCanvas;
+import org.allbinary.game.testgamedemo.canvas.TestGameDemoInputMappingHelpPaintable;
+import org.allbinary.game.testgamedemo.canvas.TestGameDemoSoftwareInfo;
+import org.allbinary.game.testgamedemo.canvas.TestGameDemoStartCanvas;
+import org.allbinary.game.testgamedemo.layer.TestGameDemoLayerManager;
+import org.allbinary.media.audio.TestGameDemoSoundsFactoryFactory;
+
+import abcs.logic.basic.string.CommonStrings;
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.game.GameInfo;
+import allbinary.game.GameMode;
+import allbinary.game.GameType;
+import allbinary.game.PlayerTypesFactory;
+import allbinary.game.displayable.canvas.GameCanvasRunnableInterface;
+import allbinary.game.layer.AllBinaryGameLayerManager;
+import allbinary.game.midlet.LicenseLoadingTypeFactory;
+import allbinary.game.midlet.SpecialDemoGameMidlet;
+import allbinary.game.paint.help.HelpPaintable;
+import allbinary.game.score.BasicHighScoresFactory;
+import allbinary.game.score.HighScoresPaintable;
+import allbinary.game.score.displayable.HighScoresCanvas;
+import allbinary.media.audio.AllBinaryMediaManagerShutdown;
+
+/**
+ * @author user
+ */
+public class TestGameDemoMIDlet extends
+   SpecialDemoGameMidlet
+   //DemoGameMidlet
+{
+   public TestGameDemoMIDlet()
+   {
+       super(LicenseLoadingTypeFactory.getIntance().OTHER);
+       //this.setSaveGameForm(SaveGameForm.getInstance(this, "Save Game"));
+   }
+   
+   protected HelpPaintable getHelpPaintable()
+   throws Exception
+   {
+       //return TestGameDemoHelpPaintable.getInstance();
+       return TestGameDemoInputMappingHelpPaintable.getInstance();
+   }
+   
+   protected GameCanvasRunnableInterface createDemoGameCanvasRunnableInterface() throws Exception
+   {
+      return new TestGameDemoStartCanvas(this);
+   }
+
+   protected GameCanvasRunnableInterface createGameCanvasRunnableInterface(
+		   AllBinaryGameLayerManager allBinaryGameLayerManager) throws Exception
+   {
+	   return new TestGameDemoGameCanvas(this, allBinaryGameLayerManager);
+   }
+   
+   protected HighScoresCanvas createHighScoresCanvas() throws Exception
+   {
+       return new HighScoresCanvas(this,
+               this.createGameLayerManager(),
+               new HighScoresPaintable(),
+               new BasicHighScoresFactory(TestGameDemoSoftwareInfo.getInstance()));
+   }
+
+   public int getHighestLevel()
+   {
+       //return LicenseLevelUtil.getMaxLevel(
+       //      TestGameDemoLayerManager.MAX_LEVEL, 1);
+       return TestGameDemoLayerManager.MAX_LEVEL;
+       
+   }
+   
+   protected AllBinaryGameLayerManager createGameLayerManager()
+   {
+       GameInfo gameInfo = new GameInfo(
+               GameType.SINGLE_PLAYER, GameMode.SERVER,
+               PlayerTypesFactory.getInstance().PLAYER_TYPE_ONE,
+               this.getHighestLevel(), 1);
+
+       return new TestGameDemoLayerManager(gameInfo);
+   }
+
+   /*
+   protected void mediaInit() throws Exception
+   {
+   }
+   */
+
+   protected void mediaShutdown() throws Exception
+   {
+        //PreLogUtil.put(CommonStrings.getInstance().START, this, "mediaShutdown - postStopGameCanvasRunnableInterface");
+
+        LogUtil.put(LogFactory.getInstance(CommonStrings.getInstance().START, this,
+                "mediaShutdown - postStopGameCanvasRunnableInterface"));
+
+        AllBinaryMediaManagerShutdown.shutdown(
+           TestGameDemoSoundsFactoryFactory.getInstance());
+
+        LogUtil.put(LogFactory.getInstance(CommonStrings.getInstance().END, this,
+                "mediaShutdown - postStopGameCanvasRunnableInterface"));
+   }
+}
