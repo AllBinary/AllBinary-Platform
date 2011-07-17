98e833b42ee9982495b64854cacb6da619dc5336
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-17 07:26:48 -0500

    added

---
+++ j2me/games/test/TestGameDemoJavaLibrary/src/org/allbinary/game/testgamedemo/canvas/TestGameDemoSoftwareInfo.java
@@ -0,0 +1,28 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.game.testgamedemo.canvas;
+
+import abcs.logic.system.SoftwareInformation;
+
+public class TestGameDemoSoftwareInfo
+{
+    //This should change in next Android Release by moving AndroidMobile info only the client info
+    private static final SoftwareInformation SINGLETON = 
+        new SoftwareInformation("TestDemoGame", "1.1.7");
+    
+    public static SoftwareInformation getInstance()
+    {
+        return SINGLETON;
+    }
+}
