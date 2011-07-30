aef49178f1a08d7803b7ec142238e713f66ef92b
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-29 23:05:54 -0500

    updated

--- j2me/input/NoGyroSensorJavaLibrary/src/allbinary/input/motion/gesture/TouchMotionGestureFactory.java
+++ j2me/input/NoGyroSensorJavaLibrary/src/allbinary/input/motion/gesture/TouchMotionGestureFactory.java
@@ -14,38 +14,69 @@
 package allbinary.input.motion.gesture;
 
 import allbinary.game.input.Input;
+import allbinary.game.input.InputFactory;
 
 public class TouchMotionGestureFactory
 {
     private static final TouchMotionGestureFactory MOTION = new TouchMotionGestureFactory();
-
-    public final MotionGestureInput LEFT = new MotionGestureInput(Input.MAX - 3, "Left Touch");
-
-    public final MotionGestureInput RIGHT = new MotionGestureInput(Input.MAX - 4, "Right Touch");
-    
-    public final MotionGestureInput UP = new MotionGestureInput(Input.MAX - 5, "Up Touch");
-
-    public final MotionGestureInput DOWN = new MotionGestureInput(Input.MAX - 6, "Down Touch");
-
-    public final MotionGestureInput PRESSED = new MotionGestureInput(Input.MAX - 7, "Screen Press");
-    public final MotionGestureInput RELEASED = new MotionGestureInput(Input.MAX - 8, "Released");
-
-    public final MotionGestureInput DIAGONAL_DOWN_RIGHT = new MotionGestureInput(Input.MAX - 9, "Diagonal Dn R");
-
-    public final MotionGestureInput DIAGONAL_DOWN_LEFT = new MotionGestureInput(Input.MAX - 10, "Diagonal Dn L");
-
-    public final MotionGestureInput DIAGONAL_UP_RIGHT = new MotionGestureInput(Input.MAX - 11, "Diagonal Up R");
-
-    public final MotionGestureInput DIAGONAL_UP_LEFT = new MotionGestureInput(Input.MAX - 12, "Diagonal Up L");
-    
-    //public final MotionGestureInput TOUCH = new MotionGestureInput(Input.MAX - 12, "Touch");
-    
-    public final MotionGestureInput NO_MOTION = new MotionGestureInput(Input.MAX - 2, "No Motion");
-    
-    public final MotionGestureInput LAST_MOTION = new MotionGestureInput(Input.MAX - 19, "Last Motion");
     
     public static final TouchMotionGestureFactory getInstance()
     {
         return MOTION;
+    }
+
+    public final MotionGestureInput LEFT;
+
+    public final MotionGestureInput RIGHT;
+    
+    public final MotionGestureInput UP;
+
+    public final MotionGestureInput DOWN;
+
+    public final MotionGestureInput PRESSED;
+    public final MotionGestureInput RELEASED;
+
+    public final MotionGestureInput DIAGONAL_DOWN_RIGHT;
+
+    public final MotionGestureInput DIAGONAL_DOWN_LEFT;
+
+    public final MotionGestureInput DIAGONAL_UP_RIGHT;
+
+    public final MotionGestureInput DIAGONAL_UP_LEFT;
+    
+    //public final MotionGestureInput TOUCH = new MotionGestureInput(Input.MAX - 12, "Touch");
+    
+    public final MotionGestureInput NO_MOTION;
+    
+    public final MotionGestureInput LAST_MOTION;
+    
+    private TouchMotionGestureFactory()
+    {
+        int MAX = InputFactory.getInstance().MAX;
+        
+        LEFT = new MotionGestureInput(MAX - 3, "Left Touch");
+
+        RIGHT = new MotionGestureInput(MAX - 4, "Right Touch");
+
+        UP = new MotionGestureInput(MAX - 5, "Up Touch");
+
+        DOWN = new MotionGestureInput(MAX - 6, "Down Touch");
+
+        PRESSED = new MotionGestureInput(MAX - 7, "Screen Press");
+        RELEASED = new MotionGestureInput(MAX - 8, "Released");
+
+        DIAGONAL_DOWN_RIGHT = new MotionGestureInput(MAX - 9, "Diagonal Dn R");
+
+        DIAGONAL_DOWN_LEFT = new MotionGestureInput(MAX - 10, "Diagonal Dn L");
+
+        DIAGONAL_UP_RIGHT = new MotionGestureInput(MAX - 11, "Diagonal Up R");
+
+        DIAGONAL_UP_LEFT = new MotionGestureInput(MAX - 12, "Diagonal Up L");
+
+        //TOUCH = new MotionGestureInput(MAX - 12, "Touch");
+
+        NO_MOTION = new MotionGestureInput(MAX - 2, "No Motion");
+
+        LAST_MOTION = new MotionGestureInput(MAX - 19, "Last Motion");
     }
 }
