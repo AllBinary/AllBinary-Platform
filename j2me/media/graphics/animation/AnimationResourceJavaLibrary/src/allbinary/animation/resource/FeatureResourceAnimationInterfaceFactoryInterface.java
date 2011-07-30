26d0b5df2bc8c297b783af6ecc65c35165ce991f
Travis Berthelot <travisberthelot@hotmail.com>
2011-07-29 23:06:44 -0500

    updated

--- j2me/media/graphics/animation/AnimationResourceJavaLibrary/src/allbinary/animation/resource/FeatureResourceAnimationInterfaceFactoryInterface.java
+++ j2me/media/graphics/animation/AnimationResourceJavaLibrary/src/allbinary/animation/resource/FeatureResourceAnimationInterfaceFactoryInterface.java
@@ -15,8 +15,9 @@ package allbinary.animation.resource;
 
 import java.util.Hashtable;
 
+import org.allbinary.game.resource.RectangleResourceFactoryInterface;
+
 import allbinary.game.configuration.feature.GameFeatureControlledInterface;
-import allbinary.game.resource.RectangleResourceFactoryInterface;
 
 public interface FeatureResourceAnimationInterfaceFactoryInterface
    extends ResourceAnimationInterfaceFactoryInterface, 
