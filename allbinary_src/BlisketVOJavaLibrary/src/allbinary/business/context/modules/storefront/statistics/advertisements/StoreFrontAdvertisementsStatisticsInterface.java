ea79f8fc94d879a69a8dd80dc2718043e24ac776
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-01 23:55:35 -0500

    added

---
+++ allbinary_src/BlisketVOJavaLibrary/src/allbinary/business/context/modules/storefront/statistics/advertisements/StoreFrontAdvertisementsStatisticsInterface.java
@@ -0,0 +1,22 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.context.modules.storefront.statistics.advertisements;
+
+import allbinary.data.tables.TableMappingInterface;
+
+public interface StoreFrontAdvertisementsStatisticsInterface extends TableMappingInterface
+{
+   //public long getNumber();
+   //public Money getTotal();
+}
