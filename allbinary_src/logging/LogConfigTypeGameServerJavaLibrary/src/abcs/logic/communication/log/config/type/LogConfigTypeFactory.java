861a91ad5c9bc64c5f1ee8a3b92572eece291331
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-01 22:04:14 -0500

    added

---
+++ allbinary_src/logging/LogConfigTypeGameServerJavaLibrary/src/abcs/logic/communication/log/config/type/LogConfigTypeFactory.java
@@ -0,0 +1,45 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package abcs.logic.communication.log.config.type;
+
+/**
+ *
+ * @author user
+ */
+public class LogConfigTypeFactory
+{
+
+    private static final LogConfigTypeFactory instance = new LogConfigTypeFactory();
+
+    /**
+     * @return the instance
+     */
+    public static LogConfigTypeFactory getInstance()
+    {
+        return instance;
+    }
+
+    private final String NO_DESCRIPTION = "No Description";
+
+    public final LogConfigType INIT_SERVER = new LogConfigType("Init Server", NO_DESCRIPTION);
+    public final LogConfigType LOBBY_SERVER = new LogConfigType("Lobby Server", NO_DESCRIPTION);
+    public final LogConfigType GAME_SERVER = new LogConfigType("Game Server", NO_DESCRIPTION);
+    public final LogConfigType GAME_SIMULATOR = new LogConfigType("Game Simulator", NO_DESCRIPTION);
+    public final LogConfigType CUSTOM_TYPES = new LogConfigType("Custom Types", NO_DESCRIPTION);
+
+    private LogConfigTypeFactory()
+    {
+        LogConfigTypes.LOGGING.add(this.INIT_SERVER);
+    }
+}
