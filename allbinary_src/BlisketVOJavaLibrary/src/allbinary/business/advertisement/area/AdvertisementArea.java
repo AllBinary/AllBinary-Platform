e0e3083de47826879eb62cbf2d6bfd009f626757
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-01 23:52:37 -0500

    added

---
+++ allbinary_src/BlisketVOJavaLibrary/src/allbinary/business/advertisement/area/AdvertisementArea.java
@@ -0,0 +1,103 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.advertisement.area;
+
+import java.util.HashMap;
+
+import org.w3c.dom.Document;
+
+import allbinary.business.advertisement.campaign.AdvertisementCampaignData;
+
+import allbinary.business.context.modules.storefront.StoreFrontData;
+
+import abcs.data.tree.dom.document.DomDocumentHelper;
+
+public class AdvertisementArea implements AdvertisementAreaInterface
+{
+   private String name;
+   private String storeName;
+   private String description;
+   private AdvertisementAreaConstraintsInterface advertisementAreaConstraintsInterface;
+   
+   //Ad campaign associated with the ad area
+   private String campaign;
+   
+   public AdvertisementArea(HashMap hashMap) throws Exception
+   {
+      this.name = (String) hashMap.get(AdvertisementAreaData.getInstance().NAME);
+      this.storeName = (String) hashMap.get(StoreFrontData.getInstance().NAME);
+      this.description = (String) hashMap.get(AdvertisementAreaData.getInstance().DESCRIPTION);
+
+      String constraintsDomString = 
+         (String) hashMap.get(AdvertisementAreaData.getInstance().CONSTRAINTS);
+      Document document = DomDocumentHelper.create(constraintsDomString);
+
+      this.advertisementAreaConstraintsInterface
+         = new AdvertisementAreaConstraints(document);
+
+      this.campaign = (String) hashMap.get(AdvertisementCampaignData.getInstance().NAME);
+   }
+
+   public String getName()
+   {
+      return this.name;
+   }
+   
+   public String getStoreName()
+   {
+      return this.storeName;
+   }
+
+   public String getDescription()
+   {
+      return this.description;
+   }
+
+   public AdvertisementAreaConstraintsInterface getConstraints()
+   {
+      return this.advertisementAreaConstraintsInterface;
+   }
+   
+   public String getCampaign()
+   {
+      return this.campaign;
+   }
+
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+
+   public void setStoreName(String storeName)
+   {
+      this.storeName = storeName;
+   }
+   
+   public void setDescription(String description)
+   {
+      this.description = description;
+   }
+
+   public void setConstraints(
+      AdvertisementAreaConstraintsInterface advertisementConstraintsInterface)
+   {
+      this.advertisementAreaConstraintsInterface = 
+         advertisementAreaConstraintsInterface;
+   }
+
+   public void setCampaign(String campaign)
+   {
+      this.campaign = campaign;
+   }
+}
