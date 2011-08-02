6779f79b136074864e3b2496697b13c70e8e4c46
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 01:21:17 -0500

    added

---
+++ allbinary_src/SMTPConfigurationJavaLibrary/src/allbinary/business/time/modified/TimeLastModified.java
@@ -0,0 +1,29 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.time.modified;
+
+import allbinary.business.time.Time;
+
+public class TimeLastModified extends Time
+{
+   public TimeLastModified(String time)
+   {
+      super(time);
+   }
+   
+   public TimeLastModified(long time)
+   {
+      super(time);
+   }
+}
