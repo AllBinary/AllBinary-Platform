1c601db8c34531d8d7a532aa777732bbea04036b
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-01 23:59:02 -0500

    added

---
+++ allbinary_src/BlisketVOJavaLibrary/src/allbinary/business/user/CreateUserFactory.java
@@ -0,0 +1,50 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user;
+
+import abcs.logic.basic.string.StringValidationUtil;
+import allbinary.business.user.role.UserRole;
+import allbinary.business.user.role.UserRoleB;
+import allbinary.business.user.role.UserRoleData;
+
+import java.util.HashMap;
+
+public class CreateUserFactory
+{
+   private CreateUserFactory()
+   {
+   }
+
+   public static UserInterface getInstance(HashMap hashMap) throws Exception
+   {
+      if(hashMap != null)
+      {
+         String roleString = (String) hashMap.get(UserRoleData.NAME.toString());
+         
+         if(!StringValidationUtil.getInstance().isEmpty(roleString))
+         {
+            UserRole role = UserRoleB.getRole(roleString);
+            return role.getUserFactory().getInstance(hashMap);
+         }
+         else 
+         {
+            throw new Exception("No Role Specified");
+         }
+      }
+      else 
+      {
+         throw new Exception("Null HashMap");
+      }
+   }   
+}
