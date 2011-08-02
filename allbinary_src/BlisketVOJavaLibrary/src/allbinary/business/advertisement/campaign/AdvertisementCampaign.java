782cd421c8e87c71887c5f98b6f16417d32b2ec2
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-01 23:53:09 -0500

    added

---
+++ allbinary_src/BlisketVOJavaLibrary/src/allbinary/business/advertisement/campaign/AdvertisementCampaign.java
@@ -0,0 +1,58 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.advertisement.campaign;
+
+import java.util.HashMap;
+
+import allbinary.business.DynamicObjectData;
+import allbinary.business.advertisement.AdvertisementsInterface;
+import allbinary.business.advertisement.search.AdvertisementSearchInterface;
+
+public class AdvertisementCampaign implements AdvertisementCampaignInterface
+{
+   private HashMap hashMap;
+   
+   public AdvertisementCampaign(HashMap hashMap)
+   {
+      this.hashMap = hashMap;
+   }
+
+   public Object getComponent()
+   {
+      //return (Object) this.hashMap.get(AdvertisementCampaignData.NAME);
+      return null;
+   }
+   
+   public String getName()
+   {
+      return (String) this.hashMap.get(AdvertisementCampaignData.getInstance().NAME);
+   }
+   
+   public void setComponentName(String name)
+   {
+      this.hashMap.put(DynamicObjectData.NAME, name);
+   }
+   
+   public void setName(String name)
+   {
+      this.hashMap.put(AdvertisementCampaignData.getInstance().NAME, name);
+   }
+   
+   //Will Retrieve the appropriate advertisment from the selected campaigns/advertisementPrograms
+   public AdvertisementsInterface search(
+      AdvertisementSearchInterface advertisementSearchInterface)
+   {
+      return null;
+   }
+}
