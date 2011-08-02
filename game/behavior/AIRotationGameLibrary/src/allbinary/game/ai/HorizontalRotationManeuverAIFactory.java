f47f2f2653661cad05c1fa88c43cc01efab7b4aa
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 01:07:29 -0500

    added

---
+++ game/behavior/AIRotationGameLibrary/src/allbinary/game/ai/HorizontalRotationManeuverAIFactory.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.ai;
+
+import java.util.Hashtable;
+
+import allbinary.ai.ArtificialIntelligenceInterface;
+import allbinary.game.input.GameInput;
+import allbinary.layer.AllBinaryLayer;
+
+public class HorizontalRotationManeuverAIFactory 
+    implements ArtificialIntelligenceInterfaceFactoryInterface
+{
+    public ArtificialIntelligenceInterface getInstance(
+          Hashtable hashtable, AllBinaryLayer ownerLayerInterface, GameInput gameInput)
+    {
+        return new HorizontaRotationManeuverAI(ownerLayerInterface, gameInput);
+    }
+}
