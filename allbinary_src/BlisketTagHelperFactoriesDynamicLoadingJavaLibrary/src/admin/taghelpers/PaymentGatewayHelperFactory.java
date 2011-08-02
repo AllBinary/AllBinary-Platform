72aeac1be891a2374290941ba995d81ab44512f3
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 00:12:08 -0500

    added

---
+++ allbinary_src/BlisketTagHelperFactoriesDynamicLoadingJavaLibrary/src/admin/taghelpers/PaymentGatewayHelperFactory.java
@@ -0,0 +1,39 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.taghelpers;
+
+import java.util.HashMap;
+
+import javax.servlet.jsp.PageContext;
+
+
+import abcs.logic.system.security.licensing.LicensingException;
+
+
+public class PaymentGatewayHelperFactory implements TagHelperFactoryInterface
+{
+   private static final String CLASSNAME = "admin.taghelpers.PaymentGatewayHelper";
+   private static final String FACTORYNAME = "PaymentGatewayFactory";
+   
+   public PaymentGatewayHelperFactory()
+   {
+   }
+   
+   public Object getInstance(
+      HashMap hashMap, PageContext pageContext) 
+      throws LicensingException
+   {
+      return HelperFactory.getInstance(FACTORYNAME, CLASSNAME, hashMap, pageContext);
+   }  
+}
