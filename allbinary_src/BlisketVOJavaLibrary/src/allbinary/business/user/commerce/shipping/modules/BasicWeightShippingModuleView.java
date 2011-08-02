53a32214a8ebfbd068fc3488cee1a529c15bb268
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-01 23:57:26 -0500

    added

---
+++ allbinary_src/BlisketVOJavaLibrary/src/allbinary/business/user/commerce/shipping/modules/BasicWeightShippingModuleView.java
@@ -0,0 +1,123 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.commerce.shipping.modules;
+
+import abcs.data.tree.dom.document.DomDocumentHelper;
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.business.user.commerce.shipping.ShippingMethodData;
+import allbinary.data.tree.dom.DomNodeInterface;
+import allbinary.data.tree.dom.ModDomHelper;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+public class BasicWeightShippingModuleView extends BasicWeightShippingModule implements DomNodeInterface
+{
+   public BasicWeightShippingModuleView()
+   {
+   }
+   
+   public Boolean isValid()
+   {
+      return null;
+   }
+   
+   public org.w3c.dom.Document toValidationInfoDoc()
+   {
+      return null;
+   }
+   
+   public org.w3c.dom.Node toValidationInfoNode(org.w3c.dom.Document document)
+   {
+      return null;
+   }
+   
+   public String validationInfo()
+   {
+      return null;
+   }
+   
+   public org.w3c.dom.Node toXmlNode(org.w3c.dom.Document document)
+   {
+      try
+      {
+         Node shippingMethodNode =
+         document.createElement(
+         ShippingMethodData.NAME);
+         
+         shippingMethodNode.appendChild(
+         ModDomHelper.createNameValueNodes(
+         document, ShippingMethodData.NAME, this.getName()));
+         
+         shippingMethodNode.appendChild(
+         ModDomHelper.createNameValueNodes(
+         document, ShippingMethodData.DESCRIPTION, this.getDescription()));
+         
+         return shippingMethodNode;
+      }
+      catch(Exception e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLLOGGING))
+         {
+            LogUtil.put(LogFactory.getInstance("Command Failed", this, "toXmlNode", e));
+         }
+         //throw e;
+         return null;
+      }
+   }
+   
+   public org.w3c.dom.Document toXmlDoc()
+   {
+      try
+      {
+         Document document = DomDocumentHelper.create();
+         /*
+         Node contentTypeNode =
+         document.createElement(this.contentType);
+         document.appendChild(contentTypeNode);
+          
+         contentTypeNode.appendChild(this.storeFrontInterface.toXmlNode(document));
+         contentTypeNode.appendChild(this.toXmlNode(document));
+          */
+         return document;
+      }
+      catch(Exception e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLLOGGING))
+         {
+            LogUtil.put(LogFactory.getInstance("Command Failed", this, "toXmlDoc", e));
+         }
+         //throw e;
+         return null;
+      }
+   }
+   
+   public String view()
+   {
+      try
+      {
+         return "Not Implemented";
+      }
+      catch(Exception e)
+      {
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.SQLLOGGING))
+         {
+            LogUtil.put(LogFactory.getInstance("Command Failed", this, "view", e));
+         }
+         //throw e;
+         return null;
+      }
+   }
+   
+}
