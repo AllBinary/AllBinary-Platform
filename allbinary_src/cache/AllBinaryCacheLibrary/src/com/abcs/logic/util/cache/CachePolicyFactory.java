36f7b7e3f54c5968438e5b22b8dc0fd378942026
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 00:28:53 -0500

    added

---
+++ allbinary_src/cache/AllBinaryCacheLibrary/src/com/abcs/logic/util/cache/CachePolicyFactory.java
@@ -0,0 +1,42 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package com.abcs.logic.util.cache;
+
+public class CachePolicyFactory
+{
+    private static final CachePolicyFactory instance = new CachePolicyFactory();
+    
+    public static CachePolicyFactory getInstance()
+    {
+        return instance;
+    }
+    
+    private final String NAME = "lru";
+    
+    public CachePolicy MAX_TIME_THOUSAND_MAX = 
+        new CachePolicy(NAME, Long.MAX_VALUE, 1000);
+
+    public CachePolicy THIRTY_MINUTES_TEN_THOUSAND_MAX = 
+        new CachePolicy(NAME, 1000 * 60 * 30, 10000);
+
+    public CachePolicy THIRTY_MINUTES_FIFTY_THOUSAND_MAX = 
+        new CachePolicy(NAME, 1000 * 60 * 30, 100000);
+    
+    public CachePolicy ONE_MINUTE_FIVE_MAX = 
+        new CachePolicy(NAME, 1000 * 60 * 1, 5);
+
+    public CachePolicy ONE_MINUTE_TEN_MAX = 
+        new CachePolicy(NAME, 1000 * 60 * 1, 10);
+    
+}
