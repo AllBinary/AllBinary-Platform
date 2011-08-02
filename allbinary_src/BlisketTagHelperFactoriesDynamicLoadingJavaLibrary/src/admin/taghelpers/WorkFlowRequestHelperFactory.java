ae23c518ceb83c6ed6495d8b82101526d9d7ea8e
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 00:14:58 -0500

    added

---
+++ allbinary_src/BlisketTagHelperFactoriesDynamicLoadingJavaLibrary/src/admin/taghelpers/WorkFlowRequestHelperFactory.java
@@ -0,0 +1,39 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.taghelpers;
+
+import java.util.HashMap;
+
+import javax.servlet.jsp.PageContext;
+
+
+import abcs.logic.system.security.licensing.LicensingException;
+
+
+public class WorkFlowRequestHelperFactory implements TagHelperFactoryInterface
+{
+   private static final String CLASSNAME = "admin.taghelpers.WorkFlowHelper";
+   private static final String FACTORYNAME = "WorkFlowHelperFactory";
+   
+   public WorkFlowRequestHelperFactory()
+   {
+   }
+   
+   public Object getInstance(
+      HashMap hashMap, PageContext pageContext) 
+      throws LicensingException
+   {
+      return HelperFactory.getInstance(FACTORYNAME, CLASSNAME, hashMap, pageContext);
+   }  
+}
