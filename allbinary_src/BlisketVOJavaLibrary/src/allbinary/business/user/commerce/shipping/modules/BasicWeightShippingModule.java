d79e5560d2bb2130cad2b88a4df2f5048b63c9e1
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-01 23:57:18 -0500

    added

---
+++ allbinary_src/BlisketVOJavaLibrary/src/allbinary/business/user/commerce/shipping/modules/BasicWeightShippingModule.java
@@ -0,0 +1,49 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.user.commerce.shipping.modules;
+
+import allbinary.business.user.commerce.inventory.basket.BasketInterface;
+import allbinary.business.user.commerce.inventory.order.OrderInterface;
+import allbinary.business.user.commerce.money.Money;
+
+public class BasicWeightShippingModule implements ShippingInterface
+{
+   private final String name = "Basic Shipping (5 to 7 working days)";
+   private final String description = "We will soon provide a variety of shipping methods.";
+
+   public BasicWeightShippingModule()
+   {            
+   }
+
+   public Money getCost(OrderInterface order)
+   {
+      BasketInterface basket = order.getBasket();
+      
+      Money money = new Money();            
+      money.add(basket.getTotalWeight());
+      money.multiply(1);      
+      money.add("3.50");
+      return money;
+   }
+   
+   public String getDescription()
+   {
+      return this.description;
+   }
+   
+   public String getName()
+   {
+      return this.name;
+   }           
+}
