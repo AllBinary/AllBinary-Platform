3a2e434a7d2ff181ec27df67a168226e8026bb93
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 00:00:16 -0500

    added

---
+++ allbinary_src/BlisketVOJavaLibrary/src/allbinary/globals/UrlGlobalsView.java
@@ -0,0 +1,41 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.globals;
+
+import allbinary.data.tree.dom.DomNodeInterface;
+import allbinary.data.tree.dom.ModDomHelper;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import java.util.HashMap;
+
+public class UrlGlobalsView implements DomNodeInterface
+{
+   public UrlGlobalsView()
+   {
+   }
+   
+   public Node toXmlNode(Document document) throws Exception
+   {
+      HashMap hashMap = new HashMap();
+      
+      hashMap.put(UrlGlobalsData.WEBAPPPATH, abcs.globals.URLGLOBALS.getWebappPath());
+      
+      Node node = 
+         ModDomHelper.createNameValueNodes(
+            document, UrlGlobalsData.NAME, hashMap);
+
+      return node;
+   }
+}
