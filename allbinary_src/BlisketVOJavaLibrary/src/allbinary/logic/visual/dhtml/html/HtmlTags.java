0b62a012c3bdbe17bd010986b61430cd5a1ba0ab
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 00:04:17 -0500

    added

---
+++ allbinary_src/BlisketVOJavaLibrary/src/allbinary/logic/visual/dhtml/html/HtmlTags.java
@@ -0,0 +1,41 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.logic.visual.dhtml.html;
+
+import java.util.Vector;
+
+public class HtmlTags
+{
+   private Vector htmlTagVector;
+   
+   public HtmlTags()
+   {
+      htmlTagVector = new Vector();
+   }
+
+   public HtmlTag get(int index)
+   {
+      return (HtmlTag) htmlTagVector.get(index);
+   }
+
+   public void add(HtmlTag htmlTag)
+   {
+      htmlTagVector.add(htmlTag);
+   }
+   
+   public int size()
+   {
+      return htmlTagVector.size();
+   }
+}
