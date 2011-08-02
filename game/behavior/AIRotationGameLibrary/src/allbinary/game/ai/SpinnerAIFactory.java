8a372473057c9d4994ce441c4e6b446adae4370e
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 01:08:46 -0500

    added

---
+++ game/behavior/AIRotationGameLibrary/src/allbinary/game/ai/SpinnerAIFactory.java
@@ -0,0 +1,33 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.game.ai;
+
+import java.util.Hashtable;
+
+import javax.microedition.lcdui.Canvas;
+
+import allbinary.ai.ArtificialIntelligenceInterface;
+import allbinary.game.input.GameInput;
+import allbinary.layer.AllBinaryLayer;
+
+public class SpinnerAIFactory 
+    implements ArtificialIntelligenceInterfaceFactoryInterface
+{
+    public ArtificialIntelligenceInterface getInstance(
+          Hashtable hashtable, AllBinaryLayer ownerLayerInterface, GameInput gameInput)
+    {
+        int direction[] = {Canvas.LEFT};
+        return new SpinnerAI(direction, ownerLayerInterface, gameInput);
+    }
+}
