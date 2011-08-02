cb4a337a1a060bb81c9ad0c459118e03b28aa49d
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-01 22:11:55 -0500

    added

---
+++ allbinary_src/data/EntityPaymentGatewayJavaLibrary/src/allbinary/data/tables/user/commerce/money/payment/gateway/PaymentGatewayEntityFactory.java
@@ -0,0 +1,56 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.data.tables.user.commerce.money.payment.gateway;
+
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+
+public class PaymentGatewayEntityFactory
+{
+   //private static final String CLASSNAME = "allbinary.data.tables.user.commerce.money.payment.gateway.PaymentGatewayEntity";
+   
+   private PaymentGatewayEntityFactory()
+   {
+   }
+   
+   public static PaymentGatewayEntityInterface getInstance() throws Exception
+   {
+      try
+      {
+         //Object object = AbeFactory.getInstance(CLASSNAME);
+         //return (PaymentGatewayTransactionEntityInterface) InterfaceCastProxy.newInstance(object);
+         return (PaymentGatewayEntityInterface) new allbinary.data.tables.user.commerce.money.payment.gateway.PaymentGatewayEntity();
+      }
+      /*
+      catch(LicensingException e)
+      {
+         String error = "Failed to get instance";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.ENTITYFACTORYERROR))
+         {
+            LogUtil.put(error,"PaymentGatewayTransactionEntityFactory","getInstance()",e);
+         }
+         throw e;
+      }
+       */
+      catch(Exception e)
+      {
+         String error = "Failed to get instance";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.ENTITYFACTORYERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error, "PaymentGatewayEntityFactory", "getInstance()", e));
+         }
+         throw e;
+      }
+   }
+}
