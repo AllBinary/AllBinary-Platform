83f872efcd164902815972d047968f5ca15d4afc
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-01 23:53:55 -0500

    added

---
+++ allbinary_src/BlisketVOJavaLibrary/src/allbinary/business/advertisement/image/AdvertisementImage.java
@@ -0,0 +1,42 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.business.advertisement.image;
+
+import allbinary.logic.control.contraints.size.TwoDimensionalConstraintInterface;
+
+public class AdvertisementImage implements AdvertisementImageInterface
+{
+   private String imageFileName;
+   private String imageUrl;
+   private TwoDimensionalConstraintInterface constraint;
+
+   public AdvertisementImage()
+   {
+   }
+   
+   public String getFileName()
+   {
+      return this.imageFileName;
+   }
+   
+   public String getUrl()
+   {
+      return this.imageUrl;
+   }
+   
+   public TwoDimensionalConstraintInterface getTwoDimensionalConstraint()
+   {
+      return this.constraint;
+   }
+}
