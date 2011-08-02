20610d7cdc3e911d317ed58e4ebae199cc1b5900
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-01 23:48:26 -0500

    added

---
+++ allbinary_src/CustomLoaderJavaLibrary/src/admin/taghelpers/CustomLoaderHelper.java
@@ -0,0 +1,70 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package admin.taghelpers;
+
+import abcs.globals.AppUrlGlobals;
+import abcs.globals.URLGLOBALS;
+import abcs.logic.communication.log.LogFactory;
+import javax.servlet.jsp.PageContext;
+
+import abcs.logic.communication.log.LogUtil;
+import java.util.HashMap;
+
+public class CustomLoaderHelper
+{
+   public CustomLoaderHelper(HashMap hashMap, PageContext pageContext)
+   {
+   }
+
+   public CustomLoaderHelper()
+   {
+   }
+   
+   public String getWebappPath() throws Exception
+   {
+      try
+      {
+         return abcs.globals.URLGLOBALS.getWebappPath();
+      }
+      catch(Exception e)
+      {
+         /*
+         String error = "Failed to getWebappPath";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.TAGHELPERERROR))
+         {
+            LogUtil.put(error,this,"getWebappPath()",e);
+         }*/
+         return null;
+      }
+   }
+
+   public void setWebappPath(String path) throws Exception
+   {
+      try
+      {
+          AppUrlGlobals urlGlobals = new AppUrlGlobals();
+          urlGlobals.setWebappPath(path);
+         URLGLOBALS.init(urlGlobals);
+      }
+      catch(Exception e)
+      {         
+         String error = "Failed to getWebappPath";
+         if(abcs.logic.communication.log.config.type.LogConfigTypes.LOGGING.contains(abcs.logic.communication.log.config.type.LogConfigType.TAGHELPERERROR))
+         {
+            LogUtil.put(LogFactory.getInstance(error,this,"setWebappPath()",e));
+         }
+      }
+   }
+   
+}
