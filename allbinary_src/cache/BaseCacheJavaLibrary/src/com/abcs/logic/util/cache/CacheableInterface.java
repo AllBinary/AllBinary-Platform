f1d78bbec919c159001c34bc0d438dacb84e7ccf
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 00:24:11 -0500

    added

---
+++ allbinary_src/cache/BaseCacheJavaLibrary/src/com/abcs/logic/util/cache/CacheableInterface.java
@@ -0,0 +1,21 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package com.abcs.logic.util.cache;
+
+public interface CacheableInterface
+{
+   Object getKey();
+   //@Override
+   //String toString();
+}
