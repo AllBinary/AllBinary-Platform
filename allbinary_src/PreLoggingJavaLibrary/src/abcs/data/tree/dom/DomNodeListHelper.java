3efc0f337e620dadbd36500fc1c2f16aed68f6cd
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 00:56:11 -0500

    added

---
+++ allbinary_src/PreLoggingJavaLibrary/src/abcs/data/tree/dom/DomNodeListHelper.java
@@ -0,0 +1,35 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package abcs.data.tree.dom;
+
+import java.util.Vector;
+import org.w3c.dom.NodeList;
+
+/**
+ *
+ * @author user
+ */
+public class DomNodeListHelper {
+
+    public static Vector getVector(NodeList nodeList)
+    {
+        Vector vector = new Vector();
+        int size = nodeList.getLength();
+        for(int index = 0; index < size; index++)
+        {
+            vector.add(DomNodeHelper.getTextNodeValue(nodeList.item(index)));
+        }
+        return vector;
+    }
+}
