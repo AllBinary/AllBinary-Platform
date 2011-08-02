2f935567f1ac44baa8b2ebc74320718e435d4bff
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-02 00:57:41 -0500

    added

---
+++ allbinary_src/PreLoggingJavaLibrary/src/abcs/logic/java/object/MethodUtil.java
@@ -0,0 +1,42 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package abcs.logic.java.object;
+
+import java.lang.reflect.Method;
+
+public class MethodUtil
+{
+   private MethodUtil()
+   {
+   }
+   
+   public static String viewAll(Class myClass, String lineBreak)
+   {
+      StringBuffer stringBuffer = new StringBuffer();
+      Method method[] = myClass.getMethods();
+      stringBuffer.append("Methods: ");
+      stringBuffer.append(lineBreak);
+      for(int index = 0; index < method.length; index++)
+      {
+         stringBuffer.append(method[index].getReturnType().getName());
+         stringBuffer.append(" ");
+         stringBuffer.append(method[index].getName());
+         stringBuffer.append("(");
+         ParamsUtil.viewParams(method[index].getParameterTypes());
+         stringBuffer.append(")");
+         stringBuffer.append(lineBreak);
+      }
+      return stringBuffer.toString();
+   }
+}
