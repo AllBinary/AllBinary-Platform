28df7605f1ba1e8a287a7476798756b68eb4260a
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-03 03:02:13 -0500

    added

---
+++ allbinary_src/logic/data/file/FileAdvancedJavaLibrary/src/abcs/logic/basic/io/file/visitor/IncludeFileExtensionsBooleanFileVisitor.java
@@ -0,0 +1,51 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package abcs.logic.basic.io.file.visitor;
+
+import abcs.logic.basic.io.file.AbFile;
+import java.util.Vector;
+
+public class IncludeFileExtensionsBooleanFileVisitor extends AbstractVectorBooleanFileVisitor
+{
+   public IncludeFileExtensionsBooleanFileVisitor(Vector filterStringVector)
+   {
+      super(filterStringVector);
+   }
+   
+   public Boolean visit(AbFile file)
+   {
+      if(this.getFilterStringVector().size() == 0)
+      {
+         return Boolean.FALSE;
+      }
+      
+      return super.visit(file);
+   }
+
+   public Boolean visit(AbFile file, String fileNameString)
+   {
+      String fileNameAndExtension = file.getPath();
+
+      if(fileNameAndExtension.length() > fileNameString.length())
+      {
+         int startIndex = fileNameAndExtension.length() - fileNameString.length();
+         String fileName = fileNameAndExtension.substring(startIndex);
+         if(fileName.compareTo(fileNameString) == 0)
+         {
+            return Boolean.TRUE;
+         }
+      }
+      return Boolean.FALSE;
+   }
+}
