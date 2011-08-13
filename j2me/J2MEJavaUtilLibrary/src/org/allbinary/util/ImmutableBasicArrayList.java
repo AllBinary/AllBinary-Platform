14fb1db3104b138df85761d6ea6b44b499d1a31c
Travis Berthelot <travisberthelot@hotmail.com>
2011-08-13 05:26:29 -0500

    added

---
+++ j2me/J2MEJavaUtilLibrary/src/org/allbinary/util/ImmutableBasicArrayList.java
@@ -0,0 +1,35 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.util;
+
+import abcs.logic.communication.log.ForcedLogUtil;
+
+public class ImmutableBasicArrayList extends BasicArrayList
+{
+    public ImmutableBasicArrayList(int size)
+    {
+        super(size);
+    }
+    
+    public boolean add(Object e)
+    {
+        ForcedLogUtil.log("Immutable", this);
+        return true;
+    }
+
+    public void add(int index, Object element)
+    {
+        ForcedLogUtil.log("Immutable", this);
+    }    
+}
