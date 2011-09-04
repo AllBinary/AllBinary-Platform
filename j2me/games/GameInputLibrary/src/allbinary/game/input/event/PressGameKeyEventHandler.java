dc57d8bf512f2450f0b6aba8cb188a48a7a77950
Travis Berthelot <travisberthelot@hotmail.com>
2011-09-04 01:58:23 -0500

    updated - performance enhancement

--- j2me/games/GameInputLibrary/src/allbinary/game/input/event/PressGameKeyEventHandler.java
+++ j2me/games/GameInputLibrary/src/allbinary/game/input/event/PressGameKeyEventHandler.java
@@ -13,22 +13,67 @@
 */
 package allbinary.game.input.event;
 
+import org.allbinary.util.BasicArrayList;
+
+import abcs.logic.basic.string.CommonStrings;
+import abcs.logic.communication.log.Log;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.game.input.PlayerGameInput;
 import allbinary.logic.basic.util.event.AllBinaryEventObject;
 import allbinary.logic.basic.util.event.EventListenerInterface;
 import allbinary.logic.basic.util.event.handler.BasicEventHandler;
 
 public class PressGameKeyEventHandler extends BasicEventHandler
 {
-   private static final PressGameKeyEventHandler gameKeyEventHandler = 
-      new PressGameKeyEventHandler();
-
-   private PressGameKeyEventHandler()
-   {
-   }
+   private static final PressGameKeyEventHandler instance = new PressGameKeyEventHandler();
 
    public static PressGameKeyEventHandler getInstance()
    {
-      return PressGameKeyEventHandler.gameKeyEventHandler;
+      return PressGameKeyEventHandler.instance;
+   }
+
+   private final BasicArrayList list = new BasicArrayList();
+   
+   private PressGameKeyEventHandler()
+   {
+   }
+   
+   public void addListener(PlayerGameInput playerGameInput)
+   {
+       if(!list.contains(playerGameInput))
+       {
+           list.add(playerGameInput);
+       }
+   }
+
+   public void removeAllListeners()
+   {
+       this.list.clear();
+       super.removeAllListeners();
+   }
+
+   public void removeListener(EventListenerInterface eventListenerInterface)
+   {
+       this.list.remove(eventListenerInterface);
+       super.removeListener(eventListenerInterface);
+   }
+
+   public void fireEvent(AllBinaryEventObject eventObject) throws Exception
+   {        
+       for (int index = this.list.size(); --index >= 0;)
+       {
+           try
+           {
+               PlayerGameInput playerGameInput = (PlayerGameInput) this.list.get(index);
+               playerGameInput.onPressGameKeyEvent((GameKeyEvent) eventObject);
+           }
+           catch (Exception e)
+           {
+               LogUtil.put(new Log(CommonStrings.getInstance().EXCEPTION, this, "fireEvent", e));
+           }
+       }
+
+       super.fireEvent(eventObject);
    }
    
    protected void process(AllBinaryEventObject eventObject,
