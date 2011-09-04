38c225fa1197878ac00526277bf47148325c30a2
Travis Berthelot <travisberthelot@hotmail.com>
2011-09-04 01:59:48 -0500

    updated - performance enhancement

--- j2me/input/MotionGestureBaseJavaLibrary/src/allbinary/input/motion/gesture/observer/BasicMotionGesturesHandler.java
+++ j2me/input/MotionGestureBaseJavaLibrary/src/allbinary/input/motion/gesture/observer/BasicMotionGesturesHandler.java
@@ -13,6 +13,7 @@
 */
 package allbinary.input.motion.gesture.observer;
 
+
 import allbinary.logic.basic.util.event.AllBinaryEventObject;
 import allbinary.logic.basic.util.event.EventListenerInterface;
 import allbinary.logic.basic.util.event.handler.BasicEventHandler;
@@ -27,9 +28,51 @@ public class BasicMotionGesturesHandler extends BasicEventHandler
         return SINGLETON;
     }
     
+    //private final BasicArrayList list = new BasicArrayList();
+    
     protected BasicMotionGesturesHandler()
     {
     }
+
+    /*
+    public void addListener(PlayerGameInput playerGameInput)
+    {
+        if(!list.contains(playerGameInput))
+        {
+            list.add(playerGameInput);
+        }
+    }
+
+    public void removeAllListeners()
+    {
+        this.list.clear();
+        super.removeAllListeners();
+    }
+
+    public void removeListener(EventListenerInterface eventListenerInterface)
+    {
+        this.list.remove(eventListenerInterface);
+        super.removeListener(eventListenerInterface);
+    }
+
+    public void fireEvent(AllBinaryEventObject eventObject) throws Exception
+    {        
+        for (int index = this.list.size(); --index >= 0;)
+        {
+            try
+            {
+                PlayerGameInput playerGameInput = (PlayerGameInput) this.list.get(index);
+                playerGameInput.onMotionGestureEvent((MotionGestureEvent) eventObject);
+            }
+            catch (Exception e)
+            {
+                LogUtil.put(new Log(CommonStrings.getInstance().EXCEPTION, this, "fireEvent", e));
+            }
+        }
+
+        super.fireEvent(eventObject);
+    }
+    */
 
     protected void process(AllBinaryEventObject eventObject,
            EventListenerInterface eventListenerInterface) throws Exception {
