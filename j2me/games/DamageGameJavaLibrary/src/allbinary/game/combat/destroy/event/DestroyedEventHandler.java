072876e8da4801bda8c34340f74c5008a300a05f
Travis Berthelot <travisberthelot@hotmail.com>
2011-09-04 01:57:03 -0500

    updated - performance enhancement

--- j2me/games/DamageGameJavaLibrary/src/allbinary/game/combat/destroy/event/DestroyedEventHandler.java
+++ j2me/games/DamageGameJavaLibrary/src/allbinary/game/combat/destroy/event/DestroyedEventHandler.java
@@ -13,23 +13,71 @@
 */
 package allbinary.game.combat.destroy.event;
 
+import org.allbinary.util.BasicArrayList;
+
+import abcs.logic.basic.string.CommonStrings;
+import abcs.logic.communication.log.Log;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.game.layer.special.CollidableDestroyableDamageableLayer;
 import allbinary.logic.basic.util.event.AllBinaryEventObject;
 import allbinary.logic.basic.util.event.EventListenerInterface;
 import allbinary.logic.basic.util.event.handler.BasicEventHandler;
 
 public class DestroyedEventHandler extends BasicEventHandler
 {
-   private static final DestroyedEventHandler destroyedEventHandler = 
+   private static final DestroyedEventHandler instance = 
       new DestroyedEventHandler();
 
+   public synchronized static DestroyedEventHandler getInstance()
+   {
+      return DestroyedEventHandler.instance;
+   }
+   
+   private final BasicArrayList list = new BasicArrayList();
+   
    private DestroyedEventHandler()
    {
    }
 
-   public synchronized static DestroyedEventHandler getInstance()
+   public void addListener(CollidableDestroyableDamageableLayer collidableDestroyableDamageableLayer)
    {
-      return DestroyedEventHandler.destroyedEventHandler;
+       if(!list.contains(collidableDestroyableDamageableLayer))
+       {
+           list.add(collidableDestroyableDamageableLayer);
+       }
    }
+
+   public void removeAllListeners()
+   {
+       this.list.clear();
+       super.removeAllListeners();
+   }
+
+   public void removeListener(EventListenerInterface eventListenerInterface)
+   {
+       this.list.remove(eventListenerInterface);
+       super.removeListener(eventListenerInterface);
+   }
+
+   public void fireEvent(AllBinaryEventObject eventObject) throws Exception
+   {        
+       for (int index = this.list.size(); --index >= 0;)
+       {
+           try
+           {
+               CollidableDestroyableDamageableLayer collidableDestroyableDamageableLayer = 
+                       (CollidableDestroyableDamageableLayer) this.list.get(index);
+               collidableDestroyableDamageableLayer.onDestroyed((DestroyedEvent) eventObject);
+           }
+           catch (Exception e)
+           {
+               LogUtil.put(new Log(CommonStrings.getInstance().EXCEPTION, this, "fireEvent", e));
+           }
+       }
+
+       super.fireEvent(eventObject);
+   }
+   
    
    protected void process(AllBinaryEventObject eventObject,
            EventListenerInterface eventListenerInterface) throws Exception {
