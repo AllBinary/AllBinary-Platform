9aa205317288bc39f38045720c8b01977623c361
Travis Berthelot <travisberthelot@hotmail.com>
2011-09-04 02:01:18 -0500

    updated

--- j2me/media/graphics/animation/vector/VectorAnimationJavaLibrary/src/allbinary/animation/VectorAnimationUtil.java
+++ j2me/media/graphics/animation/vector/VectorAnimationJavaLibrary/src/allbinary/animation/VectorAnimationUtil.java
@@ -29,7 +29,7 @@ public class VectorAnimationUtil
     private VectorAnimationUtil()
     {
     }
-
+    
     public int[][][] toAnimationArrayFromBasicArrayListOfPointBasicArrayList(
             BasicArrayList vector, int pointsPerFrame)
     {
