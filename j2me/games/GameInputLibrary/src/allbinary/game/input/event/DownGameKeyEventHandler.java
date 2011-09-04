4bebf552f27bad860cbfa6f4dddf21f0b205534b
Travis Berthelot <travisberthelot@hotmail.com>
2011-09-04 01:58:12 -0500

    updated - performance enhancement

--- j2me/games/GameInputLibrary/src/allbinary/game/input/event/DownGameKeyEventHandler.java
+++ j2me/games/GameInputLibrary/src/allbinary/game/input/event/DownGameKeyEventHandler.java
@@ -13,27 +13,72 @@
 */
 package allbinary.game.input.event;
 
+import org.allbinary.util.BasicArrayList;
+
+import abcs.logic.basic.string.CommonStrings;
+import abcs.logic.communication.log.Log;
+import abcs.logic.communication.log.LogUtil;
+import allbinary.game.input.PlayerGameInput;
 import allbinary.logic.basic.util.event.AllBinaryEventObject;
 import allbinary.logic.basic.util.event.EventListenerInterface;
 import allbinary.logic.basic.util.event.handler.BasicEventHandler;
 
 public class DownGameKeyEventHandler extends BasicEventHandler
 {
-    private static final DownGameKeyEventHandler gameKeyEventHandler = new DownGameKeyEventHandler();
+    private static final DownGameKeyEventHandler instance = new DownGameKeyEventHandler();
 
+    public static DownGameKeyEventHandler getInstance()
+    {
+        return instance;
+    }
+    
+    private final BasicArrayList list = new BasicArrayList();
+    
     private DownGameKeyEventHandler()
     {
     }
 
-    public static DownGameKeyEventHandler getInstance()
+    public void addListener(PlayerGameInput playerGameInput)
     {
-        return DownGameKeyEventHandler.gameKeyEventHandler;
+        if(!list.contains(playerGameInput))
+        {
+            list.add(playerGameInput);
+        }
     }
 
+    public void removeAllListeners()
+    {
+        this.list.clear();
+        super.removeAllListeners();
+    }
+
+    public void removeListener(EventListenerInterface eventListenerInterface)
+    {
+        this.list.remove(eventListenerInterface);
+        super.removeListener(eventListenerInterface);
+    }
+
+    public void fireEvent(AllBinaryEventObject eventObject) throws Exception
+    {        
+        for (int index = this.list.size(); --index >= 0;)
+        {
+            try
+            {
+                PlayerGameInput playerGameInput = (PlayerGameInput) this.list.get(index);
+                playerGameInput.onDownGameKeyEvent((GameKeyEvent) eventObject);
+            }
+            catch (Exception e)
+            {
+                LogUtil.put(new Log(CommonStrings.getInstance().EXCEPTION, this, "fireEvent", e));
+            }
+        }
+
+        super.fireEvent(eventObject);
+    }
+    
     protected void process(AllBinaryEventObject eventObject,
             EventListenerInterface eventListenerInterface) throws Exception
     {
-
         ((DownGameKeyEventListenerInterface) eventListenerInterface)
                 .onDownGameKeyEvent((GameKeyEvent) eventObject);
     }
