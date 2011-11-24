cd97ac19c22e2e71b0c675a4f4aff5c5edb823a9
Travis Berthelot <travisberthelot@hotmail.com>
2011-11-24 07:01:38 -0600

    added

---
+++ j2me/AndroidBaseJavaLibrary/src/org/allbinary/android/AndroidStrings.java
@@ -0,0 +1,33 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.android;
+
+public class AndroidStrings
+{
+    private static final AndroidStrings instance = new AndroidStrings();
+
+    public static AndroidStrings getInstance()
+    {
+        return instance;
+    }
+    
+    public final String FINISH = "finish";
+    public final String CREATE = "onCreate";
+    public final String START = "onStart";
+    public final String RESTART = "onRestart";
+    public final String RESUME = "onResume";
+    public final String PAUSE = "onPause";
+    public final String STOP = "onStop";
+    
+}
