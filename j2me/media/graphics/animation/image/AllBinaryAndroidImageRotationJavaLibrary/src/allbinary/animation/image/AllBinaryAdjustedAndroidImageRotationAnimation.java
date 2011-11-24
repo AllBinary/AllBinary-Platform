757e28c8971b794afbd930a9a3c7a9c348c1e17f
Travis Berthelot <travisberthelot@hotmail.com>
2011-11-24 09:18:10 -0600

    added

---
+++ j2me/media/graphics/animation/image/AllBinaryAndroidImageRotationJavaLibrary/src/allbinary/animation/image/AllBinaryAdjustedAndroidImageRotationAnimation.java
@@ -0,0 +1,42 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.animation.image;
+
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+
+import allbinary.math.AngleInfo;
+
+public class AllBinaryAdjustedAndroidImageRotationAnimation
+extends AllBinaryAndroidImageRotationAnimation
+{
+    private final int dx;
+    private final int dy;
+
+    protected AllBinaryAdjustedAndroidImageRotationAnimation(
+            Image originalImage, Image image, AngleInfo angleInfo, short totalAngle, 
+            int dx, int dy) 
+    throws Exception
+    {
+        super(originalImage, image, angleInfo, totalAngle);
+        
+        this.dx = dx;
+        this.dy = dy;
+    }
+    
+    public void paint(Graphics graphics, int x, int y)
+    {
+        super.paint(graphics, x + this.dx, y + this.dy);
+    }    
+}
