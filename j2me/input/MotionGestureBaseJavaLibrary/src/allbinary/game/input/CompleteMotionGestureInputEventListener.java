33f262c8017161f29b52f669d13d8c56ac401cf6
Travis Berthelot <travisberthelot@hotmail.com>
2011-11-24 22:34:47 -0600

    Added

---
+++ j2me/input/MotionGestureBaseJavaLibrary/src/allbinary/game/input/CompleteMotionGestureInputEventListener.java
@@ -0,0 +1,16 @@
+package allbinary.game.input;
+
+import allbinary.logic.basic.util.event.AllBinaryEventObject;
+
+public class CompleteMotionGestureInputEventListener
+implements CompleteMotionGestureInputEventListenerInterface
+{
+    public void onEvent(AllBinaryEventObject eventObject)
+    {
+    }
+
+    public void onCompleteMotionGestureInputEvent(
+            CompleteMotionGestureInputEvent completeMotionGestureInputEvent)
+    {
+    }
+}
