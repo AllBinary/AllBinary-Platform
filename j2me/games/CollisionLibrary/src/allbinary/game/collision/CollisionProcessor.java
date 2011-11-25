6c1772a157c95bba650666557696c3c942076c62
Travis Berthelot <travisberthelot@hotmail.com>
2011-11-24 23:20:27 -0600

    Adding and Updating

---
+++ j2me/games/CollisionLibrary/src/allbinary/game/collision/CollisionProcessor.java
@@ -0,0 +1,11 @@
+package allbinary.game.collision;
+
+import allbinary.layer.AllBinaryLayer;
+
+public class CollisionProcessor
+{
+    public boolean isCollision(AllBinaryLayer myLayer, AllBinaryLayer myLayer2)
+    {
+        return false;
+    }
+}
---
+++ j2me/games/CollisionLibrary/src/allbinary/game/collision/CollisionThreedProcessor.java
@@ -0,0 +1,22 @@
+package allbinary.game.collision;
+
+import allbinary.layer.AllBinaryLayer;
+
+public class CollisionThreedProcessor
+extends CollisionProcessor
+{
+    public boolean isCollision(AllBinaryLayer myLayer, AllBinaryLayer myLayer2)
+    {
+        if (myLayer2.getX() >= myLayer.getX2() || myLayer2.getY() >= myLayer.getY2() || 
+                myLayer2.getX2() <= myLayer.getX() || myLayer2.getY2() <= myLayer.getY() ||
+                myLayer2.getZ() < myLayer.getZ2() || myLayer2.getZ2() > myLayer.getZ())
+        {
+                return false;
+        }
+        else
+        {
+                //PreLogUtil.put(myLayer2.getZ() + " > " + myLayer.getZ2() + " || " + myLayer2.getZ2() + " < " + myLayer.getZ(), this, "isCollision");
+                return true;
+        }
+    }
+}
