2df5e0f93035df3bd1745bb3878f8b54cc046ebb
Travis Berthelot <travisberthelot@hotmail.com>
2012-03-24 13:50:38 -0500

    Adding Form Base and validation Libraries

---
+++ j2me/media/graphics/FormValidationJavaLibrary/src/allbinary/graphics/form/item/validation/TextItemVisitor.java
@@ -0,0 +1,40 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package allbinary.graphics.form.item.validation;
+
+import abcs.logic.java.bool.BooleanFactory;
+import allbinary.logic.basic.util.visitor.Visitor;
+
+public class TextItemVisitor extends Visitor
+{
+    public TextItemVisitor()
+    {
+        
+    }
+
+    public Object visit(Object object)
+    {
+        String value = (String) object;
+        
+        if (value.length() < 2)
+        {
+            return BooleanFactory.getInstance().TRUE;
+        }
+        else
+        {
+            return BooleanFactory.getInstance().FALSE;
+        }
+        
+    }
+}
---
+++ j2me/media/graphics/FormValidationJavaLibraryM/CustomItemJ2MEComposite.java
@@ -0,0 +1,151 @@
+package allbinary.graphics.form.item;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Item;
+import javax.microedition.lcdui.Screen;
+
+import abcs.logic.basic.NotImplemented;
+import abcs.logic.communication.log.ForcedLogUtil;
+import allbinary.graphics.color.BasicColor;
+
+public class CustomItemJ2MEComposite extends CustomItem implements CustomItemInterface
+{
+    private final Item item;
+    
+    public CustomItemJ2MEComposite(Item item, BasicColor backgroundBasicColor, BasicColor foregroundBasicColor)
+    {
+        super(item.getLabel(), backgroundBasicColor, foregroundBasicColor);
+        
+        //Cast test
+        CustomItemInterface formItemInterface = (CustomItemInterface) item;
+        
+        this.item = item;
+    }
+
+    public void addCommand(Command cmd)
+    {
+        this.item.addCommand(cmd);
+    }
+
+    public String getLabel()
+    {
+        return this.item.getLabel();
+    }
+
+    public int getLayout()
+    {
+        return this.item.getLayout();
+    }
+
+    public int getMinimumHeight()
+    {
+        return this.item.getMinimumHeight();
+    }
+
+    public int getMinimumWidth()
+    {
+        return this.item.getMinimumWidth();
+    }
+
+    public int getPreferredHeight()
+    {
+        return this.item.getPreferredHeight();
+    }
+
+    public int getPreferredWidth()
+    {
+        return this.item.getPreferredWidth();
+    }
+
+    public void removeCommand(Command cmd)
+    {
+        this.item.removeCommand(cmd);
+    }
+
+    public void setDefaultCommand(Command cmd)
+    {
+        this.item.setDefaultCommand(cmd);
+    }
+
+    public void setItemCommandListener(CustomItemCommandListener l)
+    {
+        ForcedLogUtil.log(NotImplemented.NAME, this);
+    }
+
+    public void setLabel(String label)
+    {
+        this.item.setLabel(label);
+    }
+
+    public void setLayout(int layout)
+    {
+        this.item.setLayout(layout);
+    }
+
+    public void setPreferredSize(int width, int height)
+    {
+        this.item.setPreferredSize(width, height);
+    }
+
+    public int getHeight()
+    {
+        return this.item.getHeight();
+    }
+
+    public boolean isFocusable()
+    {
+        return this.item.isFocusable();
+    }
+
+    public void keyPressed(int keyCode)
+    {
+        this.item.keyPressed(keyCode);
+    }
+
+    public int paint(Graphics graphics)
+    {
+        return this.item.paint(graphics);
+    }
+
+    public boolean hasFocus()
+    {
+        return this.item.hasFocus();
+    }
+
+    public void setFocus(boolean state)
+    {
+        this.item.setFocus(state);
+    }
+
+    public boolean select()
+    {
+        return this.item.select();
+    }
+
+    public int traverse(int gameKeyCode, int top, int bottom, boolean action)
+    {
+        return this.item.traverse(gameKeyCode, top, bottom, action);
+    }
+
+    public void setOwner(Screen owner)
+    {
+        this.item.setOwner(owner);
+    }
+    
+    public void paint(Graphics graphics, int x, int y)
+    {
+        ((CustomItemInterface) getItem()).paint(graphics, x, y);
+    }
+    
+    public void paintUnselected(Graphics graphics, int x, int y)
+    {
+        ((CustomItemInterface) getItem()).paintUnselected(graphics, x, y);
+    }
+
+    public Item getItem()
+    {
+        return item;
+    }
+    
+}
