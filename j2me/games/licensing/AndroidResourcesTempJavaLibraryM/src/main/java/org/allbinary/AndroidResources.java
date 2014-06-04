b8ca2c384e638da6184c8a1e65dae485b7aaa976
Travis Berthelot <travisberthelot@hotmail.com>
2014-06-04 13:29:34 -0500

    Added licensing pom

---
+++ j2me/games/licensing/AndroidResourcesTempJavaLibraryM/src/main/java/org/allbinary/AndroidResources.java
@@ -0,0 +1,16 @@
+package org.allbinary;
+
+import org.allbinary.game.testgamedemo.R;
+
+public class AndroidResources
+{
+    public static R.drawable drawable = new R.drawable();
+
+    public static R.id id = new R.id();
+
+    public static R.layout layout = new R.layout();
+
+    public static R.raw raw = new R.raw();
+
+    public static R.string string = new R.string();
+}
