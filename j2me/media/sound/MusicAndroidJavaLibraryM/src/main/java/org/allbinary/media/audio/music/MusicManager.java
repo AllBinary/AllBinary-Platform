ede8cdf89268ca16384bf55039b2ec7654f58ece
Travis Berthelot <travisberthelot@hotmail.com>
2014-07-23 18:49:16 -0500

    Dependency update

--- j2me/media/sound/MusicAndroidJavaLibraryM/src/main/java/org/allbinary/media/audio/music/MusicManager.java
+++ j2me/media/sound/MusicAndroidJavaLibraryM/src/main/java/org/allbinary/media/audio/music/MusicManager.java
@@ -12,84 +12,83 @@ import allbinary.time.GameTickTimeDelayHelperFactory;
 import allbinary.time.TimeDelayHelper;
 import android.content.Intent;
 
-public class MusicManager 
+public class MusicManager
 {
-	private final String PLAY = "Play ";
-	private final String SONG = " for: ";
+    private final String PLAY = "Play ";
+    private final String SONG = " for: ";
 
-	private final TimeDelayHelper timeDelayHelper = new TimeDelayHelper(0);
-	
-	private final BasicArrayList songList;
+    private final TimeDelayHelper timeDelayHelper = new TimeDelayHelper(0);
 
-	private Sound currentSongSound;
-	
-	//MusicService.class
-	private final Intent currentIntent;
-	
-	public MusicManager(Class musicServiceClass, BasicArrayList songList)
-	{
-		currentIntent = new Intent(ResourceUtil.getInstance().getContext(), musicServiceClass);
-		
-		this.songList = songList;
-	}
+    private final BasicArrayList songList;
 
-	public void process()
-	{
-		if(this.timeDelayHelper.isTime(GameTickTimeDelayHelperFactory.getInstance().getStartTime()))
-		{
-			this.startNewSong();
-		}
-	}
+    private Sound currentSongSound;
 
-	public void show()
-	{
-		for(int index = this.songList.size(); --index >= 0;)
-		{
-			Sound sound = (Sound) this.songList.get(index);
+    //MusicService.class
+    private final Intent currentIntent;
 
-			long duration = sound.getPlayer().getDuration();
+    public MusicManager(Class musicServiceClass, BasicArrayList songList)
+    {
+        currentIntent = new Intent(ResourceUtil.getInstance().getContext(), musicServiceClass);
 
-			PreLogUtil.put(PLAY + sound.getResource() + SONG + duration, this, CommonStrings.getInstance().PROCESS);
-		}
-	}
+        this.songList = songList;
+    }
 
-	public void startNewSong()
-	{
-		try
-		{
-			//PreLogUtil.put(Memory.getInfo(), this, CommonStrings.getInstance().PROCESS);
-
-			ResourceUtil.getInstance().getContext().stopService(this.currentIntent);
-			
-			this.currentSongSound = (Sound) BasicArrayListUtil.getInstance().getRandom(this.songList);
-
-			long duration = this.currentSongSound.getDuration();
-					//18000;
-			//this.currentSongSound.getPlayer().getDuration();
-
-			PreLogUtil.put(PLAY + this.currentSongSound.getResource() + SONG + duration, this, CommonStrings.getInstance().PROCESS);
-
-			this.timeDelayHelper.setDelay((int) duration);
-
-			this.currentIntent.putExtra("SONG", ResourceUtil.getInstance().getResourceId(this.currentSongSound.getResource()).intValue());
-			
-			ResourceUtil.getInstance().getContext().startService(this.currentIntent);
+    public void process()
+    {
+        if (this.timeDelayHelper.isTime(GameTickTimeDelayHelperFactory.getInstance().getStartTime()))
+        {
+            this.startNewSong();
         }
-        catch (Exception e)
+    }
+
+    public void show()
+    {
+        for (int index = this.songList.size(); --index >= 0;)
+        {
+            Sound sound = (Sound) this.songList.get(index);
+
+            long duration = sound.getPlayer().getDuration();
+
+            PreLogUtil.put(PLAY + sound.getResource() + SONG + duration, this, CommonStrings.getInstance().PROCESS);
+        }
+    }
+
+    public void startNewSong()
+    {
+        try
+        {
+            //PreLogUtil.put(Memory.getInfo(), this, CommonStrings.getInstance().PROCESS);
+
+            ResourceUtil.getInstance().getContext().stopService(this.currentIntent);
+
+            this.currentSongSound = (Sound) BasicArrayListUtil.getInstance().getRandom(this.songList);
+
+            long duration = this.currentSongSound.getDuration();
+					//18000;
+            //this.currentSongSound.getPlayer().getDuration();
+
+            PreLogUtil.put(PLAY + this.currentSongSound.getResource() + SONG + duration, this, CommonStrings.getInstance().PROCESS);
+
+            this.timeDelayHelper.setDelay((int) duration);
+
+            this.currentIntent.putExtra("SONG", ResourceUtil.getInstance().getResourceId(this.currentSongSound.getResource()).intValue());
+
+            ResourceUtil.getInstance().getContext().startService(this.currentIntent);
+        } catch (Exception e)
         {
             String resource = StringUtil.getInstance().EMPTY_STRING;
             if (currentSongSound != null)
             {
                 resource = currentSongSound.getResource();
             }
-            
+
             PreLogUtil.put(CommonStrings.getInstance().EXCEPTION_LABEL + resource, this, CommonStrings.getInstance().PROCESS, e);
         }
-	}
-	
-	public void stop()
-	throws Exception
-	{
-		ResourceUtil.getInstance().getContext().stopService(this.currentIntent);
-	}
+    }
+
+    public void stop()
+            throws Exception
+    {
+        ResourceUtil.getInstance().getContext().stopService(this.currentIntent);
+    }
 }
