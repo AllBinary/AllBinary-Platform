67900e3e595aab8d204e779f470d5b2e07188aa3
Travis Berthelot <travisberthelot@hotmail.com>
2014-08-05 19:47:10 -0500

    Added base Android Library for non games and assemblies for Android logging

---
+++ j2me/android/AndroidUtilJavaLibraryM/src/main/java/org/allbinary/android/ToastUtil.java
@@ -0,0 +1,47 @@
+package org.allbinary.android;
+
+import abcs.logic.basic.string.CommonStrings;
+import abcs.logic.communication.log.LogFactory;
+import abcs.logic.communication.log.LogUtil;
+import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
+
+public class ToastUtil {
+
+	private static final ToastUtil instance = new ToastUtil();
+
+	public static ToastUtil getInstance() {
+		return instance;
+	}
+
+	public void makeToast(final View view, final Context context, String message, final int time)
+	{
+	    class MakeToast implements Runnable
+	    {
+	        final String string;
+	        
+	        public MakeToast(String string)
+	        {
+	            this.string = string;
+	        }
+	        
+	        public void run()
+	        {
+	            try
+	            {
+	            	//LogUtil.put(LogFactory.getInstance("Trying to Toast: "+ string, this, CommonStrings.getInstance().RUN));
+	            	//PreLogUtil.put("Trying to Toast: "+ string, this, CommonStrings.getInstance().RUN);
+	                
+	                Toast.makeText(context, string, time).show();
+	            }
+	            catch(Exception e)
+	            {
+	                LogUtil.put(LogFactory.getInstance(CommonStrings.getInstance().EXCEPTION, this, CommonStrings.getInstance().RUN, e));
+	            }
+	        }
+	    }
+	 
+	    view.post(new MakeToast(message));
+	}
+}
---
+++ j2me/android/AndroidUtilJavaLibraryM/src/main/java/org/allbinary/android/ViewSwapper.java
@@ -0,0 +1,50 @@
+package org.allbinary.android;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ViewSwapper {
+	
+	private final int rootViewId;
+	private final Activity activity;
+	
+	private int currentMainViewId = -1;
+	private View[] mainViewArray = null;
+	
+	public ViewSwapper(Activity activity, int rootViewId)
+	{
+		this.activity = activity;
+		this.rootViewId = rootViewId;
+	}
+	
+	public void setMainViews(View[] viewArray)
+	{
+		this.mainViewArray = viewArray;
+	}
+	
+	public void setMainView(int id)
+	{
+		ViewGroup viewGroup = (ViewGroup) this.activity.findViewById(rootViewId);
+		
+		if(id != this.currentMainViewId)
+		{
+			for(int index = 0; index < this.mainViewArray.length; index++)
+			{
+				if(id != this.mainViewArray[index].getId())
+				{
+					viewGroup.removeView(this.mainViewArray[index]);
+				}
+			}
+			
+			for(int index = 0; index < this.mainViewArray.length; index++)
+			{
+				if(id == this.mainViewArray[index].getId())
+				{
+					viewGroup.addView(this.mainViewArray[index]);
+					this.currentMainViewId = id;
+				}
+			}			
+		}		
+	}	
+}
