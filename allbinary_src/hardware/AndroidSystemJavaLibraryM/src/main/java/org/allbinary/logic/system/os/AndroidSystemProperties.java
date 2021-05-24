a831ff765001babf8a5d57da245ebc360eb5405e
Travis Berthelot <travisberthelot@allbinary.com>
2021-05-23 21:39:34 -0500

    Remove READ_PHONE_STATE as the advertiser that needed is not no longer in use.

--- allbinary_src/hardware/AndroidSystemJavaLibraryM/src/main/java/org/allbinary/logic/system/os/AndroidSystemProperties.java
+++ allbinary_src/hardware/AndroidSystemJavaLibraryM/src/main/java/org/allbinary/logic/system/os/AndroidSystemProperties.java
@@ -16,7 +16,8 @@ package org.allbinary.logic.system.os;
 import org.allbinary.data.resource.ResourceUtil;
 import android.content.Context;
 import android.os.Build;
-import android.telephony.TelephonyManager;
+import android.provider.Settings;
+import org.allbinary.logic.basic.string.CommonStrings;
 
 /**
  *
@@ -26,98 +27,118 @@ import android.telephony.TelephonyManager;
 public class AndroidSystemProperties
 {
     //private static AndroidSystemProperties SINGLETON = null;
-	private static final AndroidSystemProperties SINGLETON = 
-		new AndroidSystemProperties(ResourceUtil.getInstance().getContext());
+    private static final AndroidSystemProperties SINGLETON = 
+            new AndroidSystemProperties(ResourceUtil.getInstance().getContext());
     
-    private TelephonyManager telephonyManager;
+    //private TelephonyManager telephonyManager;
 
     public static AndroidSystemProperties getInstance()
     {
         return SINGLETON;
     }
 
+    private final CommonStrings commonStrings = CommonStrings.getInstance();
+    
+    private final String androidId;
+    
     public AndroidSystemProperties(Context context)
     {
     	//TWB -  Need to be in manifest
         //<uses-permission android:name="android.permission.READ_PHONE_STATE" ></uses-permission>
-        telephonyManager = (TelephonyManager)
-            context.getSystemService(Context.TELEPHONY_SERVICE);
+        //telephonyManager = (TelephonyManager)
+            //context.getSystemService(Context.TELEPHONY_SERVICE);
+        this.androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
     }
 
     //IMEI
     public String getDeviceId()
     {
-        return telephonyManager.getDeviceId();
+        return this.androidId;
+        //return telephonyManager.getDeviceId();
     }
     
     public String getDeviceSoftwareVersion()
     {
-        return telephonyManager.getDeviceSoftwareVersion();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getDeviceSoftwareVersion();
     }
 
     public String getLine1Number()
     {
-        return telephonyManager.getLine1Number();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getLine1Number();
     }
 
     public String getNetworkCountryIso()
     {
-        return telephonyManager.getNetworkCountryIso();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getNetworkCountryIso();
     }
 
     public String getNetworkOperator()
     {
-        return telephonyManager.getNetworkOperator();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getNetworkOperator();
     }
 
     public String getNetworkOperatorName()
     {
-        return telephonyManager.getNetworkOperatorName();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getNetworkOperatorName();
     }
 
     public String getSimCountryIso()
     {
-        return telephonyManager.getSimCountryIso();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getSimCountryIso();
     }
 
     public String getSimOperator()
     {
-        return telephonyManager.getSimOperator();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getSimOperator();
     }
 
     public String getSimOperatorName()
     {
-        return telephonyManager.getSimOperatorName();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getSimOperatorName();
     }
 
     public String getSimSerialNumber()
     {
-        return telephonyManager.getSimSerialNumber();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getSimSerialNumber();
     }
 
     public String getSubscriberId()
     {
-        return telephonyManager.getSubscriberId();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getSubscriberId();
     }
 
     public String getVoiceMailAlphaTag()
     {
-        return telephonyManager.getVoiceMailAlphaTag();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getVoiceMailAlphaTag();
     }
 
     public String getVoiceMailNumber()
     {
-        return telephonyManager.getVoiceMailNumber();
+        return commonStrings.DISABLE;
+        //return telephonyManager.getVoiceMailNumber();
     }
 
     public int getNetworkType()
     {
-        return telephonyManager.getNetworkType();
+        return -1;
+        //return telephonyManager.getNetworkType();
     }
 
     public int getPhoneType()
     {
-        return telephonyManager.getPhoneType();
+        return -1;
+        //return telephonyManager.getPhoneType();
     }
     
     //Build Properties
