e483c525092eac204bdcac8d1914987e877aed68
Travis Berthelot <travisberthelot@allbinary.com>
2022-04-04 12:30:35 -0500

    The Android fix does not work for the Adjusted Android Image Animation

--- j2me/media/graphics/animation/image/AllBinaryAndroidImageRotationJavaLibraryM/src/main/java/org/allbinary/animation/image/AllBinaryAdjustedAndroidImageRotationAnimation.java
+++ j2me/media/graphics/animation/image/AllBinaryAndroidImageRotationJavaLibraryM/src/main/java/org/allbinary/animation/image/AllBinaryAdjustedAndroidImageRotationAnimation.java
@@ -19,8 +19,8 @@ import javax.microedition.lcdui.Image;
 import org.allbinary.math.AngleInfo;
 
 public class AllBinaryAdjustedAndroidImageRotationAnimation
-        extends AllBinaryNoFlickerAndroidImageRotationAnimation
-//extends AllBinaryAndroidImageRotationAnimation
+        //extends AllBinaryNoFlickerAndroidImageRotationAnimation
+        extends AllBinaryAndroidImageRotationAnimation
 {
     private final int dx;
     private final int dy;
