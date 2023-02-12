f88aa4364b8c6ed7ba5d2ca77ea35b1a07b78e7d
Travis Berthelot <travisberthelot@allbinary.com>
2023-02-12 11:33:10 -0600

    Add logic to rotate PlayN images.

---
+++ j2me/media/graphics/animation/image/ImageHTMLJavaLibraryM/src/main/java/org/allbinary/media/image/ImageRotationUtil.java
@@ -0,0 +1,75 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.media.image;
+
+import javax.microedition.lcdui.Image;
+
+import org.microemu.device.playn.PlaynImmutableImage;
+import org.microemu.device.playn.PlaynMutableImage;
+import org.microemu.device.playn.PlaynSurfaceDisplayGraphics;
+import playn.core.CanvasImage;
+import playn.core.CanvasSurface;
+
+public class ImageRotationUtil
+{
+    private static final ImageRotationUtil instance = new ImageRotationUtil();
+    
+    public static ImageRotationUtil getInstance()
+    {
+        return instance;
+    }
+    
+    private ImageRotationUtil()
+    {
+    }
+
+    public Image createRotatedImage(Image originalImage, int rotationInDegrees)
+            throws Exception
+    {
+        //PreLogUtil.put("originalImage: " + originalImage + " rotationInDegrees: " + rotationInDegrees, this, "createRotatedImage");
+        Image image = ImageCreationUtil.getInstance().getInstance(
+                originalImage.getWidth() , originalImage.getHeight());
+
+        if (image.isMutable())
+        {
+            playn.core.Image originalPlayNImage = null;
+            if(originalImage.isMutable())
+            {
+                //PreLogUtil.put("3a", this, "createRotatedImage");
+                final PlaynMutableImage originalHTMLImage = (PlaynMutableImage) originalImage;
+                originalPlayNImage = (playn.core.Image) originalHTMLImage.getImage();
+                //PreLogUtil.put("4", this, "createRotatedImage");
+            }
+            else
+            {
+                //PreLogUtil.put("3b", this, "createRotatedImage");
+                final PlaynImmutableImage originalHTMLImage = (PlaynImmutableImage) originalImage;
+                originalPlayNImage = (playn.core.Image) originalHTMLImage.getImage();
+                //PreLogUtil.put("4b", this, "createRotatedImage");
+            }
+
+            final PlaynMutableImage htmlImage = (PlaynMutableImage) image;
+            final CanvasImage canvasImage = (CanvasImage) htmlImage.getImage();
+            final CanvasSurface canvasSurface = htmlImage.getCanvasSurface(canvasImage);
+            canvasSurface.drawImage(originalPlayNImage, 0, 0);
+            canvasSurface.rotate(rotationInDegrees);
+
+            return image;
+        }
+        else
+        {
+            throw new Exception("Not Mutable");
+        }
+    }
+}
