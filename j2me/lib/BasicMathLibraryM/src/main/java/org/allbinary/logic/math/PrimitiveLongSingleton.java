68823644e69086839181d8c66f1d8ebb16ffbf74
Travis Berthelot <travisberthelot@allbinary.com>
2023-04-18 10:07:45 -0500

    Add char that is a number into number.

--- j2me/lib/BasicMathLibraryM/src/main/java/org/allbinary/logic/math/PrimitiveLongSingleton.java
+++ j2me/lib/BasicMathLibraryM/src/main/java/org/allbinary/logic/math/PrimitiveLongSingleton.java
@@ -25,9 +25,19 @@ public class PrimitiveLongSingleton
     public final char[] UNK = {'U', 'n', 'k'};
     
     //public final String[] NUMBER_STRING_ARRAY = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    public final byte[] NUMBER_ARRAY = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     public final char[] NUMBER_CHAR_ARRAY = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     public final char[][] NUMBER_CHAR_ARRAYS = {{'0'}, {'1'}, {'2'}, {'3'}, {'4'}, {'5'}, {'6'}, {'7'}, {'8'}, {'9'}};
     
     public final char[] ZERO = new char[]{this.NUMBER_CHAR_ARRAY[0]};
 
+    public byte getNumberFromChar(char numberAsChar) throws Exception {
+        final int size = NUMBER_CHAR_ARRAY.length;
+        for(int index = 0; index < size; index++) {
+            if(NUMBER_CHAR_ARRAY[index] == numberAsChar) {
+                return this.NUMBER_ARRAY[index];
+            }
+        }
+        throw new Exception("numberAsChar was not in array: " + numberAsChar);
+    }
 }
