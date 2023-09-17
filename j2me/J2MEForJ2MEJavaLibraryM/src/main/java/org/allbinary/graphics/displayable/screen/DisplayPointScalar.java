d1e8f62382dbd94f5b1d5330ef0852046558aac0
Travis Berthelot <travisberthelot@allbinary.com>
2023-09-17 10:47:45 -0500

    When the motion input is using the display resolution, but the game is not at the same resolution.

---
+++ j2me/J2MEForJ2MEJavaLibraryM/src/main/java/org/allbinary/graphics/displayable/screen/DisplayPointScalar.java
@@ -0,0 +1,36 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2022 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.graphics.displayable.screen;
+
+import org.allbinary.graphics.GPoint;
+
+/**
+ *
+ * @author User
+ */
+public class DisplayPointScalar {
+    
+    private static final DisplayPointScalar instance = new DisplayPointScalar();
+
+    /**
+     * @return the instance
+     */
+    public static DisplayPointScalar getInstance() {
+        return instance;
+    }
+    
+    public GPoint process(final GPoint point) {
+        return point;
+    }
+}
---
+++ j2me/J2SEForJ2MEJavaLibraryM/src/main/java/org/allbinary/graphics/displayable/screen/DisplayPointScalar.java
@@ -0,0 +1,51 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2022 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.graphics.displayable.screen;
+
+import org.allbinary.graphics.GPoint;
+import org.allbinary.graphics.PointFactory;
+import org.allbinary.graphics.displayable.DisplayInfoSingleton;
+import org.allbinary.logic.basic.string.CommonStrings;
+import org.allbinary.logic.communication.log.LogFactory;
+import org.allbinary.logic.communication.log.LogUtil;
+
+/**
+ *
+ * @author User
+ */
+public class DisplayPointScalar {
+    
+    private static final DisplayPointScalar instance = new DisplayPointScalar();
+
+    /**
+     * @return the instance
+     */
+    public static DisplayPointScalar getInstance() {
+        return instance;
+    }
+    
+    public DisplayPointScalar() {
+    }
+
+    public GPoint process(final GPoint point) {
+        //final CommonStrings commonStrings = CommonStrings.getInstance();
+        //LogUtil.put(LogFactory.getInstance("point: " + point, this, commonStrings.PROCESS));
+        //800 by 480 is the default window.  I assume it is being scaled from that initial window.
+        final DisplayInfoSingleton displayInfoSingleton = DisplayInfoSingleton.getInstance();
+        final float ratio = 480f / (float) (displayInfoSingleton.getLastHeight());
+        //LogUtil.put(LogFactory.getInstance("ratio: " + ratio, this, commonStrings.PROCESS));
+        return PointFactory.getInstance().getInstance((int) (point.getX() * ratio), (int) (point.getY() * ratio));
+    }
+
+}
