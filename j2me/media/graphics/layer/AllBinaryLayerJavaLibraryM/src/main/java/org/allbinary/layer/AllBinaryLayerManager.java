ff42229e4278e8dcd0e2d8966ea1e7bd9cfcc0c7
Travis Berthelot <travisberthelot@allbinary.com>
2023-11-16 09:54:51 -0600

    Missed package change.

--- j2me/media/graphics/layer/AllBinaryLayerJavaLibraryM/src/main/java/org/allbinary/layer/AllBinaryLayerManager.java
+++ j2me/media/graphics/layer/AllBinaryLayerJavaLibraryM/src/main/java/org/allbinary/layer/AllBinaryLayerManager.java
@@ -13,13 +13,14 @@
  */
 package org.allbinary.layer;
 
-import org.allbinary.logic.basic.string.CommonSeps;
-import org.allbinary.logic.basic.string.CommonStrings;
+import org.allbinary.logic.string.CommonSeps;
+import org.allbinary.logic.string.CommonStrings;
 import org.allbinary.logic.communication.log.LogFactory;
 import org.allbinary.logic.communication.log.LogUtil;
+import org.allbinary.logic.string.CommonLabels;
+
 import org.allbinary.layer.event.LayerManagerEvent;
 import org.allbinary.layer.event.LayerManagerEventHandler;
-import org.allbinary.logic.basic.string.CommonLabels;
 
 public class AllBinaryLayerManager extends LayerManager
 {
