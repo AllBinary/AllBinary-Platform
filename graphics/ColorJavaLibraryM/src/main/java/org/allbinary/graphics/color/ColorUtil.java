f8797e2b5bcdc9c6802488588a81d3f08c681534
Travis Berthelot <travisberthelot@allbinary.com>
2023-11-28 11:32:31 -0600

    Add color test.

---
+++ graphics/ColorJavaLibraryM/src/main/java/org/allbinary/graphics/color/ColorUtil.java
@@ -0,0 +1,45 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2022 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.graphics.color;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author User
+ */
+public class ColorUtil {
+
+    private static final ColorUtil instance = new ColorUtil();
+    
+    /**
+     * @return the instance
+     */
+    public static ColorUtil getInstance() {
+        return instance;
+    }
+    
+    public boolean isGreyishOrWhiteLike(final Color color) {
+        
+        //Grey/Black and Pastel/White do not have large deltas between RGB
+        if(Math.abs(color.getRed() - color.getGreen()) > 20 || 
+                Math.abs(color.getRed() - color.getBlue()) > 20 ||
+                Math.abs(color.getGreen() - color.getBlue()) > 20) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
