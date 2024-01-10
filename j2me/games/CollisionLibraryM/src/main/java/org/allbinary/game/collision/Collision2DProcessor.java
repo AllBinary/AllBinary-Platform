e942b06f90edbd71fd616cd6aeb0cca670375bba
Travis Berthelot <travisberthelot@allbinary.com>
2024-01-09 21:36:36 -0600

    Add final

--- j2me/games/CollisionLibraryM/src/main/java/org/allbinary/game/collision/Collision2DProcessor.java
+++ j2me/games/CollisionLibraryM/src/main/java/org/allbinary/game/collision/Collision2DProcessor.java
@@ -5,7 +5,7 @@ import org.allbinary.layer.AllBinaryLayer;
 public class Collision2DProcessor
 extends CollisionProcessor
 {
-    public boolean isCollision(AllBinaryLayer myLayer, AllBinaryLayer myLayer2)
+    public boolean isCollision(final AllBinaryLayer myLayer, final AllBinaryLayer myLayer2)
     {
         if (myLayer2.getX() >= myLayer.getX2() || myLayer2.getY() >= myLayer.getY2() || 
                 myLayer2.getX2() <= myLayer.getX() || myLayer2.getY2() <= myLayer.getY())
