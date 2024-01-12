a266c446ee5da7bb41107e768b44a415ee0a18c3
Travis Berthelot <travisberthelot@allbinary.com>
2024-01-12 17:05:49 -0600

    Shuffle 2 arrays with the same order.

--- j2me/lib/BasicMathLibraryM/src/main/java/org/allbinary/game/rand/MyRandomFactory.java
+++ j2me/lib/BasicMathLibraryM/src/main/java/org/allbinary/game/rand/MyRandomFactory.java
@@ -77,20 +77,53 @@ public class MyRandomFactory
             intArray[randomIndex2] = value;
         }
     }
+
+    public void shuffle(final int[] intArray, final int[] intArray2) {
+        this.shuffle(intArray, intArray2, intArray.length * 7);
+    }
+    
+    public void shuffle(final int[] intArray, final int[] intArray2, final int shuffleTotal) {
+        final int size = intArray.length;
+        int randomIndex;
+        int randomIndex2;
+        int value;
+        int value2;
+        for(int index = 0; index < shuffleTotal; index++) {
+            randomIndex = this.getAbsoluteNextIntAllowZero(size);
+            randomIndex2 = this.getAbsoluteNextIntAllowZero(size);
+            value = intArray[randomIndex];
+            value2 = intArray2[randomIndex];
+            intArray[randomIndex] = intArray[randomIndex2];
+            intArray[randomIndex2] = value;
+            intArray2[randomIndex] = intArray2[randomIndex2];
+            intArray2[randomIndex2] = value2;
+        }
+    }
     
     public static void main(final String[] args) {
         final MyRandomFactory randomFactory = MyRandomFactory.getInstance();
         final int[] intArray = new int[52];
+        final int[] intArray2 = new int[52];
         final int size = intArray.length;
         for(int index = 0; index < size; index++) {
             intArray[index] = index;
+            intArray2[index] = index;
         }
-        randomFactory.shuffle(intArray);
+        randomFactory.shuffle(intArray, intArray2);
         
         final StringBuilder stringBuilder = new StringBuilder();
         for(int index = 0; index < size; index++) {
             stringBuilder.append(intArray[index]).append(',');
         }
+        
+        System.out.println(stringBuilder.toString());
+        
+        stringBuilder.delete(0, stringBuilder.length());
+        for(int index = 0; index < size; index++) {
+            stringBuilder.append(intArray2[index]).append(',');
+        }
+        
+        System.out.println(stringBuilder.toString());
         
         //Validation
         boolean found;
@@ -104,6 +137,5 @@ public class MyRandomFactory
             if(!found) throw new RuntimeException();
         }
         
-        System.out.println(stringBuilder.toString());
     }
 }
