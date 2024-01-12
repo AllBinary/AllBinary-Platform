0c54684f0e4cd7211b8c4896de4677b59ec2089d
Travis Berthelot <travisberthelot@allbinary.com>
2024-01-12 16:58:34 -0600

    Add shuffle.

--- j2me/lib/BasicMathLibraryM/src/main/java/org/allbinary/game/rand/MyRandomFactory.java
+++ j2me/lib/BasicMathLibraryM/src/main/java/org/allbinary/game/rand/MyRandomFactory.java
@@ -15,6 +15,7 @@ package org.allbinary.game.rand;
 
 import java.util.Random;
 
+//Do not use in client code for money related things.
 public class MyRandomFactory
 {
     private static final MyRandomFactory instance = new MyRandomFactory();
@@ -57,5 +58,52 @@ public class MyRandomFactory
         {
             return Math.abs(this.getNextInt(range));
         }
+    }
+    
+    public void shuffle(final int[] intArray) {
+        this.shuffle(intArray, intArray.length * 7);
+    }
+    
+    public void shuffle(final int[] intArray, final int shuffleTotal) {
+        final int size = intArray.length;
+        int randomIndex;
+        int randomIndex2;
+        int value;
+        for(int index = 0; index < shuffleTotal; index++) {
+            randomIndex = this.getAbsoluteNextIntAllowZero(size);
+            randomIndex2 = this.getAbsoluteNextIntAllowZero(size);
+            value = intArray[randomIndex];
+            intArray[randomIndex] = intArray[randomIndex2];
+            intArray[randomIndex2] = value;
+        }
+    }
+    
+    public static void main(final String[] args) {
+        final MyRandomFactory randomFactory = MyRandomFactory.getInstance();
+        final int[] intArray = new int[52];
+        final int size = intArray.length;
+        for(int index = 0; index < size; index++) {
+            intArray[index] = index;
+        }
+        randomFactory.shuffle(intArray);
+        
+        final StringBuilder stringBuilder = new StringBuilder();
+        for(int index = 0; index < size; index++) {
+            stringBuilder.append(intArray[index]).append(',');
+        }
+        
+        //Validation
+        boolean found;
+        for(int index = 0; index < size; index++) {
+            found = false;
+            for(int index2 = 0; index2 < size; index2++) {
+                if(intArray[index2] == index) {
+                    found = true;
+                }
+            }
+            if(!found) throw new RuntimeException();
+        }
+        
+        System.out.println(stringBuilder.toString());
     }
 }
