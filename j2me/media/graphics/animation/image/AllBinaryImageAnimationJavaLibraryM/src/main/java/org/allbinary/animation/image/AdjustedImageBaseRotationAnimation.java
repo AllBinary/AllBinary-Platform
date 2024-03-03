1b5028cc0ef944c9851458d72b135276aa56edaf
Travis Berthelot <travisberthelot@allbinary.com>
2024-03-03 01:58:45 -0600

    Add the generic case.

---
+++ j2me/media/graphics/animation/image/AllBinaryImageAnimationJavaLibraryM/src/main/java/org/allbinary/animation/image/AdjustedImageBaseRotationAnimation.java
@@ -0,0 +1,50 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.animation.image;
+
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+
+import org.allbinary.animation.AnimationBehavior;
+import org.allbinary.math.AngleInfo;
+
+public class AdjustedImageBaseRotationAnimation
+extends ImageBaseRotationAnimation
+{
+    private int dx;
+    private int dy;
+
+    public AdjustedImageBaseRotationAnimation(final Image image, final AngleInfo angleInfo, final short totalAngle, final int dx, final int dy, final AnimationBehavior animationBehavior)
+        throws Exception
+    {
+        super(image, angleInfo, totalAngle, animationBehavior);
+
+        // LogUtil.put(LogFactory.getInstance(, this,));
+
+        this.init(dx, dy);
+        //-(imageArray[0].getWidth() >> 2), -(imageArray[0].getHeight() >> 2)
+    }
+
+    public void init(final int dx, final int dy) throws Exception
+    {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void paint(final Graphics graphics, final int x, final int y)
+    {
+        super.paint(graphics, x + this.dx, y + this.dy);
+    }
+
+}
---
+++ j2me/media/graphics/animation/image/AllBinaryImageAnimationJavaLibraryM/src/main/java/org/allbinary/animation/image/ImageRotationAnimationFactory.java
@@ -0,0 +1,149 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.animation.image;
+
+import javax.microedition.lcdui.Image;
+
+import org.allbinary.animation.Animation;
+import org.allbinary.animation.AnimationBehaviorFactory;
+import org.allbinary.game.configuration.GameConfigurationCentral;
+import org.allbinary.image.AnimationFactoryImageScaleUtil;
+import org.allbinary.math.AngleFactory;
+import org.allbinary.math.AngleInfo;
+
+public class ImageRotationAnimationFactory 
+    extends BaseImageAnimationFactory
+{
+    private final AnimationFactoryImageScaleUtil animationFactoryImageScaleUtil = AnimationFactoryImageScaleUtil.getInstance();
+    
+    protected final short angleIncrement;
+
+    public ImageRotationAnimationFactory(final Image image, final int dx, final int dy)
+    throws Exception
+    {
+        this(image, image.getWidth(), image.getHeight(), dx, dy, AnimationBehaviorFactory.getInstance());
+    }
+    
+    public ImageRotationAnimationFactory(final Image image, final int dx, final int dy, final Object unused, final AnimationBehaviorFactory animationBehaviorFactory)
+    throws Exception
+    {
+        this(image, image.getWidth(), image.getHeight(), dx, dy, animationBehaviorFactory);
+    }
+
+    public ImageRotationAnimationFactory(final Image image, final int dx, final int dy, final short angleIncrement, final Object unused) 
+    throws Exception
+    {
+        this(image, image.getWidth(), image.getHeight(), dx, dy, angleIncrement, AnimationBehaviorFactory.getInstance());
+    }
+
+    public ImageRotationAnimationFactory(final Image image, final int dx, final int dy, final short angleIncrement, final Object unused, final AnimationBehaviorFactory animationBehaviorFactory) 
+    throws Exception
+    {
+        this(image, image.getWidth(), image.getHeight(), dx, dy, angleIncrement, animationBehaviorFactory);
+    }
+
+    public ImageRotationAnimationFactory(final Image image,
+            final int width, final int height, final int dx, final int dy, final short angleIncrement) throws Exception
+    {
+
+        this(image, width, height, dx, dy, angleIncrement, AnimationBehaviorFactory.getInstance());
+        
+    }
+    
+    public ImageRotationAnimationFactory(final Image image,
+            final int width, final int height, final int dx, final int dy, final short angleIncrement, final AnimationBehaviorFactory animationBehaviorFactory) throws Exception
+    {
+
+        this(image, width, height, angleIncrement, animationBehaviorFactory);
+
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public ImageRotationAnimationFactory(final Image image,
+            final int width, final int height, final int dx, final int dy) throws Exception
+    {
+
+        this(image, width, height, dx, dy, AnimationBehaviorFactory.getInstance());
+    }
+    
+    public ImageRotationAnimationFactory(final Image image,
+            final int width, final int height, final int dx, final int dy, final AnimationBehaviorFactory animationBehaviorFactory) throws Exception
+    {
+
+        this(image, width, height, animationBehaviorFactory);
+
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public ImageRotationAnimationFactory(final Image image)
+            throws Exception
+    {
+        this(image, image.getWidth(), image.getHeight(), AnimationBehaviorFactory.getInstance());
+    }
+    
+    public ImageRotationAnimationFactory(final Image image, final AnimationBehaviorFactory animationBehaviorFactory)
+            throws Exception
+    {
+        this(image, image.getWidth(), image.getHeight(), animationBehaviorFactory);
+    }
+    
+    public ImageRotationAnimationFactory(final Image image, final int width, final int height, final AnimationBehaviorFactory animationBehaviorFactory)
+            throws Exception
+    {
+        super(image, width, height, animationBehaviorFactory);
+
+        this.angleIncrement = (short) (AngleFactory.getInstance().TOTAL_ANGLE / GameConfigurationCentral.getInstance().getGameControlFidelity());
+    }
+
+    public ImageRotationAnimationFactory(final Image image, final int width, final int height,
+            final short angleIncrement, final AnimationBehaviorFactory animationBehaviorFactory) throws Exception
+    {
+        super(image, width, height, animationBehaviorFactory);
+
+        this.angleIncrement = angleIncrement;
+    }
+    
+    public Animation getInstance() throws Exception {
+
+        if (dx != 0 || dy != 0) {
+
+            animationFactoryImageScaleUtil.processAdjust(this);
+
+            return new AdjustedImageBaseRotationAnimation(
+                this.getImage(),
+                AngleInfo.getInstance(this.angleIncrement),
+                AngleFactory.getInstance().TOTAL_ANGLE, this.dx, this.dy, this.animationBehaviorFactory.getOrCreateInstance());
+
+        } else {
+            return new ImageBaseRotationAnimation(
+                this.getImage(),
+                AngleInfo.getInstance(this.angleIncrement),
+                AngleFactory.getInstance().TOTAL_ANGLE, this.animationBehaviorFactory.getOrCreateInstance());
+        }
+        
+    }
+
+    protected short getAngleIncrement()
+    {
+        return angleIncrement;
+    }
+    
+    public void setInitialSize(final int width, final int height) {
+        this.scaleWidth = width;
+        this.scaleHeight = height;
+    }
+    
+}
