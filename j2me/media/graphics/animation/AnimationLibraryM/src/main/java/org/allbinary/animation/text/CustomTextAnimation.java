4e6b771821ad5197d4ebf2bd1725ec77ffaf728b
Travis Berthelot <travisberthelot@allbinary.com>
2024-03-08 22:57:19 -0600

    Text hack for HTML5 builds.

--- j2me/media/graphics/animation/AnimationLibraryM/src/main/java/org/allbinary/animation/text/CustomTextAnimation.java
+++ j2me/media/graphics/animation/AnimationLibraryM/src/main/java/org/allbinary/animation/text/CustomTextAnimation.java
@@ -16,8 +16,14 @@ package org.allbinary.animation.text;
 import javax.microedition.lcdui.Font;
 import javax.microedition.lcdui.Graphics;
 import org.allbinary.animation.AnimationBehavior;
+import org.allbinary.game.configuration.feature.Features;
+import org.allbinary.game.configuration.feature.HTMLFeatureFactory;
 
 import org.allbinary.graphics.font.FontDebugFactory;
+import org.allbinary.logic.communication.log.LogFactory;
+import org.allbinary.logic.communication.log.LogUtil;
+import org.allbinary.logic.string.CommonStrings;
+import org.allbinary.logic.string.StringMaker;
 
 /**
  *
@@ -30,14 +36,32 @@ public class CustomTextAnimation extends TextAnimation
     protected final int fontSize;
     
     protected Font font;
-    
+
     public CustomTextAnimation(final String text, final int fontSize, final AnimationBehavior animationBehavior)
     {
         super(text, animationBehavior);
         
         this.fontSize = fontSize;
-        //LogUtil.put(LogFactory.getInstance(new StringMaker().append("font: ").append(fontSize).toString(), this, CommonStrings.getInstance().PROCESS));
+        //LogUtil.put(LogFactory.getInstance(new StringMaker().append("font: ").append(fontSize).append(" text: ").append(text).toString(), this, CommonStrings.getInstance().PROCESS));
         this.font = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, fontSize);
+        
+        this.hack();
+    }
+    
+    private int xtraWidth = 0;
+    private void hack() {
+        //TWB - Hack for HTML5 build since FontUtil microemu-playn kerning is not correct
+        final Features features = Features.getInstance();
+        final boolean isHTML = features.isDefault(HTMLFeatureFactory.getInstance().HTML);
+        if(isHTML) {
+            if(fontSize >= 15 && fontSize <= 23) {
+                this.xtraWidth = (fontSize * this.getText().length * this.getText().length);
+            } else if(fontSize < 15) {
+                this.xtraWidth = (this.getText().length * this.getWidth() / 3);
+            } else {
+                this.xtraWidth = (fontSize * fontSize * this.getText().length * this.getWidth() / 1800);
+            }
+        }
     }
     
     public void paint(final Graphics graphics, final int x, final int y)
@@ -46,7 +70,7 @@ public class CustomTextAnimation extends TextAnimation
         
         fontDebugFactory.setFont(this.font, graphics);
         
-        super.paint(graphics, x, y);
+        super.paint(graphics, x + this.xtraWidth, y);
         
         fontDebugFactory.setFont(existingFont, graphics);
     }
@@ -56,6 +80,7 @@ public class CustomTextAnimation extends TextAnimation
             throw new RuntimeException();
         }
 
+        LogUtil.put(LogFactory.getInstance(new StringMaker().append("setScale font: ").append((fontSize * scaleX)).append(" text: ").append(this.getText()).toString(), this, CommonStrings.getInstance().PROCESS));
         this.font = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, (int) (fontSize * scaleX));
     }
     
