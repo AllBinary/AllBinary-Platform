a784860165370290dd9d5ce7bec412d205c7ed37
Travis Berthelot <travisberthelot@allbinary.com>
2024-03-14 00:30:26 -0500

    Remove stacktrace on logging.

--- j2me/games/GameJ2MELibraryM/src/main/java/org/allbinary/graphics/form/item/CustomTextBox.java
+++ j2me/games/GameJ2MELibraryM/src/main/java/org/allbinary/graphics/form/item/CustomTextBox.java
@@ -101,7 +101,7 @@ public class CustomTextBox extends GameCommandCanvas
     
     public void keyPressed(final int keyCode, final int deviceId)
     {
-        LogUtil.put(LogFactory.getInstance(new StringMaker().append(commonStrings.START).append(keyCode).toString(), this, "keyPressed", new Exception()));
+        LogUtil.put(LogFactory.getInstance(new StringMaker().append(commonStrings.START).append(keyCode).toString(), this, "keyPressed"));
         
         final PlatformKeyFactory platformKeyFactory = PlatformKeyFactory.getInstance();
         
