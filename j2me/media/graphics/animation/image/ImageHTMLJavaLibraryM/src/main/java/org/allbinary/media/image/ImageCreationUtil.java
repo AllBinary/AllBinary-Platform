443088c9f8c5c6b94ca65df64127ffba1858df86
Travis Berthelot <travisberthelot@allbinary.com>
2024-05-07 08:49:17 -0500

    Remove ImageBaseJavaLibrary. Update pom files to reflect that.

---
+++ j2me/media/graphics/animation/image/ImageHTMLJavaLibraryM/src/main/java/org/allbinary/media/image/ImageCreationUtil.java
@@ -0,0 +1,48 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.media.image;
+
+import javax.microedition.lcdui.Image;
+
+import org.allbinary.image.GameFeatureImageCacheFactory;
+
+public class ImageCreationUtil
+{
+    private static final ImageCreationUtil instance = new ImageCreationUtil();
+
+    public static ImageCreationUtil getInstance()
+    {
+        return instance;
+    }
+    
+    private ImageCreationUtil()
+    {
+        
+    }
+    
+    public Image getInstance(final int width, final int height) throws Exception
+    {
+        final Image image = GameFeatureImageCacheFactory.getInstance().get(this.getClass().getName(), width, height);
+        return image;
+    }
+    
+    public Image createImage(final int width, final int height, final float maxScaleX, final float maxScaleY) 
+    throws Exception
+    {
+        //LogUtil.put(LogFactory.getInstance(": " + scaleNominatorX + " / " + scaleDenominatorX + " = " + scaleX, this, "createImage"));
+        final Image image = GameFeatureImageCacheFactory.getInstance().get(this.getClass().getName(), (int) (width * maxScaleX) + 1, (int) (height * maxScaleY) + 1);
+        return image;
+    }
+    
+}
---
+++ j2me/media/graphics/animation/image/ImageJ2SEJavaLibraryM/src/main/java/org/allbinary/media/image/ImageCopyUtil.java
@@ -0,0 +1,104 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.media.image;
+
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+
+import org.allbinary.graphics.Anchor;
+import org.allbinary.graphics.color.BasicColorFactory;
+import org.allbinary.graphics.color.BasicColorSetUtil;
+import org.allbinary.logic.string.CommonStrings;
+import org.allbinary.logic.communication.log.LogFactory;
+import org.allbinary.logic.communication.log.LogUtil;
+
+public class ImageCopyUtil
+{
+    private static final ImageCopyUtil instance = new ImageCopyUtil();
+    
+    public static ImageCopyUtil getInstance()
+    {
+        return instance;
+    }
+    
+    private final ImageCreationUtil imageCreationUtil = ImageCreationUtil.getInstance();
+    //private final BasicColorSetUtil basicColorUtil = BasicColorSetUtil.getInstance();
+
+    private ImageCopyUtil()
+    {
+    }
+    
+    private int anchor = Anchor.TOP_LEFT;
+    
+    // String resource,
+    public Image createImage(final Image originalImage)
+            throws Exception
+    {
+        final Image image = imageCreationUtil.getInstance(
+                originalImage.getWidth() , originalImage.getHeight());
+
+        if (image.isMutable())
+        {
+            image.getGraphics().drawImage(originalImage, 0, 0, anchor);
+            
+            return image;
+        }
+        else
+        {
+            throw new Exception("Not Mutable");
+        }
+    }
+
+    public Image createImage(final Image originalImage, final float canvasScale, final boolean resize)
+            throws Exception
+    {
+        //LogUtil.put(LogFactory.getInstance("width: " + originalImage.getWidth() + " height: " + originalImage.getHeight(), this, CommonStrings.getInstance().CONSTRUCTOR));
+        
+        int newWidth = (int) (originalImage.getWidth() * canvasScale);
+        int newHeight = (int) (originalImage.getHeight() * canvasScale);
+        
+        if(resize) {
+            if (newWidth < newHeight) {
+                newWidth = newHeight;
+            }
+
+            if (newHeight < newWidth) {
+                newHeight = newWidth;
+            }
+        }
+        
+        //LogUtil.put(LogFactory.getInstance("newWidth: " + newWidth + " newHeight: " + newHeight, this, CommonStrings.getInstance().CONSTRUCTOR));
+        
+        final Image image = imageCreationUtil.getInstance(newWidth, newHeight);
+
+        if (image.isMutable())
+        {
+            final int halfWidthDelta = (newWidth - originalImage.getWidth()) / 2;
+            final int halfHeightDelta = (newHeight - originalImage.getHeight()) / 2;
+            //LogUtil.put(LogFactory.getInstance("deltas width: " + halfWidthDelta + " height: " + halfHeightDelta, this, CommonStrings.getInstance().CONSTRUCTOR));
+            final Graphics graphics = image.getGraphics();
+            graphics.drawImage(originalImage, halfWidthDelta, halfHeightDelta, anchor);
+            //this.basicSetColorUtil.setBasicColor(graphics, BasicColorFactory.getInstance().YELLOW);
+            //graphics.drawRect(halfWidthDelta, halfHeightDelta, originalImage.getWidth(), originalImage.getHeight());
+            //this.basicSetColorUtil.setBasicColor(graphics, BasicColorFactory.getInstance().WHITE);
+            //graphics.drawRect(0, 0, newWidth, newHeight);
+            
+            return image;
+        }
+        else
+        {
+            throw new Exception("Not Mutable");
+        }
+    }
+}
