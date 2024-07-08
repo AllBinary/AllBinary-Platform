54bd2b19f14372ffcd36589b772483c7e8f54a8a
Travis Berthelot <travisberthelot@allbinary.com>
2024-07-07 21:19:20 -0500

    Remark logging.

--- j2me/games/PreGameJ2MEJavaLibraryM/src/main/java/org/allbinary/canvas/RunnableCanvas.java
+++ j2me/games/PreGameJ2MEJavaLibraryM/src/main/java/org/allbinary/canvas/RunnableCanvas.java
@@ -45,7 +45,7 @@ public class RunnableCanvas extends MyCanvas
     
     public RunnableCanvas(final CommandListener commandListener, final boolean hasParam)
     {
-        LogUtil.put(LogFactory.getInstance("delay: " + this.loopTimeHelper.delay, this, this.commonStrings.CONSTRUCTOR));
+        //LogUtil.put(LogFactory.getInstance(new StringMaker().append("delay: ").append(this.loopTimeHelper.delay).toString(), this, this.commonStrings.CONSTRUCTOR));
         
         //this.processPaintable = ProcessPaintableSingletonFactory.getInstance();
         this.runnableCanvasRefreshHelper = new RunnableCanvasRefreshHelper(this);
