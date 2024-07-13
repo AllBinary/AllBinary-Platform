adfa1ea3d80fd9eb1889fa3c7fd27f66bf1a557e
Travis Berthelot <travisberthelot@allbinary.com>
2024-07-13 17:25:09 -0500

    Add to temp

---
+++ j2me/TempJavaLibraryM/src/main/java/org/allbinary/ThreedResources.java
@@ -0,0 +1,34 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2022 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary;
+
+/**
+ *
+ * @author User
+ */
+public class ThreedResources {
+
+    private static final ThreedResources threedResources = new ThreedResources();
+
+    /**
+     * @return the threedResources
+     */
+    public static ThreedResources getThreedResources() {
+        return threedResources;
+    }
+
+    public void add() {
+
+    }
+}
