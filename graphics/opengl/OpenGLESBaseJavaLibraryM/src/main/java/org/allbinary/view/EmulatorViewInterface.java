71cb455c90e0ba0a42ba357af4f405ba2ce3cc0a
Travis Berthelot <travisberthelot@allbinary.com>
2024-08-07 09:07:51 -0500

    Add setMidlet for JOGL.

--- graphics/opengl/OpenGLESBaseJavaLibraryM/src/main/java/org/allbinary/view/EmulatorViewInterface.java
+++ graphics/opengl/OpenGLESBaseJavaLibraryM/src/main/java/org/allbinary/view/EmulatorViewInterface.java
@@ -1,9 +1,12 @@
 package org.allbinary.view;
 
 import javax.microedition.lcdui.Displayable;
+import javax.microedition.midlet.MIDlet;
 
 public interface EmulatorViewInterface
 {
+    void setMidlet(final MIDlet midlet);
+    
     //KeyInputViewCompositeInterface
     void onEmulatorInitComplete(final Object midletActivity);
 
