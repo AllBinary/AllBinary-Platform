5e805c1bbf61750883e1785d0b90f2245c50962f
Travis Berthelot <travisberthelot@allbinary.com>
2024-08-08 16:18:20 -0500

    Move the no SWT option to the OpenGL directory.

---
+++ graphics/opengl/SWTWithoutJOGLJavaLibraryM/src/main/java/org/allbinary/graphics/threed/GLHolder.java
@@ -0,0 +1,39 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2022 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.graphics.threed;
+
+/**
+ *
+ * @author User
+ */
+public class GLHolder {
+        
+    public boolean isCreated = true;
+
+    public GLHolder() {
+    }
+
+    public void runOnSurfaceCreated(final Object renderer) {
+        
+    }
+    
+    public void runOnSurfaceChanged(final Object renderer, final int width, final int height) {
+        
+    }
+    
+    public void run(final Object renderer) {
+        
+    }
+
+}
---
+++ graphics/opengl/SWTWithoutJOGLJavaLibraryM/src/main/java/org/allbinary/graphics/threed/SWTJOGLProcessor.java
@@ -0,0 +1,87 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2022 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.graphics.threed;
+
+import javax.microedition.midlet.MIDlet;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+
+/**
+ *
+ * @author User
+ */
+public class SWTJOGLProcessor implements Runnable {
+    
+    private static final SWTJOGLProcessor instance = new SWTJOGLProcessor();
+
+    /**
+     * @return the instance
+     */
+    public static SWTJOGLProcessor getInstance() {
+        return instance;
+    }
+   
+    public GLHolder glHolder = new GLHolder() {
+        public void run(Object renderer) {
+            
+        }
+    };
+    
+    public void init(final Display display, final Composite comp) {
+        
+    }
+    
+    public void setRenderer(final Object renderer) {
+
+    }
+    
+    public boolean isJOGL() {
+        return false;
+    }
+    
+    public void createSurface() {
+        
+    }
+    
+    public void onSurfaceCreated() {
+        
+    }
+
+    public void onSurfaceChanged(final Event event) {
+
+    }
+   
+    public void onSurfaceChanged() {
+        
+    }
+    
+    public void onSurfaceChanged(final int width, final int height) {
+        
+    }
+    
+    public void addListener() {
+
+    }
+
+    public void addListeners(final MIDlet midlet) {
+    
+    }
+    
+    public void run() {
+                
+    }
+    
+}
