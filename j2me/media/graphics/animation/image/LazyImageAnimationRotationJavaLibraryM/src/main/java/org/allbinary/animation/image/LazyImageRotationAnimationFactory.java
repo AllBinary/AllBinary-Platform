dc49bad439291bb25cb12176f4d3bcc5b237caf0
Travis Berthelot <travisberthelot@allbinary.com>
2024-09-11 11:27:38 -0500

    Start adding in lazy loading for animations.

---
+++ j2me/media/graphics/animation/image/LazyImageAnimationRotationJavaLibraryM/src/main/java/org/allbinary/animation/image/LazyImageRotationAnimationFactory.java
@@ -0,0 +1,41 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2022 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.animation.image;
+
+import org.allbinary.animation.Animation;
+import org.allbinary.animation.AnimationInterfaceFactoryInterface;
+import org.allbinary.animation.IndexedAnimationBehavior;
+import org.allbinary.media.ScaleProperties;
+
+/**
+ *
+ * @author User
+ */
+public class LazyImageRotationAnimationFactory implements AnimationInterfaceFactoryInterface {
+    
+    private final AnimationInterfaceFactoryInterface animationInterfaceFactoryInterface;
+    
+    public LazyImageRotationAnimationFactory(final AnimationInterfaceFactoryInterface animationInterfaceFactoryInterface) {
+        this.animationInterfaceFactoryInterface = animationInterfaceFactoryInterface;
+    }
+    
+    public Animation getInstance() throws Exception {
+        return new LazyImageRotationAnimation(this.animationInterfaceFactoryInterface, new IndexedAnimationBehavior(1, 250));
+    }
+    
+    public void setInitialScale(final ScaleProperties scaleProperties) {
+        
+    }
+    
+}
