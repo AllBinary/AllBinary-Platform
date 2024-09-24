c26ac1e50f1cd1c94dca774c0eba03bd1e98a8e1
Travis Berthelot <travisberthelot@allbinary.com>
2024-09-24 18:13:48 -0500

    Include the background color.

--- j2me/ProgressLazyJ2MEJavaLibraryM/src/main/java/org/allbinary/graphics/canvas/transition/progress/LazyProgressCanvas.java
+++ j2me/ProgressLazyJ2MEJavaLibraryM/src/main/java/org/allbinary/graphics/canvas/transition/progress/LazyProgressCanvas.java
@@ -31,15 +31,20 @@ public class LazyProgressCanvas extends ProgressCanvas {
     private final Paintable GAUGE_PAINTABLE = new Paintable() {
         public void paint(Graphics graphics) {
             final DisplayInfoSingleton displayInfoSingleton = DisplayInfoSingleton.getInstance();
+            graphics.setColor(backgroundBasicColor.intValue());
             graphics.fillRect(0, 0, displayInfoSingleton.getLastWidth(), displayInfoSingleton.getLastHeight());
             gauge.paint(graphics, 0, 0);
         }
     };
     
+    private final BasicColor backgroundBasicColor;
+        
     private Paintable paintable = GAUGE_PAINTABLE;
     
     protected LazyProgressCanvas(final String title, final BasicColor backgroundBasicColor, final BasicColor foregroundBasicColor) {
         super(title, backgroundBasicColor, foregroundBasicColor);
+        
+        this.backgroundBasicColor = backgroundBasicColor;
     }
     
     @Override    
