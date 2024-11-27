e636fcc91d5ee4cb427a559e473b87664b948509
Travis Berthelot <travisberthelot@allbinary.com>
2024-11-27 12:19:20 -0600

    Add resize for width and height divided by 16 without remainder.

--- graphics/ImageToolJavaApplicationM/src/main/java/org/allbinary/image/gui/ResizeImageJPanel.java
+++ graphics/ImageToolJavaApplicationM/src/main/java/org/allbinary/image/gui/ResizeImageJPanel.java
@@ -141,6 +141,58 @@ public class ResizeImageJPanel extends javax.swing.JPanel
         }.start();
     }
 
+    public void updateFor16Above() {
+        new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    final ImageProcessorInput imageProcessorInput = ResizeImageJPanel.this.getImageProcessorInput();
+                    
+                    final int size = imageProcessorInput.getBufferedImageArray().length;
+                    final BufferedImage[] bufferedImageArray = imageProcessorInput.getBufferedImageArray();
+                    BufferedImage bufferedImage;
+                    for(int index = 0; index < size; index++) {
+                        bufferedImage = bufferedImageArray[0];
+                        ResizeImageJPanel.this.jTextField1.setText(Integer.toString(bufferedImage.getWidth() / 16 * 16));
+                        ResizeImageJPanel.this.jTextField2.setText(Integer.toString(bufferedImage.getHeight() / 16 * 16));
+                    }
+
+                    ResizeImageJPanel.this.getParent().repaint();
+
+                } catch (Exception e) {
+                    LogUtil.put(LogFactory.getInstance(commonStrings.EXCEPTION, this, CommonStrings.getInstance().RUN, e));
+                }
+            }
+        }.start();
+    }
+
+    public void updateFor16Below() {
+        new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    final ImageProcessorInput imageProcessorInput = ResizeImageJPanel.this.getImageProcessorInput();
+                    
+                    final int size = imageProcessorInput.getBufferedImageArray().length;
+                    final BufferedImage[] bufferedImageArray = imageProcessorInput.getBufferedImageArray();
+                    BufferedImage bufferedImage;
+                    for(int index = 0; index < size; index++) {
+                        bufferedImage = bufferedImageArray[0];
+                        ResizeImageJPanel.this.jTextField1.setText(Integer.toString(((bufferedImage.getWidth() / 16) + 1) * 16));
+                        ResizeImageJPanel.this.jTextField2.setText(Integer.toString(((bufferedImage.getHeight() / 16) + 1) * 16));
+                    }
+
+                    ResizeImageJPanel.this.getParent().repaint();
+
+                } catch (Exception e) {
+                    LogUtil.put(LogFactory.getInstance(commonStrings.EXCEPTION, this, CommonStrings.getInstance().RUN, e));
+                }
+            }
+        }.start();
+    }
+    
     public ImageProcessorInput getImageProcessorInput() {
         return imageProcessorInput;
     }
@@ -160,22 +212,24 @@ public class ResizeImageJPanel extends javax.swing.JPanel
 
         jLabel1 = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
-        jButton1 = new javax.swing.JButton();
+        aboveJButton = new javax.swing.JButton();
         jTextField1 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
-        jButton2 = new javax.swing.JButton();
+        updateJButton = new javax.swing.JButton();
         floatPercentJTextField = new javax.swing.JTextField();
+        adjustFor16AboveJButton = new javax.swing.JButton();
+        adjustFor16BelowJButton = new javax.swing.JButton();
 
         jLabel1.setText("Percent:");
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
-        jButton1.setText("Process");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        aboveJButton.setText("Process");
+        aboveJButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                aboveJButtonActionPerformed(evt);
             }
         });
 
@@ -183,15 +237,29 @@ public class ResizeImageJPanel extends javax.swing.JPanel
 
         jLabel3.setText("Height:");
 
-        jButton2.setText("Update");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        updateJButton.setText("Update");
+        updateJButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                updateJButtonActionPerformed(evt);
             }
         });
 
         floatPercentJTextField.setText("-1.000000");
         floatPercentJTextField.setMinimumSize(new java.awt.Dimension(120, 22));
+
+        adjustFor16AboveJButton.setText("Adjust for 16 Above");
+        adjustFor16AboveJButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                adjustFor16AboveJButtonActionPerformed(evt);
+            }
+        });
+
+        adjustFor16BelowJButton.setText("Adjust for 16 Below");
+        adjustFor16BelowJButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                adjustFor16BelowJButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -209,12 +277,15 @@ public class ResizeImageJPanel extends javax.swing.JPanel
                     .addComponent(jTextField1)
                     .addComponent(jTextField2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(floatPercentJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(floatPercentJTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(adjustFor16AboveJButton)
+                    .addComponent(adjustFor16BelowJButton))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jButton2)
+                .addComponent(updateJButton)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
+                .addComponent(aboveJButton)
                 .addGap(0, 250, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -228,37 +299,49 @@ public class ResizeImageJPanel extends javax.swing.JPanel
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
+                    .addComponent(jLabel2)
+                    .addComponent(adjustFor16AboveJButton))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(adjustFor16BelowJButton))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(aboveJButton)
+                    .addComponent(updateJButton))
                 .addContainerGap(175, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void aboveJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_aboveJButtonActionPerformed
         this.process();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_aboveJButtonActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void updateJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_updateJButtonActionPerformed
         this.update();
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_updateJButtonActionPerformed
+
+    private void adjustFor16AboveJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_adjustFor16AboveJButtonActionPerformed
+        this.updateFor16Above();
+    }//GEN-LAST:event_adjustFor16AboveJButtonActionPerformed
+
+    private void adjustFor16BelowJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_adjustFor16BelowJButtonActionPerformed
+        this.updateFor16Below();
+    }//GEN-LAST:event_adjustFor16BelowJButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton aboveJButton;
+    private javax.swing.JButton adjustFor16AboveJButton;
+    private javax.swing.JButton adjustFor16BelowJButton;
     private javax.swing.JTextField floatPercentJTextField;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
+    private javax.swing.JButton updateJButton;
     // End of variables declaration//GEN-END:variables
 }
