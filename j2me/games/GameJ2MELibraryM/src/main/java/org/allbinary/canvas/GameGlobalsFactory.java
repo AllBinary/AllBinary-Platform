acb80c6ea5e1deea9c034cbc5f6017170ae459c6
Travis Berthelot <travisberthelot@allbinary.com>
2025-01-09 20:05:59 -0600

    move new Canvas to the game library.

---
+++ j2me/games/GameJ2MELibraryM/src/main/java/org/allbinary/canvas/GameGlobalsFactory.java
@@ -0,0 +1,36 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2022 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.canvas;
+
+import org.allbinary.graphics.GPoint;
+import org.allbinary.graphics.PointFactory;
+
+/**
+ *
+ * @author User
+ */
+public class GameGlobalsFactory {
+
+    private static final GameGlobalsFactory instance = new GameGlobalsFactory();
+
+    /**
+     * @return the instance
+     */
+    public static GameGlobalsFactory getInstance() {
+        return instance;
+    }
+
+    public boolean newCanvas = true;
+    public GPoint point = PointFactory.getInstance().ZERO_ZERO;
+}
