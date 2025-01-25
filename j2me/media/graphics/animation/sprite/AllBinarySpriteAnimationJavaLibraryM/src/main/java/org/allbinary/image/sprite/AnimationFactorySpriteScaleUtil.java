de8308a880aeefd7a90faef2e1d63426765daffe
Travis Berthelot <travisberthelot@allbinary.com>
2025-01-25 01:59:30 -0600

    Update sprite scaling.

--- j2me/media/graphics/animation/sprite/AllBinarySpriteAnimationJavaLibraryM/src/main/java/org/allbinary/image/sprite/AnimationFactorySpriteScaleUtil.java
+++ j2me/media/graphics/animation/sprite/AllBinarySpriteAnimationJavaLibraryM/src/main/java/org/allbinary/image/sprite/AnimationFactorySpriteScaleUtil.java
@@ -18,6 +18,7 @@ import javax.microedition.lcdui.game.Sprite;
 
 import org.allbinary.game.layer.SpriteFactory;
 import org.allbinary.graphics.SpacialStrings;
+import org.allbinary.graphics.opengles.OpenGLUtil;
 import org.allbinary.image.ImageCache;
 import org.allbinary.image.ImageCacheFactory;
 import org.allbinary.image.opengles.OpenGLESImageExclusionUtil;
@@ -46,6 +47,7 @@ public class AnimationFactorySpriteScaleUtil {
     private final ImageCache imageCache = ImageCacheFactory.getInstance();
     private final ImageScaleUtil imageScaleUtil = ImageScaleUtil.getInstance();
     //private final ImageCopyUtil imageCopyUtil = ImageCopyUtil.getInstance();
+    private final OpenGLUtil openGLUtil = OpenGLUtil.getInstance();
     
     private final OpenGLESImageExclusionUtil openGLESImageExclusionUtil = OpenGLESImageExclusionUtil.getInstance();
     
@@ -53,12 +55,13 @@ public class AnimationFactorySpriteScaleUtil {
 //       final CommonStrings commonStrings = CommonStrings.getInstance();
 //       final SpacialStrings spacialStrings = SpacialStrings.getInstance();
 //       final StringMaker stringMaker = new StringMaker();
+//       .append(image.getName())
 //       LogUtil.put(LogFactory.getInstance(stringMaker.append("width: ").append(width).append(" height: ").append(height).append("scaleWidth: ").append(scaleWidth).append(" scaleHeight: ").append(scaleHeight).toString(), this, commonStrings.PROCESS));
 
       final SpriteFactory spriteFactory = SpriteFactory.getInstance();
 
        Sprite sprite;
-
+       Image scaledImage;
        if(scaleWidth != 0 && scaleHeight != 0) {
            float scaleX = ((float) scaleWidth) / ((float) width);
            float scaleY = ((float) scaleHeight) / ((float) height);
@@ -68,16 +71,16 @@ public class AnimationFactorySpriteScaleUtil {
 //           stringMaker.delete(0, stringMaker.length());
 //           LogUtil.put(LogFactory.getInstance(stringMaker.append("0scaleX: ").append(scaleX).append(" scaleY: ").append(scaleY).toString(), this, commonStrings.PROCESS));
 
-           Image scaledImage;
            if ((scaleX == 1 && scaleY == 1) || (scaleX == 0 || scaleY == 0)) {
 //               stringMaker.delete(0, stringMaker.length());
 //               LogUtil.put(LogFactory.getInstance(stringMaker.append("noscale ").append(spacialStrings.WIDTH_LABEL).append(width).append(spacialStrings.HEIGHT_LABEL).append(height).toString(), this, commonStrings.PROCESS));
-               scaledImage = image;
-               sprite = spriteFactory.create(image, width, height);
+               scaledImage = openGLUtil.add(image);
+               sprite = spriteFactory.create(scaledImage, width, height);
            } else {
 //               stringMaker.delete(0, stringMaker.length());
 //               LogUtil.put(LogFactory.getInstance(stringMaker.append("scaleX: ").append(scaleX).append(" scaleY: ").append(scaleY).toString(), this, commonStrings.PROCESS));
                if(openGLESImageExclusionUtil.isCustomScaling(image)) {
+                   //This does not work for J2ME.
                    final int width2 =  Math.round((scaleWidth) - 0.5f);
                    final int height2 =  Math.round((scaleHeight) - 0.5f);
 
@@ -126,7 +129,8 @@ public class AnimationFactorySpriteScaleUtil {
        } else {
 //           stringMaker.delete(0, stringMaker.length());
 //           LogUtil.put(LogFactory.getInstance(stringMaker.append("noscale2").append(spacialStrings.WIDTH_LABEL).append(width).append(spacialStrings.HEIGHT_LABEL).append(height).toString(), this, commonStrings.PROCESS));
-           sprite = spriteFactory.create(image, width, height);
+           scaledImage = openGLUtil.add(image);
+           sprite = spriteFactory.create(scaledImage, width, height);
        }
         
         return sprite;
