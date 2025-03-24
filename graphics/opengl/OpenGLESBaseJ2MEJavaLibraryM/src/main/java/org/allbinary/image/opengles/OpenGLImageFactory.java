ccab3a5ac059dce1883a6e7a0a39593f4a2bffed
Travis Berthelot <travisberthelot@allbinary.com>
2025-03-24 08:05:51 -0500

    More bloat for the J2ME build for now.

---
+++ graphics/opengl/OpenGLESBaseJ2MEJavaLibraryM/src/main/java/org/allbinary/image/opengles/OpenGLImageFactory.java
@@ -0,0 +1,36 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.image.opengles;
+
+import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.lcdui.Image;
+import org.allbinary.platform.graphics.PlatformBitmapBaseFactory;
+import org.allbinary.platform.opengles.PlatformTextureBaseFactory;
+
+public class OpenGLImageFactory
+{
+//    public Image getInstance(GL10 gl, Image image, boolean matchColor)
+//    {
+//        throw new RuntimeException();
+//        //return null;
+//    }
+
+    public Image getInstance(final Image image, final PlatformBitmapBaseFactory bitmapFactory, 
+        final PlatformTextureBaseFactory textureFactory)
+    {
+        throw new RuntimeException();
+        //return null;
+    }
+    
+}
---
+++ graphics/opengl/OpenGLESBaseJ2MEJavaLibraryM/src/main/java/org/allbinary/image/opengles/OpenGLImageSpecificFactory.java
@@ -0,0 +1,44 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.image.opengles;
+
+import org.allbinary.logic.communication.log.PreLogUtil;
+import org.allbinary.logic.string.StringMaker;
+import org.allbinary.logic.string.StringUtil;
+
+public class OpenGLImageSpecificFactory
+{
+    private static final OpenGLImageSpecificFactory instance = new OpenGLImageSpecificFactory();
+
+    private OpenGLImageFactory imageFactory;
+    
+    public boolean updating = false;
+    
+    public static OpenGLImageSpecificFactory getInstance()
+    {
+        return instance;
+    }
+
+    public void setImageFactory(OpenGLImageFactory imageFactory)
+    {
+        this.imageFactory = imageFactory;
+        
+        PreLogUtil.put(new StringMaker().append("Selected Image Factory: ").append(StringUtil.getInstance().toString(this.imageFactory)).toString(), this, "setImageFactory");
+    }
+
+    public OpenGLImageFactory getImageFactory()
+    {
+        return imageFactory;
+    }
+}
