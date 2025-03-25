1fec9bbd8877287a4345ef26116cf893ddf86ff7
Travis Berthelot <travisberthelot@allbinary.com>
2025-03-24 21:34:04 -0500

    Update dependency and removal.

--- graphics/opengl/OpenGLESJavaLibraryM/src/main/java/org/allbinary/image/opengles/OpenGLSurfaceChangedInterface.java
+++
@@ -1,8 +0,0 @@
-package org.allbinary.image.opengles;
-
-import javax.microedition.khronos.opengles.GL;
-
-public interface OpenGLSurfaceChangedInterface
-{
-    void set(GL gl) throws Exception;
-}
