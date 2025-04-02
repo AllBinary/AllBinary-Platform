3dbfc535779c9fb878bb3d0c7b2aa1abeb53cc0a
Travis Berthelot <travisberthelot@allbinary.com>
2025-04-02 16:44:05 -0500

    Add more of the null platform cases for VBO.

---
+++ graphics/opengl/AppNoneOpenGLESJavaLibraryM/src/main/java/org/allbinary/graphics/opengles/shader/NullifyShaderMatrixOpenGLProcessor.java
@@ -0,0 +1,39 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2025 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.graphics.opengles.shader;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import org.allbinary.graphics.opengles.OpenGLProcessor;
+
+/**
+ *
+ * @author User
+ */
+public class NullifyShaderMatrixOpenGLProcessor extends OpenGLProcessor {
+ 
+    private static final NullifyShaderMatrixOpenGLProcessor instance = new NullifyShaderMatrixOpenGLProcessor();
+
+    /**
+     * @return the instance
+     */
+    public static NullifyShaderMatrixOpenGLProcessor getInstance() {
+        return instance;
+    }
+ 
+    public void process(final GL10 gl)
+    {        
+    }
+
+}
