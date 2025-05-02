757bc2c8040d7ee7026b1b28133c5f38f79c7530
Travis Berthelot <travisberthelot@allbinary.com>
2025-05-02 15:23:32 -0500

    Add the Faces impl for shaders.

---
+++ graphics/opengl/AppNoneOpenGLESJavaLibraryM/src/main/java/org/allbinary/graphics/opengles/EnableFacesShaderOpenGLProcessor.java
@@ -0,0 +1,27 @@
+package org.allbinary.graphics.opengles;
+
+import javax.microedition.khronos.opengles.GL10;
+import org.allbinary.graphics.threed.min3d.renderer.processor.ObjectThreedOpenGLProcessor;
+
+public class EnableFacesShaderOpenGLProcessor extends ObjectThreedOpenGLProcessor
+{
+    private static final EnableFacesShaderOpenGLProcessor instance = new EnableFacesShaderOpenGLProcessor();
+
+    /**
+     * @return the instance
+     */
+    public static EnableFacesShaderOpenGLProcessor getInstance() {
+        return instance;
+    }
+    
+    public EnableFacesShaderOpenGLProcessor()
+    {
+        //PreLogUtil.put(commonStrings.START, this, commonStrings.CONSTRUCTOR);
+
+    }
+
+    public void process(final GL10 gl, final Object object)
+    {
+
+    }
+}
