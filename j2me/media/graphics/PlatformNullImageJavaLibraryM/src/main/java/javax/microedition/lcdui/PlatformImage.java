63895a651fb13708ca26bcae9f169cc2ef586b87
Travis Berthelot <travisberthelot@allbinary.com>
2025-05-05 10:08:17 -0500

    Update the null case.

--- j2me/media/graphics/PlatformNullImageJavaLibraryM/src/main/java/javax/microedition/lcdui/PlatformImage.java
+++ j2me/media/graphics/PlatformNullImageJavaLibraryM/src/main/java/javax/microedition/lcdui/PlatformImage.java
@@ -21,12 +21,49 @@ import javax.microedition.lcdui.Image;
  */
 public class PlatformImage {
 
-    public int getWidth() {
-        return 0;
+    public Object getImage(final Object graphicsSurface2) {
+        return null;
+    }
+
+    public int getWidth(final Object graphicsSurface2, final int width) {
+        return width;
+    }
+
+    public int getHeight(final Object graphicsSurface2, final int height) {
+        return height;
     }
     
-    public int getHeight() {
-        return 0;
+    public int getWidth(final Image image, final int width) {
+        return width;
+    }
+
+    public int getHeight(final Image image, final int height) {
+        return height;
+    }
+    
+    public javax.microedition.lcdui.Graphics getGraphics(final Object graphicsSurface, final int width, final int height, final Image image) {
+        return null;
+    }
+
+    public void drawImage(final Image image, final int x, final int y, int anchor, final javax.microedition.lcdui.Graphics g2, final Object g) {
+
+    }
+
+    public void drawRegion(final Image image, final int x_src, final int y_src, final int width,
+        final int height, final int transform, final int x_dst, final int y_dst, final int anchor, final javax.microedition.lcdui.Graphics g) {
+
+    }
+
+    public void getRGB(final int[] argb, final int offset, final int scanlength, final int x, final int y, final int width, final int height, final Image image) {
+
+    }
+
+    public void setRGB(final int[] argb, final int offset, final int scanlength, final int x, final int y, final int width, final int height, final Image image) {
+        
+    }
+
+    public int[] getData(final Image image) {
+        return null;
     }
 
 }
