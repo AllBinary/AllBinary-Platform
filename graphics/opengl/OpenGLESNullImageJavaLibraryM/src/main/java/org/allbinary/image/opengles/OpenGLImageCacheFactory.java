83f9cc616ae85831d2223a0d683730381c530bf5
Travis Berthelot <travisberthelot@allbinary.com>
2025-05-10 12:25:10 -0500

    Simplify the null case.

---
+++ graphics/opengl/OpenGLESNullImageJavaLibraryM/src/main/java/org/allbinary/image/opengles/OpenGLImageCacheFactory.java
@@ -0,0 +1,41 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.image.opengles;
+
+import org.allbinary.image.ImageCache;
+
+public class OpenGLImageCacheFactory
+{
+    private static ImageCache IMAGE_CACHE;
+
+    /*
+    public static void init(GL10 gl)
+    {
+        ((OpenGLImageCache) IMAGE_CACHE).setGl(gl);
+    }
+    */
+
+    public static ImageCache getInstance()
+    {
+        return IMAGE_CACHE;
+    }
+    
+    public static void init()
+    {
+        if(IMAGE_CACHE == null)
+        {
+            //IMAGE_CACHE = new OpenGLImageCache();
+        }
+    }
+}
