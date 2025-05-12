d8cc4be201980261159bbc2a281ceae14637b712
Travis Berthelot <travisberthelot@allbinary.com>
2025-05-11 22:08:07 -0500

    Add Loaded instance

---
+++ graphics/opengl/OpenGLESJavaLibraryM/src/main/java/org/allbinary/graphics/opengles/shader/LoadedShaderManager.java
@@ -0,0 +1,31 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2025 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.graphics.opengles.shader;
+
+/**
+ *
+ * @author User
+ */
+public class LoadedShaderManager {
+    
+    protected static final ShaderManager instance = new ShaderManager(-1, -1);
+
+    /**
+     * @return the instance
+     */
+    public static ShaderManager getInstance() {
+        return instance;
+    }
+    
+}
