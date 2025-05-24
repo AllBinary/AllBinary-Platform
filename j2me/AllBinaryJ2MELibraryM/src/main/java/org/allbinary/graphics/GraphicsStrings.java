8275497fad5c09f62643d14b523b09275f049308
Travis Berthelot <travisberthelot@allbinary.com>
2025-05-23 21:54:24 -0500

    Add graphics strings.

---
+++ j2me/AllBinaryJ2MELibraryM/src/main/java/org/allbinary/graphics/GraphicsStrings.java
@@ -0,0 +1,37 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2025 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.graphics;
+
+/**
+ *
+ * @author User
+ */
+public class GraphicsStrings {
+    
+    private static final GraphicsStrings instance = new GraphicsStrings();
+
+    /**
+     * @return the instance
+     */
+    public static GraphicsStrings getInstance() {
+        return instance;
+    }
+    
+    public final String ANIMATION = "animation";
+    public final String ANGLE = "angle";
+    public final String MOVEMENT_ANGLE = "movement_angle";
+    public final String ROTATION = "rotation";
+    public final String OPACITY = "opacity";
+    
+}
