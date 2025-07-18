7f047322009604b6d5d79791856b4c163afada5d
Travis Berthelot <travisberthelot@allbinary.com>
2025-07-18 12:22:37 -0500

    Simplify for Kotlin build

--- allbinary_src/logic/data/file/FileJavaLibraryM/src/main/java/org/allbinary/logic/io/StreamUtil.java
+++ allbinary_src/logic/data/file/FileJavaLibraryM/src/main/java/org/allbinary/logic/io/StreamUtil.java
@@ -10,7 +10,7 @@
 * 
 * Created By: Travis Berthelot
 * 
-*/
+ */
 package org.allbinary.logic.io;
 
 import java.io.ByteArrayOutputStream;
@@ -21,23 +21,20 @@ import java.io.OutputStream;
 import org.allbinary.logic.communication.log.LogUtil;
 import org.allbinary.string.CommonStrings;
 
-public class StreamUtil
-{
-    protected final LogUtil logUtil = LogUtil.getInstance();
+public class StreamUtil {
 
+    protected final LogUtil logUtil = LogUtil.getInstance();
 
     private static final StreamUtil instance = new StreamUtil();
 
     /**
      * @return the instance
      */
-    public static StreamUtil getInstance()
-    {
+    public static StreamUtil getInstance() {
         return instance;
     }
 
-    private StreamUtil()
-    {
+    private StreamUtil() {
     }
 
     /*
@@ -52,14 +49,18 @@ public class StreamUtil
      *
      * return buffer; }
      */
-
     public OutputStream get(final InputStream inputStream,
-        final OutputStream outputStream, byte[] buffer) throws Exception
-    {
+        final OutputStream outputStream, byte[] buffer) throws Exception {
         //int total = 0;
         int len = 0;
-        while ((len = inputStream.read(buffer, 0, buffer.length)) != -1)
-        {
+        while (true) {
+            len = inputStream.read(buffer, 0, buffer.length);
+
+            if (len != -1) {
+            } else {
+                break;
+            }
+
             outputStream.write(buffer, 0, len);
             //total = total + len;
         }
@@ -70,7 +71,6 @@ public class StreamUtil
         logUtil.put("Total Bytes Written: " + total, this, commonStrings.GET);
         }
          */
-
         return outputStream;
     }
 
@@ -80,39 +80,31 @@ public class StreamUtil
 //        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream(8000);
 //        return this.getByteArray(inputStream, outputStream, byteArray);
 //    }
-
     public byte[] getByteArray(final InputStream inputStream, final ByteArrayOutputStream outputStream2, final byte[] byteArray)
-        throws Exception
-    {
+        throws Exception {
         final ByteArrayOutputStream outputStream = outputStream2;
-    	try
-    	{
+        try {
             this.get(inputStream, outputStream, byteArray);
             return outputStream.toByteArray();
-        } 
-        finally
-        {
+        } catch (Exception e) {
+            throw e;
+        } finally {
             this.close(outputStream);
-        }        
+        }
     }
 
 //    public String getAsString(final InputStream inputStream, final byte[] byteArray1) throws Exception {
 //        final byte[] byteArray = this.getByteArray(inputStream, byteArray1);
 //        return new String(byteArray);
 //    }
-    
-    public boolean close(Closeable closeable)
-    {
-        try
-        {
-            if (closeable != null)
-            {
+    public boolean close(Closeable closeable) {
+        try {
+            if (closeable != null) {
                 //logUtil.put("Closing: " + closeable, this, CLOSE);
                 closeable.close();
             }
             return true;
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             final CommonStrings commonStrings = CommonStrings.getInstance();
             logUtil.put(commonStrings.EXCEPTION, this, commonStrings.CLOSE, e);
             return false;
