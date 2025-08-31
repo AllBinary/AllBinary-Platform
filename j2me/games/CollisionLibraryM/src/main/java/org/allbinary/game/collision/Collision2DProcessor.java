2d8eda4cebb7da22ce980460bba0ba9a2d51a28e
Travis Berthelot <travisberthelot@allbinary.com>
2025-08-30 20:07:32 -0500

    Correct typo from KMP changes.

--- j2me/games/CollisionLibraryM/src/main/java/org/allbinary/game/collision/Collision2DProcessor.java
+++ j2me/games/CollisionLibraryM/src/main/java/org/allbinary/game/collision/Collision2DProcessor.java
@@ -8,7 +8,7 @@ extends CollisionProcessor
     @Override
     public boolean isCollision(final AllBinaryLayer myLayer, final AllBinaryLayer myLayer2)
     {
-        if (myLayer2.getXP() >= myLayer.getX2() || myLayer2.getY2() >= myLayer.getY2() || 
+        if (myLayer2.getXP() >= myLayer.getX2() || myLayer2.getYP() >= myLayer.getY2() || 
                 myLayer2.getX2() <= myLayer.getXP() || myLayer2.getY2() <= myLayer.getYP())
         {
             return false;
