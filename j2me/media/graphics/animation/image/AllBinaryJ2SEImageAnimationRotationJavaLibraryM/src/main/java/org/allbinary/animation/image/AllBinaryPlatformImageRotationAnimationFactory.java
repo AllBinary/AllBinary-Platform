af595a10f354f753654d26a1b1a3e989cc1ab2f6
Travis Berthelot <travisberthelot@allbinary.com>
2025-09-19 08:52:13 -0500

    Handle OpenGL ImageRotationAnimations.

---
+++ j2me/media/graphics/animation/image/AllBinaryJ2SEImageAnimationRotationJavaLibraryM/src/main/java/org/allbinary/animation/image/AllBinaryPlatformImageRotationAnimationFactory.java
@@ -0,0 +1,32 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2025 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.animation.image;
+
+import javax.microedition.lcdui.Image;
+
+import org.allbinary.animation.AnimationBehaviorFactory;
+
+/**
+ *
+ * @author User
+ */
+public class AllBinaryPlatformImageRotationAnimationFactory extends AllBinaryJ2SEImageRotationAnimationFactory {
+    
+    public AllBinaryPlatformImageRotationAnimationFactory(final Image image, final int width, final int height,
+            final short angleIncrement, final AnimationBehaviorFactory animationBehaviorFactory, final boolean resizeCanvasForRotation) throws Exception
+    {
+        super(image, width, height, angleIncrement, animationBehaviorFactory, resizeCanvasForRotation);
+    }
+    
+}
