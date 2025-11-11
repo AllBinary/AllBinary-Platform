577bbe78b5e8ad71c98c1564791862eba11501c5
Travis Berthelot <travisberthelot@allbinary.com>
2025-11-11 07:22:12 -0600

    Add logging to the OpenGLProcessor when OpenGL is allowed

--- graphics/opengl/OpenGLESJavaLibraryM/src/main/java/org/allbinary/graphics/opengles/OpenGLProcessor.java
+++ graphics/opengl/OpenGLESJavaLibraryM/src/main/java/org/allbinary/graphics/opengles/OpenGLProcessor.java
@@ -2,10 +2,14 @@ package org.allbinary.graphics.opengles;
 
 import javax.microedition.khronos.opengles.GL10;
 
+import org.allbinary.logic.communication.log.LogUtil;
+
 import org.allbinary.string.CommonStrings;
 
 public class OpenGLProcessor
 {
+    protected final LogUtil logUtil = LogUtil.getInstance();
+
     protected final CommonStrings commonStrings = CommonStrings.getInstance();
     
     public void process(GL10 gl)
