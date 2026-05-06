17fd159d32587b7e9d5e1c2ab71b36619c4c7980
Travis Berthelot <travisberthelot@allbinary.com>
2026-05-06 07:08:56 -0500

    Updates for TS conversion

--- j2me/media/graphics/NullCustomUIPreJavaLibraryM/src/main/java/org/allbinary/graphics/form/item/ABStringComponent.java
+++ j2me/media/graphics/NullCustomUIPreJavaLibraryM/src/main/java/org/allbinary/graphics/form/item/ABStringComponent.java
@@ -5,13 +5,13 @@ import org.allbinary.graphics.color.BasicColorFactory;
 
 public class ABStringComponent
 {
-    public static final ABStringComponent NULL_STRING_COMPONENT = new ABStringComponent();
+    public static final ABStringComponent NULL_STRING_COMPONENT = new ABStringComponent(BasicColorFactory.getInstance().BLACK, BasicColorFactory.getInstance().WHITE);
     
     public BasicColor getBackgroundBasicColor() {
         return BasicColorFactory.getInstance().WHITE;
     }
 
-    public void setBackgroundBasicColor(BasicColor backgroundBasicColor) {
+    public ABStringComponent(final BasicColor backgroundBasicColor, final BasicColor foregroundBasicColor) {
     }
 
     public BasicColor getForegroundBasicColor() {
