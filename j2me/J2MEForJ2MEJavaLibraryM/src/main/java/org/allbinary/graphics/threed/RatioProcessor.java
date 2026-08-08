4fd71b203e4da9d708977089bbaae4c697111b60
Travis Berthelot <travisberthelot@allbinary.com>
2026-08-08 01:35:27 -0500

    Remove AbPathData from J2ME builds. Add empty SWTJOGLProcessor to J2MEForJ2ME

---
+++ j2me/J2MEForJ2MEJavaLibraryM/src/main/java/org/allbinary/graphics/threed/RatioProcessor.java
@@ -0,0 +1,26 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2025 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.graphics.threed;
+
+/**
+ *
+ * @author User
+ */
+public class RatioProcessor {
+    
+    public void setCustom(final int width, final int height, final float ratio) {
+        
+    }
+
+}
---
+++ j2me/J2MEForJ2MEJavaLibraryM/src/main/java/org/allbinary/graphics/threed/SWTJOGLProcessor.java
@@ -0,0 +1,116 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2022 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary.graphics.threed;
+
+import javax.microedition.lcdui.Font;
+import javax.microedition.midlet.MIDlet;
+
+/**
+ *
+ * @author User
+ */
+//OpenGLNone
+public class SWTJOGLProcessor implements Runnable {
+    
+    private static final SWTJOGLProcessor instance = new SWTJOGLProcessor();
+
+    /**
+     * @return the instance
+     */
+    public static SWTJOGLProcessor getInstance() {
+        return SWTJOGLProcessor.instance;
+    }
+
+    public void init(final Object display, final Object comp) {
+        
+    }
+
+    public boolean isHolderCreated() {
+        return true;
+    }
+
+    public void setRenderer(final Object renderer) {
+
+    }
+    
+    public boolean isJOGL() {
+        return false;
+    }
+    
+    public void createSurface() {
+        
+    }
+    
+    public void onSurfaceCreated() {
+        
+    }
+
+    public void onSurfaceChangedForEvent(final Object event) {
+
+    }
+   
+    public void onSurfaceChanged() {
+        
+    }
+    
+    public void onSurfaceChangedWH(final int width, final int height) {
+        
+    }
+    
+    public void addListener() {
+
+    }
+
+    public void addListeners(final MIDlet midlet) {
+    
+    }
+    
+    @Override
+    public void run() {
+                
+    }
+
+    public void clear() {
+        
+    }
+
+    public void setCustom(final int width, final int height, final float ratio) {
+    
+    }
+
+    public void setRatioProcessor(RatioProcessor ratioProcessor) {
+
+    }
+
+    public boolean isPortraitRatioProcessor() {
+        return true;
+    }
+
+    public int charWidth(Font font, char ch) {
+        throw new RuntimeException();
+    }
+
+    public int charsWidth(Font font, char[] ch, int offset, int length) {
+        throw new RuntimeException();
+    }
+
+    public int stringWidth(Font font, String str) {
+        throw new RuntimeException();
+    }
+
+    public int substringWidth(Font font, String str, int offset, int len) {
+        throw new RuntimeException();
+    }
+    
+}
