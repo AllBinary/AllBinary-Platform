706ae2fe8594bfde5273409f4587ee5960d40d8a
Travis Berthelot <travisberthelot@allbinary.com>
2026-08-16 03:18:09 -0500

    Updates for the KMP build.

---
+++ j2me/TempBaseJavaLibraryM/src/main/java/org/allbinary/TsUtil.java
@@ -0,0 +1,40 @@
+/*
+ * AllBinary Open License Version 1
+ * Copyright (c) 2025 AllBinary
+ * 
+ * By agreeing to this license you and any business entity you represent are
+ * legally bound to the AllBinary Open License Version 1 legal agreement.
+ * 
+ * You may obtain the AllBinary Open License Version 1 legal agreement from
+ * AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+ * 
+ * Created By: Travis Berthelot
+ * 
+ */
+package org.allbinary;
+
+/**
+ *
+ * @author User
+ */
+public class TsUtil {
+
+    private static final TsUtil instance = new TsUtil();
+
+    /**
+     * @return the instance
+     */
+    public static TsUtil getInstance() {
+        return TsUtil.instance;
+    }
+
+    public int hashCode(Object object) {
+        return object.hashCode();
+    }
+    
+    public void waitFor(Object any, final long timeoutMillis) throws InterruptedException {
+        final Object object = (Object) any;
+        object.wait(timeoutMillis);
+    }
+    
+}
