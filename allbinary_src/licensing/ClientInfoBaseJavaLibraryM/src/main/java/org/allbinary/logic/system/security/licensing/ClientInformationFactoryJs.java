488cd6063027a7335bec29784ee0e9cbec2c2911
Travis Berthelot <travisberthelot@allbinary.com>
2026-08-30 03:39:39 -0500

    Remove

--- allbinary_src/licensing/ClientInfoBaseJavaLibraryM/src/main/java/org/allbinary/logic/system/security/licensing/ClientInformationFactoryJs.java
+++
@@ -1,9 +0,0 @@
-package org.allbinary.logic.system.security.licensing;
-
-import jsinterop.annotations.JsPackage;
-import jsinterop.annotations.JsType;
-
-@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
-public interface ClientInformationFactoryJs {
-    ClientInformation getInstance();
-}
