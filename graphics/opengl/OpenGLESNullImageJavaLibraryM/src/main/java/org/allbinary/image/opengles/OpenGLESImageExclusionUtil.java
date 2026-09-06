8165047f218893a70994aa9215efb4205bd892da
Travis Berthelot <travisberthelot@allbinary.com>
2026-09-06 07:12:21 -0500

    This should have only been on the HTML version of this class

--- graphics/opengl/OpenGLESNullImageJavaLibraryM/src/main/java/org/allbinary/image/opengles/OpenGLESImageExclusionUtil.java
+++ graphics/opengl/OpenGLESNullImageJavaLibraryM/src/main/java/org/allbinary/image/opengles/OpenGLESImageExclusionUtil.java
@@ -14,7 +14,6 @@
 package org.allbinary.image.opengles;
 
 import javax.microedition.lcdui.Image;
-import jsinterop.annotations.JsType;
 
 import org.allbinary.util.BasicArrayList;
 import org.allbinary.util.BasicArrayListD;
@@ -23,7 +22,6 @@ import org.allbinary.util.BasicArrayListD;
  *
  * @author User
  */
-@JsType
 public class OpenGLESImageExclusionUtil {
     
     private static final OpenGLESImageExclusionUtil instance = new OpenGLESImageExclusionUtil();
