f3d17941c6f4ec94fbfdad2091db2b51053154d5
Travis Berthelot <travisberthelot@allbinary.com>
2026-09-24 18:56:05 -0500

    Update for KT build.

--- j2me/input/TouchButtonJavaLibraryM/src/main/java/org/allbinary/input/motion/button/TouchButtonLocationHelper.java
+++ j2me/input/TouchButtonJavaLibraryM/src/main/java/org/allbinary/input/motion/button/TouchButtonLocationHelper.java
@@ -36,8 +36,8 @@ public class TouchButtonLocationHelper
 
         //PreLogUtil.put(commonStrings.START_LABEL + displayInfo.toString(), this, "TouchButtonLocationHelper");
 
-        this.totalColumns = Math.round(displayInfo.getLastWidth() / buttonSize);
-        this.totalRows = Math.round(displayInfo.getLastHeight() / buttonSize);
+        this.totalColumns = Math.round((float) displayInfo.getLastWidth() / buttonSize);
+        this.totalRows = Math.round((float) displayInfo.getLastHeight() / buttonSize);
 
         this.columnsRemainder = displayInfo.getLastWidth() % buttonSize;
         this.columnsRemainderHalf = (this.columnsRemainder >> 1);
